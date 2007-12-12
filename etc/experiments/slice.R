29ae391f38ce33af4470ad199ceb8ccc9f8817e5
Jon Clayden <jon.clayden@gmail.com>
2007-12-12 14:19:57 +0000

    Added slice expt
    git-svn-id: https://tractor.googlecode.com/svn/trunk@73 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/slice.R
@@ -0,0 +1,18 @@
+require(tractor.fsl)
+
+runExperiment <- function ()
+{
+    requireArguments(1)
+    image <- newMriImageFromFile(Arguments[1])
+    
+    if (nArguments() > 1)
+        outputFile <- Arguments[2]
+    else
+        outputFile <- image$getSource()
+    
+    x <- getWithDefault("X", NA, "numeric", errorIfInvalid=TRUE)
+    y <- getWithDefault("Y", NA, "numeric", errorIfInvalid=TRUE)
+    z <- getWithDefault("Z", NA, "numeric", errorIfInvalid=TRUE)
+    
+    createSliceGraphic(image, x, y, z, device="png", file=outputFile)
+}
