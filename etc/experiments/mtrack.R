6cc3ce9db91cda68eb3b9c2b0f2fb28d638ba6bb
Jon Clayden <jon.clayden@gmail.com>
2007-12-12 14:35:01 +0000

    Added mtrack expt
    git-svn-id: https://tractor.googlecode.com/svn/trunk@75 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/mtrack.R
@@ -0,0 +1,47 @@
+require(tractor.fsl)
+
+runExperiment <- function ()
+{
+    requireArguments(1)
+    session <- newSessionFromDirectory(Arguments[1])
+    
+    nSamples <- getWithDefault("NumberOfSamples", 5000)
+    
+    createImages <- getWithDefault("CreateImages", FALSE)
+    tractName <- getWithDefault("TractName", "tract")
+    vizThreshold <- getWithDefault("VisualisationThreshold", 0.01)
+    showSeed <- getWithDefault("ShowSeedPoint", TRUE)
+    
+    seedMaskFile <- getWithDefault("SeedMaskFile", NULL, "character", errorIfInvalid=TRUE, errorIfMissing=TRUE)
+    seedMaskInStandardSpace <- getWithDefault("SeedMaskInStandardSpace", FALSE)
+    waypointMaskFiles <- getWithDefault("WaypointMaskFiles", NULL, "character", errorIfInvalid=TRUE)
+    waypointMasksInStandardSpace <- getWithDefault("WaypointMasksInStandardSpace", FALSE)
+    
+    seedMask <- newMriImageFromFile(seedMaskFile)
+    if (seedMaskInStandardSpace)
+        seedMask <- transformStandardSpaceImage(session, seedMask)
+    
+    if (is.null(waypointMaskFiles))
+        result <- runProbtrackWithSession(session, mode="seedmask", seedMask=seedMask, requireImage=createImages, nSamples=nSamples)
+    else
+    {
+        waypointMasks <- list()
+        for (waypointFile in waypointMaskFiles)
+        {
+            waypointMask <- newMriImageFromFile(waypointFile)
+            if (waypointMasksInStandardSpace)
+                waypointMask <- transformStandardSpaceImage(session, waypointMask)
+            waypointMasks <- c(waypointMasks, list(waypointMask))
+        }
+        
+        result <- runProbtrackWithSession(session, mode="waypoints", seedMask=seedMask, waypointMasks=waypointMasks, requireImage=createImages, nSamples=nSamples)
+    }
+    
+    if (createImages)
+    {
+        output(OL$Info, "Creating tract images")
+        writePngsForResult(result, prefix=tractName, threshold=vizThreshold, showSeed=showSeed)
+    }
+    
+    invisible (NULL)
+}
