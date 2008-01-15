8febf0e197c13fa49800214f567012be62e80064
Jon Clayden <jon.clayden@gmail.com>
2008-01-15 15:16:49 +0000

    pnt-train expt uses output from pnt-data now
    git-svn-id: https://tractor.googlecode.com/svn/trunk@113 fd513b17-773f-0410-bea1-c334db35fb47

--- etc/experiments/pnt-train.R
+++ etc/experiments/pnt-train.R
@@ -1,43 +1,19 @@
-#@desc Create a matching model for probabilistic neighbourhood tractography from a set
-#@desc of B-spline tracts using a list of seed points and scan sessions, plus a refer-
-#@desc ence tract, which must already exist
-
 suppressPackageStartupMessages(require(tractor.fsl))
 suppressPackageStartupMessages(require(tractor.nt))
 
 runExperiment <- function ()
 {
     refTractFile <- getWithDefault("ReferenceTractFile", NULL, "character", errorIfMissing=TRUE)
-    sessionList <- getWithDefault("SessionList", NULL, "character", errorIfMissing=TRUE)
-    seedList <- getWithDefault("SeedPointList", NULL, "integer", errorIfMissing=TRUE)
-    seedMatrix <- matrix(seedList, ncol=3, byrow=TRUE)
-    pointType <- getWithDefault("PointType", NULL, mode="character", errorIfMissing=TRUE)
-    pointType <- match.arg(tolower(pointType), c("fsl","r","mm"))
+    dataFile <- getWithDefault("DatasetFile", NULL, "character", errorIfMissing=TRUE)
     maxKnotCount <- getWithDefault("MaximumKnotCount", NULL, "integer")
     modelName <- getWithDefault("ModelName", "model")
     
-    if (pointType == "fsl")
-        seedMatrix <- transformFslVoxelToRVoxel(seedMatrix)
-    
     if (!all(c("seed","spline","options") %in% load(refTractFile)))
         output(OL$Error, "The file specified does not seem to contain reference tract information")
-    if (!exists("refSession"))
-        refSession <- NULL
-    
-    trainingSplines <- list()
-    for (i in seq_along(sessionList))
-    {
-        currentSession <- newSessionFromDirectory(sessionList[i])
-        currentSeed <- seedMatrix[i,]
-        if (pointType == "mm")
-            currentSeed <- transformWorldToRVoxel(currentSeed, newMriImageMetadataFromFile(currentSession$getImageFileNameByType("t2")), useOrigin=TRUE)
-        currentSpline <- splineTractWithOptions(options, currentSession, currentSeed, refSession)
-        trainingSplines <- c(trainingSplines, list(currentSpline))
-    }
-    
-    data <- createDataTableForSplines(trainingSplines, spline, "knot")    
-    write.table(data, file=paste(modelName,"_data.txt",sep=""))
+
+    data <- read.table(dataFile)
     model <- newMatchingTractModelFromDataTable(data, spline, maxLength=maxKnotCount)
-    save(model, file=paste(modelName,"Rdata",sep="."))
+    
+    save(model, file=ensureFileSuffix(modelName,"Rdata"))
     model$summarise()
 }
