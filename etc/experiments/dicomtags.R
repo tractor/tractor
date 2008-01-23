d23a5d2b036d83e4ff0aa047eea476f1a3c6bf18
Jon Clayden <jon.clayden@gmail.com>
2008-01-23 18:11:21 +0000

    dicomtags expt reports if it cannot read the image
    git-svn-id: https://tractor.googlecode.com/svn/trunk@119 fd513b17-773f-0410-bea1-c334db35fb47

--- etc/experiments/dicomtags.R
+++ etc/experiments/dicomtags.R
@@ -4,5 +4,9 @@ runExperiment <- function ()
 {
     requireArguments("file name")
     descriptions <- getWithDefault("Descriptions", TRUE)
-    print(newDicomMetadataFromFile(Arguments[1]), descriptions=descriptions)
+    metadata <- newDicomMetadataFromFile(Arguments[1])
+    if (is.null(metadata))
+        output(OL$Error, "File ", Arguments[1], " appears not to be a DICOM file or is unreadable")
+    else
+        print(metadata, descriptions=descriptions)
 }
