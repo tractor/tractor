a5acdf989a505187a36a31ee44b76bab78edd018
Jon Clayden <jon.clayden@gmail.com>
2008-04-29 11:40:18 +0000

    Added peek expt for inspecting serialised objects
    git-svn-id: https://tractor.googlecode.com/svn/trunk@249 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/peek.R
@@ -0,0 +1,35 @@
+#@args file name
+#@desc Attempt to deserialise the R object stored in the file name specified (to which
+#@desc the suffix ".Rdata" will be added if it's not already present). If successful,
+#@desc the class of the stored object will be displayed, along with a summary of the
+#@desc properties of the object if available.
+
+runExperiment <- function ()
+{
+    requireArguments("file name")
+    fileName <- ensureFileSuffix(Arguments[1], "Rdata")
+    
+    if (!file.exists(fileName))
+        output(OL$Error, "File ", fileName, " does not exist")
+    
+    setOutputLevel(OL$Info, FALSE)
+    
+    object <- deserialiseListObject(file=fileName, raw=TRUE)
+    if (!is.null(attr(object,"originalClass")))
+        output(OL$Info, "Object class: ", implode(attr(object,"originalClass"),", "))
+    
+    if (isDeserialisable(object, "metatract.reference"))
+        object <- deserialiseReferenceTract(NULL, object)
+    else if (isDeserialisable(object, "results.nt.heuristic"))
+        object <- deserialiseHeuristicNTResults(NULL, object)
+    else if (isDeserialisable(object, "results.nt.probabilistic"))
+        object <- deserialiseProbabilisticNTResults(NULL, object)
+    else if (isDeserialisable(object, "model.tract.matching"))
+        object <- deserialiseMatchingTractModel(NULL, object)
+    
+    if (!is.null(object$summarise))
+    {
+        cat("---\n")
+        object$summarise()
+    }
+}
