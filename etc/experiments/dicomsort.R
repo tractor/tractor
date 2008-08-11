dcafe38ecb2577965cda8e1b97b0ca0ef86c6d49
Jon Clayden <jon.clayden@gmail.com>
2008-08-11 16:49:44 +0000

    Moved in two utility scripts
    git-svn-id: https://tractor.googlecode.com/svn/trunk@280 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/dicomsort.R
@@ -0,0 +1,66 @@
+#@args [directory]
+#@desc Sort a directory containing DICOM files (default ".") into subdirectories by
+#@desc series number (as given in DICOM tag 0020,0011). The original files will not be
+#@desc deleted or otherwise moved unless DeleteOriginals:true is given.
+
+runExperiment <- function ()
+{
+    if (nArguments() == 0)
+        directory <- "."
+    else
+        directory <- expandFileName(Arguments[1])
+    
+    deleteOriginals <- getWithDefault("DeleteOriginals", FALSE)
+    
+    files <- expandFileName(list.files(directory, full.names=TRUE, recursive=TRUE))
+    files <- files[!file.info(files)$isdir]
+    nFiles <- length(files)
+
+    count <- 0
+    seriesNumbers <- numeric(nFiles)
+    
+    output(OL$Info, "Reading series numbers from ", nFiles, " files")
+    for (i in 1:nFiles)
+    {
+        metadata <- try(newDicomMetadataFromFile(files[i]), silent=TRUE)
+        if (is.null(metadata) || ("try-error" %in% class(metadata)))
+        {
+            output(OL$Info, "Skipping ", files[i])
+            seriesNumbers[i] <- NA
+        }
+        else
+        {
+            seriesNumbers[i] <- metadata$getTagValue(0x0020, 0x0011)
+            count <- count + 1
+            if (count %% 100 == 0)
+                output(OL$Verbose, "Done ", count)
+        }
+    }
+
+    nDicomFiles <- count
+    if (nDicomFiles == 0)
+        output(OL$Error, "No readable DICOM files were found")
+
+    uniqueSeries <- sort(unique(seriesNumbers))
+    output(OL$Info, "Found series ", implode(uniqueSeries,", "), "; creating subdirectories")
+    
+    for (series in uniqueSeries)
+    {
+        dir.create(file.path(directory, series))
+        
+        matchingFiles <- which(seriesNumbers==series)
+        if (length(matchingFiles) > 0)
+        {
+            metadata <- newDicomMetadataFromFile(files[matchingFiles[1]])
+            description <- metadata$getTagValue(0x0008, 0x103e)
+            output(OL$Info, "Series ", series, " includes ", length(matchingFiles), " files; description is \"", description, "\"")
+            
+            success <- file.copy(files[matchingFiles], file.path(directory,series,basename(files[matchingFiles])))
+
+            if (!all(success))
+                output(OL$Warning, "Not all files copied successfully for series ", series, " - nothing will be deleted")
+            else if (deleteOriginals)
+                unlink(files[matchingFiles])
+        }
+    }
+}
---
+++ etc/experiments/mkbvecs.R
@@ -0,0 +1,97 @@
+#@args session directory, text file, large b-value, [small b-value]
+#@desc Create the "bvals" and "bvecs" files required for preprocessing from a text
+#@desc file, which should contain the diffusion gradient vectors applied to the
+#@desc data set. This script is considerably more flexible than FSL-FDT, allowing
+#@desc for the gradient vectors to be given either one-per-column or one-per-row,
+#@desc normalised or unnormalised, and with or without zeroes for b=0 measurements.
+#@desc The small b-value is assumed to be zero if it is not explicitly specified.
+
+suppressPackageStartupMessages(require(tractor.fsl))
+
+runExperiment <- function ()
+{
+    requireArguments("session directory", "text file", "large b-value")
+    session <- newSessionFromDirectory(Arguments[1])
+    fileName <- Arguments[2]
+    largeBValue <- as.numeric(Arguments[3])
+    
+    if (nArguments() > 3)
+        smallBValue <- as.numeric(Arguments[4])
+    else
+        smallBValue <- 0
+    
+    targetDir <- session$getPreBedpostDirectory()
+    metadata <- newMriImageMetadataFromFile(file.path(targetDir,"basic"))
+    
+    bvecs <- as.matrix(read.table(fileName))
+    if (nrow(bvecs) != 3)
+    {
+        if (ncol(bvecs) == 3)
+        {
+            output(OL$Info, "Transposing gradient vector matrix")
+            bvecs <- t(bvecs)
+        }
+        else
+            output(OL$Error, "The gradient vector matrix must have one dimension equal to 3")
+    }
+    
+    nVectorsFile <- ncol(bvecs)
+    nVectorsImage <- metadata$getDimensions()[4]
+    output(OL$Info, "Gradient vector matrix has ", nVectorsFile, " columns; data image contains ", nVectorsImage, " volumes")
+    
+    bvals <- rep(NA, nVectorsImage)
+    
+    if (nVectorsFile == nVectorsImage && smallBValue == 0)
+    {
+        lowBValueIndices <- which(bvecs[1,]==0 & bvecs[2,]==0 & bvecs[3,]==0)
+        highBValueIndices <- setdiff(1:nVectorsFile, lowBValueIndices)
+    }
+    else
+    {
+        output(OL$Info, "Loading data image for more information")
+        dataImage <- newMriImageFromFile(file.path(targetDir,"basic"))
+        
+        meanIntensities <- numeric(nVectorsImage)
+        for (i in seq_len(nVectorsImage))
+            meanIntensities[i] <- mean(dataImage[,,,i])
+        
+        kMeansResult <- kmeans(meanIntensities, 2, nstart=3)
+        
+        if (nVectorsFile != nVectorsImage)
+        {
+            if (!(nVectorsFile %in% kMeansResult$size))
+                output(OL$Error, "K-means cluster sizes are ", kMeansResult$size[1], " and ", kMeansResult$size[2], " - neither matches your text file")
+            else
+            {
+                highBValueCluster <- which(kMeansResult$size == nVectorsFile)
+                lowBValueCluster <- ifelse(highBValueCluster==1, 2, 1)
+                if (kMeansResult$centers[highBValueCluster] > kMeansResult$centers[lowBValueCluster])
+                    output(OL$Warning, "High b-value images seem to have higher mean signal")
+            }
+        }
+        else
+        {
+            highBValueCluster <- which.min(kMeansResult$centers)
+            lowBValueCluster <- which.max(kMeansResult$centers)
+        }
+        
+        highBValueIndices <- which(kMeansResult$cluster == highBValueCluster)
+        lowBValueIndices <- which(kMeansResult$cluster == lowBValueCluster)
+        output(OL$Info, "Image volume(s) ", implode(lowBValueIndices,sep=", ",finalSep=" and "), " appear(s) to have lower diffusion weighting")
+        
+        if (nVectorsFile != nVectorsImage)
+        {
+            newBVecs <- matrix(NA, nrow=3, ncol=nVectorsImage)
+            newBVecs[,highBValueIndices] <- bvecs
+            newBVecs[,lowBValueIndices] <- 0
+            bvecs <- newBVecs
+        }
+    }
+    
+    bvals[highBValueIndices] <- largeBValue
+    bvals[lowBValueIndices] <- smallBValue
+    
+    output(OL$Info, "Writing bvals and bvecs files")
+    write.table(bvecs, file.path(targetDir,"bvecs"), row.names=FALSE, col.names=FALSE)
+    write.table(matrix(bvals,nrow=1), file.path(targetDir,"bvals"), row.names=FALSE, col.names=FALSE)
+}
