3b8008970e445de5f14bb33cd8333fe31ea880fe
Jon Clayden <jon.clayden@gmail.com>
2008-08-11 14:58:23 +0000

    Added caminofiles expt
    git-svn-id: https://tractor.googlecode.com/svn/trunk@279 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/caminofiles.R
@@ -0,0 +1,23 @@
+#@args [session directory]
+#@desc Creates files required by the Camino diffusion toolkit under the specified
+#@desc session directory (or "." if none is given). A scheme file will be created,
+#@desc which describes the diffusion weighting applied to the data set, and the
+#@desc data images and brain mask will be written using the file formats that
+#@desc Camino recognises. All this information is copied from the FSL directory,
+#@desc so preprocessing with the "preproc" script must be completed before running
+#@desc this one. The script will prompt for the diffusion time (in seconds) of the
+#@desc data set if it is not specified using the DiffusionTime option.
+
+suppressPackageStartupMessages(require(tractor.camino))
+
+runExperiment <- function ()
+{
+    if (nArguments() == 0)
+        session <- newSessionFromDirectory(".")
+    else
+        session <- newSessionFromDirectory(Arguments[1])
+    
+    diffusionTime <- getWithDefault("DiffusionTime", NULL, "numeric")
+    
+    createCaminoFilesForSession(session, diffusionTime)
+}
