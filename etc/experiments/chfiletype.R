c4770da3603a5c04cc6555bda35a61ce3e0cb243
Jon Clayden <jon.clayden@gmail.com>
2008-09-08 17:39:41 +0000

    chfiletype expt now supports a TypeCode option for changing file data types
    git-svn-id: https://tractor.googlecode.com/svn/trunk@297 fd513b17-773f-0410-bea1-c334db35fb47

--- etc/experiments/chfiletype.R
+++ etc/experiments/chfiletype.R
@@ -4,21 +4,27 @@
 #@desc single-file NIfTI), "NIFTI_PAIR" (NIfTI header/image pair), or any of these
 #@desc with "_GZ" appended for the equivalent formats compressed with gzip(1). If
 #@desc the format is not specified, it will be taken from the TRACTOR_FILETYPE
-#@desc environment variable.
+#@desc environment variable. An Analyze/NIfTI data type code can also be specified
+#@desc with the TypeCode option, perhaps for compatibility with other software.
 
 library(tractor.base)
 
 runExperiment <- function ()
 {
     requireArguments("image file")
+
+    datatype <- getWithDefault("TypeCode", NULL, "integer")
+    if (!is.null(datatype))
+        datatype <- getDataTypeByNiftiCode(datatype)
+    
     image <- newMriImageFromFile(Arguments[1])
     
     if (nArguments() > 1)
-        writeMriImageToFile(image, fileType=toupper(Arguments[2]))
+        writeMriImageToFile(image, fileType=toupper(Arguments[2]), datatype=datatype)
     else if (is.null(getOption("tractorFileType")))
         output(OL$Error, "No default file type is available - format must be given")
     else
-        writeMriImageToFile(image, fileType=getOption("tractorFileType"))
+        writeMriImageToFile(image, fileType=getOption("tractorFileType"), datatype=datatype)
     
     invisible(NULL)
 }
