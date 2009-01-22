3d27be3a666b984929c3c78ceaa40ca765198068
Jon Clayden <jon.clayden@gmail.com>
2009-01-22 12:46:50 +0000

    Added dicomread script
    git-svn-id: https://tractor.googlecode.com/svn/trunk@352 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ etc/experiments/dicomread.R
@@ -0,0 +1,16 @@
+#@args directory, output image name
+#@desc Create an Analyze/NIfTI volume by combining data from a set of DICOM files
+#@desc stored in the specified directory. A 3D or 4D image will be created, as
+#@desc appropriate, using the specified output name.
+
+library(tractor.base)
+
+runExperiment <- function ()
+{
+    requireArguments("directory", "output image name")
+    
+    info <- newMriImageFromDicomDirectory(Arguments[1])
+    reportFlags()
+    writeMriImageToFile(info$image, Arguments[2])
+    info$image$summarise()
+}
