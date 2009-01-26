cd469cdcfe6072da2f57a7d4bf2b4f4d86feae4f
Jon Clayden <jon.clayden@gmail.com>
2009-01-26 17:47:54 +0000

    Added functions for finding NT-related resources
    git-svn-id: https://tractor.googlecode.com/svn/trunk@366 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tractor.nt/R/resources.R
@@ -0,0 +1,86 @@
+getFileNameForNTResource <- function (type, mode, options, expectExists = FALSE)
+{
+    type <- tolower(type)
+    mode <- tolower(mode)
+    
+    if (!(mode %in% c("hnt","pnt")))
+        output(OL$Error, "Mode must be \"hnt\" or \"pnt\"")
+    
+    validTypes <- list(hnt=c("reference","results"), pnt=c("reference","model","results"))
+    if (!(type %in% validTypes[[mode]]))
+        output(OL$Error, "The specified resource type (\"", type, "\") is not valid in this mode")
+    
+    tractorHome <- Sys.getenv("TRACTOR_HOME")
+    if (tractorHome == "" || !file.exists(tractorHome))
+        standardRefTractDir <- NULL
+    else
+        standardRefTractDir <- file.path(tractorHome, "share", "reftracts", mode)
+    
+    if (type == "reference")
+    {
+        if (!("tractName" %in% names(options)))
+            output(OL$Error, "Tract name must be specified")
+            
+        fileName <- ensureFileSuffix(paste(options$tractName,"ref",sep="_"), "Rdata")
+        if (!expectExists || file.exists(fileName))
+            return (fileName)
+        else if (file.exists(file.path(standardRefTractDir, fileName)))
+            return (file.path(standardRefTractDir, fileName))
+        else
+            output(OL$Error, "No reference for tract name \"", options$tractName, "\" was found")
+    }
+    else if (type == "results")
+    {
+        if (!("resultsName" %in% names(options)))
+            output(OL$Error, "Results name must be specified")
+        
+        fileName <- ensureFileSuffix(options$resultsName, "Rdata")
+        if (!expectExists || file.exists(fileName))
+            return (fileName)
+        else
+            output(OL$Error, "No results file with name \"", options$resultsName, "\" was found")
+    }
+    else if (type == "model")
+    {
+        if (!any(c("datasetName","tractName") %in% names(options)))
+            output(OL$Error, "Dataset and tract names must be specified")
+        
+        fileName <- ensureFileSuffix(paste(options$datasetName,"model",sep="_"), "Rdata")
+        if (!expectExists || file.exists(fileName))
+            return (fileName)
+        
+        fileName <- ensureFileSuffix(paste(options$tractName,"model",sep="_"), "Rdata")
+        if (file.exists(fileName))
+            return (fileName)
+        else
+            output(OL$Error, "No suitable model was found")
+    }
+}
+
+getNTResource <- function (type, mode, options)
+{
+    type <- tolower(type)
+    mode <- tolower(mode)
+    
+    fileName <- getFileNameForNTResource(type, mode, options, expectExists=TRUE)
+    
+    if (type == "reference")
+    {
+        reference <- deserialiseReferenceTract(fileName)
+        if ((mode == "hnt" && !isFieldTract(reference)) || (mode == "pnt" && !isBSplineTract(reference)))
+            output(OL$Error, "The specified reference tract is not in the correct form")
+        else
+            return (invisible(reference))
+    }
+    else if (type == "results")
+    {
+        deserialiseFunction <- ifelse(mode=="hnt", deserialiseHeuristicNTResults, deserialiseProbabilisticNTResults)
+        results <- deserialiseFunction(fileName)
+        return (invisible(results))
+    }
+    else if (type == "model")
+    {
+        model <- deserialiseMatchingTractModel(fileName)
+        return (invisible(model))
+    }
+}
