eba55ca15eeca2412cd5312b2e5efccd3bfeafc9
Jon Clayden <jon.clayden@gmail.com>
2009-02-11 17:29:55 +0000

    Added list expt
    git-svn-id: https://tractor.googlecode.com/svn/trunk@376 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ share/experiments/list.R
@@ -0,0 +1,18 @@
+#@desc List all available TractoR experiments
+
+library(tractor.base)
+
+runExperiment <- function ()
+{
+    pathDirs <- file.path(Sys.getenv("TRACTOR_HOME"), "share", "experiments")
+    pathDirs <- c(pathDirs, unlist(strsplit(Sys.getenv("TRACTOR_PATH"), ":", fixed=TRUE)))
+    
+    for (pathDir in pathDirs)
+    {
+        files <- list.files(pathDir, "\\.R$")
+        filesWithoutExtensions <- ensureFileSuffix(files, NULL, strip="R")
+        cat(paste("Experiment scripts found in ", pathDir, ":\n", sep=""))
+        print(noquote(filesWithoutExtensions))
+        cat("\n")
+    }
+}
