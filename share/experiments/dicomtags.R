c803f75b8633de3819e68eff80b768d959b518f6
Jon Clayden <jon.clayden@gmail.com>
2009-08-19 17:14:48 +0000

    The dicomtags expt now accepts a directory name (default .) instead of a file name, in which case it will look for a DICOM file in that directory and print the tags for that file
    git-svn-id: https://tractor.googlecode.com/svn/trunk@453 fd513b17-773f-0410-bea1-c334db35fb47

--- share/experiments/dicomtags.R
+++ share/experiments/dicomtags.R
@@ -1,18 +1,39 @@
-#@args file name
-#@desc Produce a full list of all the DICOM tags contained in the specified file.
-#@desc Descriptions of the tag, rather than its DICOM group and element numbers, will
-#@desc be given unless Descriptions:false is specified.
+#@args [file name]
+#@desc Produce a full list of all the DICOM tags contained in the specified file. If the name given corresponds to a directory then the tags for the first DICOM file found in that directory will be shown. If the file name is missing the current directory will be used. Descriptions of the tag, rather than its DICOM group and element numbers, will be given unless Descriptions:false is specified.
 
 library(tractor.base)
 
 runExperiment <- function ()
 {
-    requireArguments("file name")
     descriptions <- getWithDefault("Descriptions", TRUE)
-    fileName <- implode(Arguments, sep=" ")
-    metadata <- newDicomMetadataFromFile(fileName)
-    if (is.null(metadata))
-        output(OL$Error, "File ", fileName, " appears not to be a DICOM file or is unreadable")
+    
+    if (nArguments() > 0)
+        fileName <- implode(Arguments, sep=" ")
     else
-        print(metadata, descriptions=descriptions)
+        fileName <- "."
+    
+    if (file.info(fileName)$isdir)
+    {
+        allFileNames <- list.files(fileName, full.names=TRUE)
+        allFileNames <- allFileNames[!file.info(allFileNames)$isdir]
+        for (currentFileName in allFileNames)
+        {
+            metadata <- newDicomMetadataFromFile(currentFileName)
+            if (!is.null(metadata))
+            {
+                output(OL$Info, "Displaying tags for file \"", currentFileName, "\"")
+                break
+            }
+        }
+        if (!exists("metadata") || is.null(metadata))
+            output(OL$Error, "The directory \"", fileName, "\" appears to contain no readable DICOM files")
+    }
+    else
+    {
+        metadata <- newDicomMetadataFromFile(fileName)
+        if (is.null(metadata))
+            output(OL$Error, "The file \"", fileName, "\" appears not to be a DICOM file or is unreadable")
+    }
+    
+    print(metadata, descriptions=descriptions)
 }
