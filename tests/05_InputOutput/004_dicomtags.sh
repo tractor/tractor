c976101a1c2c48d06fdaa390b786d0d022c5ac6a
Jon Clayden <jon.clayden@gmail.com>
2009-09-15 15:38:31 +0000

    Added new DICOM test; relabelled old one slightly
    git-svn-id: https://tractor.googlecode.com/svn/trunk@491 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tests/05_InputOutput/004_dicomtags.sh
@@ -0,0 +1,2 @@
+#@desc Checking that DICOM reader works
+${TRACTOR} dicomtags data/dicom/01.dcm
--- tests/05_InputOutput/005_dicomread.sh
+++ tests/05_InputOutput/005_dicomread.sh
@@ -1,4 +1,4 @@
-#@desc Checking DICOM file sorting and reading
+#@desc Checking DICOM file sorting and reconstruction
 ${TRACTOR} -v1 dicomsort data/dicom && mv data/dicom/8 data/dicom/9 tmp/
 ${TRACTOR} -v1 -w tmp dicomread 9 t1 | grep -v t1
 ${TRACTOR} value tmp/t1 1 145 139
