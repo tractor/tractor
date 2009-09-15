a12a204be48104d1dbf39a39324b9b07002153f5
Jon Clayden <jon.clayden@gmail.com>
2009-09-15 14:47:04 +0000

    Added DICOM test and data
    git-svn-id: https://tractor.googlecode.com/svn/trunk@489 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tests/05_InputOutput/005_dicom.sh
@@ -0,0 +1,5 @@
+#@desc Checking DICOM file sorting and reading
+${TRACTOR} -v1 dicomsort data/dicom && mv data/dicom/8 data/dicom/9 tmp/
+${TRACTOR} -v1 -w tmp dicomread 9 t1 | grep -v t1
+${TRACTOR} value tmp/t1 1 145 139
+rm -rf tmp/8 tmp/9 tmp/t1.*
