05ef375d544988e692fc3768365661a70ba1b278
Jon Clayden <jon.clayden@gmail.com>
2009-09-15 16:58:55 +0000

    More tests; typo correction
    git-svn-id: https://tractor.googlecode.com/svn/trunk@495 fd513b17-773f-0410-bea1-c334db35fb47

--- tests/15_FSL/010_track.sh
+++ tests/15_FSL/010_track.sh
@@ -1,4 +1,4 @@
 #@desc Checking that we can run FSL tractography
 ${TRACTOR} track data/session-12dir 49 58 14 PointType:R NumberOfSamples:50 TractName:tmp/tract CreateVolumes:true Force:true
 ${TRACTOR} value tmp/tract 49 58 14
-rm -r tmp/tract*
+rm -f tmp/tract*
---
+++ tests/15_FSL/015_mtrack.sh
@@ -0,0 +1,5 @@
+#@desc Checking that ROI-based tractography works
+${TRACTOR} mkroi data/session-12dir 49 58 14 PointType:R Width:1 ROIName:tmp/region
+${TRACTOR} mtrack data/session-12dir SeedMaskFile:tmp/region NumberOfSamples:50 TractName:tmp/tract CreateVolumes:true
+${TRACTOR} value tmp/tract 49 58 14
+rm -f tmp/tract* tmp/region.*
