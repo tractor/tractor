05048430587012478444084a84658a34f7981371
Jon Clayden <jon.clayden@gmail.com>
2010-02-08 12:04:50 +0000

    Updated caminofiles expt description to reflect removal of DiffusionTime option
    git-svn-id: https://tractor.googlecode.com/svn/trunk@557 fd513b17-773f-0410-bea1-c334db35fb47

--- share/experiments/caminofiles.R
+++ share/experiments/caminofiles.R
@@ -1,12 +1,5 @@
 #@args [session directory]
-#@desc Creates files required by the Camino diffusion toolkit under the specified
-#@desc session directory (or "." if none is given). A scheme file will be created,
-#@desc which describes the diffusion weighting applied to the data set, and the
-#@desc data images and brain mask will be written using the file formats that
-#@desc Camino recognises. All this information is copied from the FSL directory,
-#@desc so preprocessing with the "preproc" script must be completed before running
-#@desc this one. The script will prompt for the diffusion time (in seconds) of the
-#@desc data set if it is not specified using the DiffusionTime option.
+#@desc Creates files required by the Camino diffusion toolkit under the specified session directory (or "." if none is given). A scheme file will be created, which describes the diffusion weighting applied to the data set, and the data images and brain mask will be written using the file formats that Camino recognises. All this information is copied from the FSL directory, so preprocessing with the "preproc" script (up to at least stage 3) must be completed before running this one.
 #@interactive TRUE
 
 suppressPackageStartupMessages(require(tractor.session))
