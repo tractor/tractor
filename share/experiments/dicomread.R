db627f6d87ba2cefb9121cef574bcf7f946e8780
Jon Clayden <jon.clayden@gmail.com>
2010-02-08 14:52:52 +0000

    Output file name defaults to the same as the dir name in dicomread expt
    git-svn-id: https://tractor.googlecode.com/svn/trunk@559 fd513b17-773f-0410-bea1-c334db35fb47

--- share/experiments/dicomread.R
+++ share/experiments/dicomread.R
@@ -1,17 +1,19 @@
-#@args directory, output image name
-#@desc Create an Analyze/NIfTI volume by combining data from a set of DICOM files
-#@desc stored in the specified directory. A 3D or 4D image will be created, as
-#@desc appropriate, using the specified output name.
+#@args directory, [output image name]
+#@desc Create an Analyze/NIfTI volume by combining data from a set of DICOM files stored in the specified directory. A 3D or 4D image will be created, as appropriate, using the specified output name (or the same name as the directory if the second argument is missing).
 
 library(tractor.base)
-library(tractor.session)
 
 runExperiment <- function ()
 {
-    requireArguments("directory", "output image name")
+    requireArguments("directory")
+    
+    if (nArguments() > 1)
+        fileName <- Arguments[2]
+    else
+        fileName <- Arguments[1]
     
     info <- newMriImageFromDicomDirectory(Arguments[1])
     reportFlags()
-    writeMriImageToFile(info$image, Arguments[2])
+    writeMriImageToFile(info$image, fileName)
     info$image$summarise()
 }
