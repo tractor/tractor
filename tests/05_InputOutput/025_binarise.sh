7ea17f16aac2788e2073537de57cf62c019542bd
Jon Clayden <jon.clayden@gmail.com>
2010-03-11 11:48:13 +0000

    Added binarise test
    git-svn-id: https://tractor.googlecode.com/svn/trunk@589 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tests/05_InputOutput/025_binarise.sh
@@ -0,0 +1,6 @@
+#@desc Checking that image binarising works
+${TRACTOR} binarise data/session-12dir/tractor/fdt/dti_FA tmp/dti_FA_binarised ThresholdLevel:0.5 ThresholdRelativeTo:nothing
+${TRACTOR} value tmp/dti_FA_binarised 54 50 13
+${TRACTOR} binarise data/session-12dir/tractor/fdt/dti_FA tmp/dti_FA_binarised ThresholdLevel:0.5 ThresholdRelativeTo:max
+${TRACTOR} value tmp/dti_FA_binarised 54 50 13
+rm -f tmp/dti_FA_binarised.*
