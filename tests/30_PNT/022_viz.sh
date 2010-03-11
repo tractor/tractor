5947a36121891742ef1310e6798c95fd43048a37
Jon Clayden <jon.clayden@gmail.com>
2010-03-11 12:07:56 +0000

    Added pnt-viz/gmap test
    git-svn-id: https://tractor.googlecode.com/svn/trunk@590 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tests/30_PNT/022_viz.sh
@@ -0,0 +1,8 @@
+#@desc Checking visualisation and group mapping
+${TRACTOR} pnt-eval DatasetName:data/pnt/pnt_test_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results
+${TRACTOR} pnt-viz TractName:genu ResultsName:tmp/pnt_results SessionList:data/session-12dir CreateVolumes:true CreateImages:false
+${TRACTOR} gmap genu_session SessionList:data/session-12dir TractName:genu ThresholdLevel:0.01 ThresholdRelativeTo:max
+${TRACTOR} value genu_group_map 46 78 42
+rm -f tmp/pnt_results.Rdata
+rm -f genu_session1.*
+rm -f genu_group_map.*
