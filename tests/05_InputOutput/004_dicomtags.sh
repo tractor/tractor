f824d6fcd927e9deaae03bc6effac8e9568fb81b
Jon Clayden <jon.clayden@gmail.com>
2010-05-19 09:32:13 +0000

    Updating test for compatibility with r616
    git-svn-id: https://tractor.googlecode.com/svn/trunk@623 fd513b17-773f-0410-bea1-c334db35fb47

--- tests/05_InputOutput/004_dicomtags.sh
+++ tests/05_InputOutput/004_dicomtags.sh
@@ -1,2 +1,2 @@
 #@desc Checking that DICOM reader works
-${TRACTOR} dicomtags data/dicom/01.dcm
+${TRACTOR} dicomtags data/dicom/01.dcm | grep -v -E '(locale|Referenced Image Sequence|0x0088, 0x0200)'
