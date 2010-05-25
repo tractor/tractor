c4a1fcf8ad46069b1ea8175648971d15cb6568e1
Jon Clayden <jon.clayden@gmail.com>
2010-05-25 11:30:02 +0000

    Moving code into tractor.utils
    git-svn-id: https://tractor.googlecode.com/svn/trunk@630 fd513b17-773f-0410-bea1-c334db35fb47

---
+++ tractor.utils/R/misc.R
@@ -0,0 +1,57 @@
+insertRowAt <- function (index, x, rowData)
+{
+    x <- promote(x)
+    end <- nrow(x)
+    row <- matrix(rowData, nrow=1)
+    
+    if (index == 1)
+        result <- rbind(row, x)
+    else if (index == end+1)
+        result <- rbind(x, row)
+    else if (index > 1 && index <= end)
+        result <- rbind(x[1:(index-1),,drop=FALSE], row, x[index:end,,drop=FALSE])
+    else
+        output(OL$Error, "Index (", index, ") is out of bounds")
+    
+    return (result)
+}
+
+insertColumnAt <- function (index, x, colData)
+{
+    x <- promote(x, byrow=TRUE)
+    end <- ncol(x)
+    col <- matrix(colData, ncol=1)
+    
+    if (index == 1)
+        result <- cbind(col, x)
+    else if (index == end+1)
+        result <- cbind(x, col)
+    else if (index > 1 && index <= end)
+        result <- cbind(x[,1:(index-1),drop=FALSE], col, x[,index:end,drop=FALSE])
+    else
+        output(OL$Error, "Index (", index, ") is out of bounds")
+    
+    return (result)
+}
+
+restrict <- function (x, fun = NULL, ..., invert = FALSE, na.rm=TRUE)
+{
+    if (!is.vector(x))
+        output(OL$Error, "The \"restrict\" function only works for vectors")
+    if (is.null(fun))
+        matches <- rep(TRUE, length(x))
+    else
+    {
+        fun <- match.fun(fun)
+        matches <- fun(x, ...)
+        if (!is.logical(matches))
+            output(OL$Error, "The result of applying the specified function to the vector is not of logical type")
+    }
+    
+    if (invert)
+        matches <- !matches
+    if (na.rm)
+        matches <- matches & !is.na(x)
+    
+    return (x[matches])
+}
---
+++ tractor.utils/R/scripts.R
@@ -0,0 +1,103 @@
+splitAndConvertString <- function (string, split = "", mode = "character", errorIfInvalid = FALSE, allowRanges = TRUE, ...)
+{
+    values <- unlist(strsplit(string, split, ...))
+    if (allowRanges && mode=="integer")
+    {
+        values <- unlist(lapply(values, function (x) {
+            x <- sub("(\\d)-", "\\1:", x, perl=TRUE)
+            eval(parse(text=x))
+        }))
+    }
+    else
+        values <- suppressWarnings(as(values, mode))
+    
+    if (errorIfInvalid && any(is.na(values)))
+        output(OL$Error, "Specified list, \"", implode(string,sep=" "), "\", is not valid here")
+    else
+        return (values)
+}
+
+isValidAs <- function (value, mode)
+{
+    coercedValue <- suppressWarnings(as(value, mode))
+    return (!any(is.na(coercedValue)))
+}
+
+getWithDefault <- function (name, defaultValue, mode = NULL, errorIfMissing = FALSE, errorIfInvalid = FALSE, validValues = NULL)
+{
+    reportInvalid <- function ()
+    {
+        level <- ifelse(errorIfInvalid, OL$Error, OL$Warning)
+        message <- paste("The configuration variable \"", name, "\" does not have a suitable and unambiguous value", ifelse(errorIfInvalid,""," - using default"), sep="")
+        output(level, message)
+    }
+    
+    matchAgainstValidValues <- function (currentValue)
+    {
+        if (is.null(validValues))
+            return (currentValue)
+        else if (isTRUE(mode == "character"))
+            loc <- pmatch(tolower(currentValue), tolower(validValues), nomatch=0)
+        else
+            loc <- match(currentValue, validValues, nomatch=0)
+        
+        if (loc != 0)
+            return (validValues[loc])
+        else
+        {
+            reportInvalid()
+            return (defaultValue)
+        }
+    }
+    
+    if (is.null(mode) && !is.null(defaultValue))
+        mode <- mode(defaultValue)
+    
+    if (!exists(name))
+    {
+        if (errorIfMissing)
+            output(OL$Error, "The configuration variable \"", name, "\" must be specified")
+        else
+            return (defaultValue)
+    }
+    else
+    {
+        value <- get(name)
+        if (is.null(mode) || mode == "NULL")
+            return (matchAgainstValidValues(value))
+        else if (!isValidAs(value, mode))
+        {
+            reportInvalid()
+            return (defaultValue)
+        }
+        else
+        {
+            value <- as(value, mode)
+            return (matchAgainstValidValues(value))
+        }
+    }
+}
+
+nArguments <- function ()
+{
+    if (!exists("Arguments"))
+        return (0)
+    else
+        return (length(get("Arguments")))
+}
+
+requireArguments <- function (...)
+{
+    args <- c(...)
+    
+    if (is.numeric(args) && nArguments() < args)
+        output(OL$Error, "At least ", args, " argument(s) must be specified")
+    else if (is.character(args) && nArguments() < length(args))
+        output(OL$Error, "At least ", length(args), " argument(s) must be specified: ", implode(args,", "))
+}
+
+producesError <- function (expr, silent = TRUE)
+{
+    returnValue <- try(expr, silent)
+    return ("try-error" %in% class(returnValue))
+}
