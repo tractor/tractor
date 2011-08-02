ae0e0221551aac77243d0acb39ff3f2d5d02c736
Jon Clayden <jon.clayden@gmail.com>
2011-08-02 10:25:03 +0000

    Revamp of the status script
    git-svn-id: https://tractor.googlecode.com/svn/trunk@980 fd513b17-773f-0410-bea1-c334db35fb47

--- share/experiments/status.R
+++ share/experiments/status.R
@@ -1,5 +1,5 @@
 #@args [session directory]
-#@desc Print the status of a session directory (default ".") with regard to preprocessing for FSL and Camino. Preprocessing stages relate to those used by the "preproc" script.
+#@desc Print the status of a session directory (default "."), including various information about what processes have been run on it.
 
 library(tractor.session)
 
@@ -7,15 +7,25 @@ runExperiment <- function ()
 {
     session <- newSessionFromDirectory(ifelse(nArguments()==0, ".", Arguments[1]))
     
-    stagesComplete <- c(imageFileExists(session$getImageFileNameByType("rawdata","diffusion")),
-                        imageFileExists(session$getImageFileNameByType("refb0","diffusion")),
-                        imageFileExists(session$getImageFileNameByType("mask","diffusion")),
-                        imageFileExists(session$getImageFileNameByType("fa","diffusion")))
-    
-    labels <- c("Session directory", "Working directory exists", "Preprocessing stages complete", "Number of fibres per voxel", "Camino files created")
-    values <- c(session$getDirectory(), file.exists(session$getDirectory("root")), implode(which(stagesComplete),","), getBedpostNumberOfFibresForSession(session), file.exists(file.path(session$getDirectory("camino"),"sequence.scheme")))
-    
     if (getOutputLevel() > OL$Info)
         setOutputLevel(OL$Info)
-    printLabelledValues(labels, values)
+    
+    report(OL$Info, "GENERAL:", prefixFormat="")
+    printLabelledValues(c("Session directory","Working directory exists"), c(session$getDirectory(),file.exists(session$getDirectory("root"))), leftJustify=TRUE)
+    
+    report(OL$Info, "\nDIFFUSION:", prefixFormat="")
+    labels <- c("Preprocessing complete", "Diffusion b-values", "Number of gradient directions", "Diffusion tensors fitted", "FSL BEDPOST run", "Camino files created")
+    if (is.na(nFibres <- getBedpostNumberOfFibresForSession(session)))
+        bedpostValue <- FALSE
+    else
+        bedpostValue <- paste("TRUE (", nFibres, " fibre(s) per voxel)", sep="")
+    if (is.null(scheme <- newSimpleDiffusionSchemeFromSession(session)))
+        bValues <- directions <- NA
+    else
+    {
+        bValues <- paste(implode(round(scheme$getBValues()),", "), "s/mm^2")
+        directions <- implode(scheme$nDirections(), ", ")
+    }
+    values <- c(imageFileExists(session$getImageFileNameByType("mask","diffusion")), bValues, directions, imageFileExists(session$getImageFileNameByType("fa","diffusion")), bedpostValue, file.exists(file.path(session$getDirectory("camino"),"sequence.scheme")))
+    printLabelledValues(labels, values, leftJustify=TRUE)
 }
