187e7434ef4896c1998046a121f31752327f6f46
Jon Clayden <jon.clayden@gmail.com>
2011-08-09 12:12:27 +0100

    Renaming LIA test image for consistency

--- tests/05_InputOutput/011_nifti_lia.sh
+++ tests/05_InputOutput/011_nifti_lia.sh
@@ -1,3 +1,3 @@
 #@desc Checking that NIfTI reader can handle nondiagonal xform matrices
-${TRACTOR} imageinfo data/nifti/nodif_brain_lia | grep -v source
-${TRACTOR} value data/nifti/nodif_brain_lia 49 58 14
+${TRACTOR} imageinfo data/nifti/maskedb0_lia | grep -v source
+${TRACTOR} value data/nifti/maskedb0_lia 49 58 14
