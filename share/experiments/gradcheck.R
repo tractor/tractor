38ad919d10be99627912ab36aadb6f0cf135bde4
Jon Clayden <jon.clayden@gmail.com>
2011-09-22 14:28:46 +0100

    Adding option to gradcheck to allow user to avoid using the internal viewer

--- share/experiments/gradcheck.R
+++ share/experiments/gradcheck.R
@@ -1,5 +1,5 @@
 #@args [session directory]
-#@desc Check the signs of the diffusion gradient directions associated with a session directory (default "."), flip them as necessary, and optionally update the gradient cache. A least-squares diffusion tensor fit is performed for one slice in each orientation, and the principal directions shown for the user to judge whether their orientations are appropriate.
+#@desc Check the signs of the diffusion gradient directions associated with a session directory (default "."), flip them as necessary, and optionally update the gradient cache. By default, a least-squares diffusion tensor fit is performed for one slice in each orientation, and the principal directions shown for the user to judge whether their orientations are appropriate.
 #@interactive TRUE
 
 library(tractor.session)
@@ -8,6 +8,7 @@ runExperiment <- function ()
 {
     session <- newSessionFromDirectory(ifelse(nArguments()==0, ".", Arguments[1]))
     
+    useInternalViewer <- getConfigVariable("UseInternalViewer", TRUE)
     useOrthographic <- getConfigVariable("UseOrthographicView", TRUE)
     faThreshold <- getConfigVariable("AnisotropyThreshold", 0.15)
     lineWidth <- getConfigVariable("LineWidth", 1.5)
@@ -19,61 +20,91 @@ runExperiment <- function ()
     maskImage <- session$getImageByType("mask", "diffusion")
     biggestLoc <- sapply(1:3, function (i) which.max(apply(maskImage$getData(),i,sum)))
     
-    repeat
+    if (useInternalViewer)
     {
-        if (useOrthographic)
-            layout(matrix(1:4,nrow=2,byrow=TRUE))
-        
-        scheme <- newSimpleDiffusionSchemeFromSession(session)
-        
-        for (i in 1:3)
-        {
-            inPlaneAxes <- setdiff(1:3, i)
-            point <- rep(NA, 3)
-            point[i] <- biggestLoc[i]
-            
-            createSliceGraphic(b0Image, point[1], point[2], point[3], device="internal")
-            
-            currentData <- extractDataFromMriImage(dataImage, i, point[i])
-            maskData <- extractDataFromMriImage(maskImage, i, point[i])
-            dim(currentData) <- c(prod(dim(currentData))/dim(currentData)[3], dim(currentData)[3])
-            currentData <- currentData[maskData>0,]
-            fit <- estimateDiffusionTensors(currentData, scheme, method="ls")
-            
-            fullEigenvectors <- fit$eigenvectors[,1,fit$fa>=faThreshold]
-            eigenvectors <- fullEigenvectors[inPlaneAxes,]
-            
-            dims <- maskImage$getDimensions()[inPlaneAxes]
-            locs <- which(maskData > 0, arr.ind=TRUE)
-            d1 <- locs[fit$fa>=faThreshold,1]
-            d2 <- locs[fit$fa>=faThreshold,2]
-            
-            col <- rgb(abs(fullEigenvectors[1,]), abs(fullEigenvectors[2,]), abs(fullEigenvectors[3,]))
-
-            segments((d1-1)/(dims[1]-1)-eigenvectors[1,]/(2*dims[1]), (d2-1)/(dims[2]-1)-eigenvectors[2,]/(2*dims[2]), (d1-1)/(dims[1]-1)+eigenvectors[1,]/(2*dims[1]), (d2-1)/(dims[2]-1)+eigenvectors[2,]/(2*dims[2]), lwd=lineWidth, col=col)
-        }
-        
         repeat
         {
-            done <- tolower(ask("Are the principal directions appropriate? [yn; s to show full images in fslview]"))
-            if (done %in% c("y","n"))
-                break
-            else if (done == "s")
+            if (useOrthographic)
+                layout(matrix(1:4,nrow=2,byrow=TRUE))
+
+            scheme <- newSimpleDiffusionSchemeFromSession(session)
+
+            for (i in 1:3)
             {
-                runDtifitWithSession(session)
-                showImagesInFslview(session$getImageFileNameByType("fa","diffusion"), session$getImageFileNameByType("eigenvector","diffusion",index=1))
+                inPlaneAxes <- setdiff(1:3, i)
+                point <- rep(NA, 3)
+                point[i] <- biggestLoc[i]
+
+                createSliceGraphic(b0Image, point[1], point[2], point[3], device="internal")
+
+                currentData <- extractDataFromMriImage(dataImage, i, point[i])
+                maskData <- extractDataFromMriImage(maskImage, i, point[i])
+                dim(currentData) <- c(prod(dim(currentData))/dim(currentData)[3], dim(currentData)[3])
+                currentData <- currentData[maskData>0,]
+                fit <- estimateDiffusionTensors(currentData, scheme, method="ls")
+
+                fullEigenvectors <- fit$eigenvectors[,1,fit$fa>=faThreshold]
+                eigenvectors <- fullEigenvectors[inPlaneAxes,]
+
+                dims <- maskImage$getDimensions()[inPlaneAxes]
+                locs <- which(maskData > 0, arr.ind=TRUE)
+                d1 <- locs[fit$fa>=faThreshold,1]
+                d2 <- locs[fit$fa>=faThreshold,2]
+
+                col <- rgb(abs(fullEigenvectors[1,]), abs(fullEigenvectors[2,]), abs(fullEigenvectors[3,]))
+
+                segments((d1-1)/(dims[1]-1)-eigenvectors[1,]/(2*dims[1]), (d2-1)/(dims[2]-1)-eigenvectors[2,]/(2*dims[2]), (d1-1)/(dims[1]-1)+eigenvectors[1,]/(2*dims[1]), (d2-1)/(dims[2]-1)+eigenvectors[2,]/(2*dims[2]), lwd=lineWidth, col=col)
+            }
+
+            repeat
+            {
+                done <- tolower(ask("Are the principal directions appropriate? [yn; s to show full images in fslview]"))
+                if (done %in% c("y","n"))
+                    break
+                else if (done == "s")
+                {
+                    runDtifitWithSession(session)
+                    showImagesInFslview(session$getImageFileNameByType("fa","diffusion"), session$getImageFileNameByType("eigenvector","diffusion",index=1))
+                }
+            }
+
+            if (done == "y")
+                break
+            else
+            {
+                ans <- tolower(ask("Flip diffusion gradient vectors along which axes? [xyz; Enter for none]"))
+                flipAxes <- which(letters[24:26] %in% splitAndConvertString(ans, ",", fixed=TRUE))
+
+                if (length(flipAxes) > 0)
+                    flipGradientVectorsForSession(session, flipAxes)
             }
         }
-        
-        if (done == "y")
-            break
-        else
+    }
+    else
+    {
+        repeat
         {
-            ans <- tolower(ask("Flip diffusion gradient vectors along which axes? [xyz; Enter for none]"))
-            flipAxes <- which(letters[24:26] %in% splitAndConvertString(ans, ",", fixed=TRUE))
+            runDtifitWithSession(session)
             
-            if (length(flipAxes) > 0)
-                flipGradientVectorsForSession(session, flipAxes)
+            repeat
+            {
+                done <- tolower(ask("Are the principal directions appropriate? [yn; s to show images in fslview]"))
+                if (done %in% c("y","n"))
+                    break
+                else if (done == "s")
+                    showImagesInFslview(session$getImageFileNameByType("fa","diffusion"), session$getImageFileNameByType("eigenvector","diffusion",index=1))
+            }
+
+            if (done == "y")
+                break
+            else
+            {
+                ans <- tolower(ask("Flip diffusion gradient vectors along which axes? [xyz; Enter for none]"))
+                flipAxes <- which(letters[24:26] %in% splitAndConvertString(ans, ",", fixed=TRUE))
+
+                if (length(flipAxes) > 0)
+                    flipGradientVectorsForSession(session, flipAxes)
+            }
         }
     }
     
