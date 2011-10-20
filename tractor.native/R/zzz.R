63184448134d03ae36254da60a4ada562b1c6251
Jon Clayden <jon.clayden@gmail.com>
2011-10-20 15:59:09 +0000

    Specifying "lib.loc" param when loading dynamic library, to avoid warning in R 2.14.0
    git-svn-id: https://tractor.googlecode.com/svn/branches/1.8@1019 fd513b17-773f-0410-bea1-c334db35fb47

--- tractor.native/R/zzz.R
+++ tractor.native/R/zzz.R
@@ -1,4 +1,4 @@
 .First.lib <- function (libname, pkgname)
 {
-    library.dynam("tractor.native", package="tractor.native")
+    library.dynam("tractor.native", package="tractor.native", lib.loc=NULL)
 }
