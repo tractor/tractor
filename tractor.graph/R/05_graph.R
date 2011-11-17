8ef517ccd01f9b246f97622a10c7dc6dbad632b7
Jon Clayden <jon.clayden@gmail.com>
2011-11-17 12:34:22 +0000

    Adding Graph class

---
+++ tractor.graph/R/05_graph.R
@@ -0,0 +1,27 @@
+Graph <- setRefClass("Graph", contains="SerialisableObject", fields=list(nVertices="integer",vertexNames="character",vertexLocations="matrix",locationUnit="character",edges="matrix",edgeNames="character",edgeWeights="numeric",directed="logical"), methods=list(
+    getEdge = function (i)
+    {
+        if (i < 0 || i > .self$nEdges())
+            return (NA)
+        else
+            return (edges[i,])
+    },
+    
+    getEdges = function () { return (vertexLocations) },
+    
+    getEdgeNames = function () { return (edgeNames) },
+    
+    getEdgeWeights = function () { return (edgeWeights) },
+    
+    getVertexLocations = function () { return (vertexLocations) },
+    
+    getVertexLocationUnit = function () { return (locationUnit) },
+    
+    getVertexNames = function () { return (vertexNames) },
+    
+    isDirected = function () { return (directed) },
+    
+    nEdges = function () { return (nrow(edges)) },
+    
+    nVertices = function () { return (nVertices) }
+))
