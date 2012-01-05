c312996c0dd4ed608105dc83a5da4ffa34fcedc2
Jon Clayden <jon.clayden@gmail.com>
2012-01-05 17:47:16 +0000

    Adding initialize function for Graph class

--- tractor.graph/R/05_graph.R
+++ tractor.graph/R/05_graph.R
@@ -1,4 +1,9 @@
 Graph <- setRefClass("Graph", contains="SerialisableObject", fields=list(nVertices="integer",vertexNames="character",vertexLocations="matrix",locationUnit="character",edges="matrix",edgeNames="character",edgeWeights="numeric",directed="logical"), methods=list(
+    initialize = function (nVertices = 0, vertexNames = NULL, vertexLocations = matrix(NA,0,0), locationUnit = "", edges = matrix(NA,0,0), edgeNames = character(0), edgeWeights = rep(NA,nrow(edges)), directed = FALSE)
+    {
+        return (initFields(nVertices=as.integer(nVertices), vertexNames=as.character(vertexNames), vertexLocations=vertexLocations, locationUnit=locationUnit, edges=edges, edgeNames=edgeNames, edgeWeights=edgeWeights, directed=directed))
+    },
+        
     getEdge = function (i)
     {
         if (i < 0 || i > .self$nEdges())
