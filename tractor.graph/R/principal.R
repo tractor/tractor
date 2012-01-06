a9264e2db5ea7b9fb0377f17ca248d6c586d1884
Jon Clayden <jon.clayden@gmail.com>
2012-01-06 22:14:27 +0000

    Adding pureComponents option and full correlation matrices in return value

--- tractor.graph/R/principal.R
+++ tractor.graph/R/principal.R
@@ -1,4 +1,4 @@
-calculatePrincipalGraphsForTable <- function (table, nComponents = NULL, loadingThreshold = 0.1, correlationThreshold = 0.2, ignoreCorrelationSign = FALSE)
+calculatePrincipalGraphsForTable <- function (table, nComponents = NULL, loadingThreshold = 0.1, correlationThreshold = 0.2, ignoreCorrelationSign = FALSE, pureComponents = FALSE)
 {
     # Scaling the data turns the covariances into correlations
     pcaResult <- prcomp(table, scale.=TRUE)
@@ -13,9 +13,18 @@ calculatePrincipalGraphsForTable <- function (table, nComponents = NULL, loading
     report(OL$Info, nComponents, " of ", length(varianceProportions), " components will be kept")
     
     verticesToKeep <- abs(pcaResult$rotation) >= loadingThreshold
-    correlations <- lapply(1:nComponents, function (i) pcaResult$sdev[i]^2 * pcaResult$rotation[verticesToKeep[,i],i] %o% pcaResult$rotation[verticesToKeep[,i],i])
+    fullCorrelations <- lapply(1:nComponents, function (i) pcaResult$sdev[i]^2 * pcaResult$rotation[,i] %o% pcaResult$rotation[,i])
+    
+    # Calculate residual correlations after subtracting out higher components
+    if (!pureComponents)
+    {
+        fullCorrelations <- Reduce("-", fullCorrelations, init=cor(table), accumulate=TRUE)
+        fullCorrelations <- fullCorrelations[-length(fullCorrelations)]
+    }
+    
+    correlations <- lapply(1:nComponents, function (i) fullCorrelations[[i]][verticesToKeep[,i],verticesToKeep[,i]])
     
     graphs <- lapply(correlations, newGraphFromConnectionMatrix, threshold=correlationThreshold, ignoreSign=ignoreCorrelationSign, allVertexNames=colnames(table))
     
-    return (list(eigenvalues=pcaResult$sdev^2, eigenvectors=pcaResult$rotation, correlations=correlations, graphs=graphs))
+    return (list(eigenvalues=pcaResult$sdev^2, eigenvectors=pcaResult$rotation, fullCorrelations=fullCorrelations, correlations=correlations, graphs=graphs))
 }
