5d4cc46c312cbd54dcb08ccea23addc9b0e9006f
Jon Clayden <jon.clayden@gmail.com>
2012-02-10 12:19:21 +0000

    Adding some more standard session directories, and a Freesurfer map

--- tractor.session/R/00_constants.R
+++ tractor.session/R/00_constants.R
@@ -17,7 +17,8 @@
     diffusion=list(rawdata="rawdata", data="data", refb0="refb0", mask="mask", maskedb0="maskedb0", s0="dti_S0", fa="dti_FA", md="dti_MD", eigenvalue="dti_eigval%", eigenvector="dti_eigvec%", axialdiff="dti_eigval1", radialdiff="dti_radial", sse="dti_SSE"),
     fdt=list(data="data", mask="nodif_brain_mask", s0="dti_S0", fa="dti_FA", md="dti_MD", eigenvalue="dti_L%", eigenvector="dti_V%", sse="dti_sse"),
     bedpost=list(dyads="dyads%", avf="mean_f%samples", theta="mean_th%samples", phi="mean_ph%samples", avfsamples="merged_f%samples", thetasamples="merged_th%samples", phisamples="merged_ph%samples"),
-    structural=list(t1="T1w_vol%", t2="T2w_vol%", pd="PDw_vol%"))
+    structural=list(t1="T1w_vol%", t2="T2w_vol%", pd="PDw_vol%"),
+    freesurfer=list(desikan_killiany=file.path("mri","aparc+aseg"), destrieux=file.path("mri","aparc.a2009s+aseg")))
 
 .FdtDiffusionMap <- list(rawdata="basic", data="data", refb0="nodif", mask="nodif_brain_mask", maskedb0="nodif_brain", s0="dti_S0", fa="dti_FA", md="dti_MD", eigenvalue="dti_L%", eigenvector="dti_V%", axialdiff="dti_L1", radialdiff="dti_Lrad")
 
