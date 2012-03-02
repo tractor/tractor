6b70f01a42fa150ba33a503123a45669bece4bb9
Jon Clayden <jon.clayden@gmail.com>
2012-03-02 17:04:39 +0000

    Getting imageinfo to work with directories of DICOM files as well as NIfTI-like images

--- share/experiments/imageinfo.R
+++ share/experiments/imageinfo.R
@@ -1,5 +1,5 @@
-#@args image file
-#@desc List the key metadata parameters of the specified Analyze, NIfTI or MGH image volume.
+#@args image file or DICOM directory
+#@desc List the key metadata parameters of the specified Analyze, NIfTI or MGH image volume. If a directory is specified, the script will assume that it is composed of DICOM files and attempt to reconstruct an image from these files. (See "dicomread" if this is your primary purpose.)
 
 library(tractor.base)
 
@@ -9,5 +9,9 @@ runExperiment <- function ()
     if (getOutputLevel() > OL$Info)
         setOutputLevel(OL$Info)
     fileName <- implode(Arguments, sep=" ")
-    print(newMriImageMetadataFromFile(fileName))
+    
+    if (isTRUE(file.info(fileName)$isdir))
+        print(newMriImageFromDicomDirectory(fileName)$image)
+    else
+        print(newMriImageMetadataFromFile(fileName))
 }
