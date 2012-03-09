cb267fae64d019ce9ba6802f32e257226625cdcd
Jon Clayden <jon.clayden@gmail.com>
2012-03-09 17:18:28 +0000

    plotcorrections script now allows Mode:all and can run noninteractively

--- share/experiments/plotcorrections.R
+++ share/experiments/plotcorrections.R
@@ -9,31 +9,47 @@ runExperiment <- function ()
 {    
     session <- newSessionFromDirectory(ifelse(nArguments()==0, ".", Arguments[1]))
     
-    mode <- getConfigVariable("Mode", "rotation", validValues=c("rotation","translation","scale","skew"))
+    mode <- getConfigVariable("Mode", "rotation", validValues=c("rotation","translation","scale","skew","all"))
+    interactive <- getConfigVariable("Interactive", TRUE)
     
-    ylab <- switch(mode, rotation="rotation angle, deg", translation="translation, mm", scale="scale factor", skew="skew")
-    legend <- switch(mode, rotation=c("X (roll)","Y (pitch)","Z (yaw)"),
-                           translation=,
-                           scale=c("X (left-right)","Y (anterior-posterior)","Z (superior-inferior)"),
-                           skew=c("X-Y", "X-Z", "Y-Z"))
+    if (mode == "all")
+        mode <- c("rotation","translation","scale","skew")
     
-    transforms <- readEddyCorrectTransformsForSession(session)
-    if (mode == "rotation")
+    for (currentMode in mode)
     {
-        values <- sapply(transforms, function (x) decomposeAffineTransform3D(x)$angles)
-        values <- values / pi * 180
+        ylab <- switch(currentMode, rotation="rotation angle, deg", translation="translation, mm", scale="scale factor", skew="skew")
+        legend <- switch(currentMode, rotation=c("X (roll)","Y (pitch)","Z (yaw)"),
+                                      translation=,
+                                      scale=c("X (left-right)","Y (anterior-posterior)","Z (superior-inferior)"),
+                                      skew=c("X-Y", "X-Z", "Y-Z"))
+    
+        transforms <- readEddyCorrectTransformsForSession(session)
+        if (currentMode == "rotation")
+        {
+            values <- sapply(transforms, function (x) decomposeAffineTransform3D(x)$angles)
+            values <- values / pi * 180
+        }
+        else
+        {
+            elementName <- switch(currentMode, translation="translation", scale="scales", skew="skews")
+            values <- sapply(transforms, function (x) decomposeAffineTransform3D(x)[[elementName]])
+        }
+    
+        data <- data.frame(index=seq_len(ncol(values)), x=values[1,], y=values[2,], z=values[3,])
+    
+        if (interactive)
+        {
+            print(xyplot(x + y + z ~ index, data, allow.multiple=TRUE, type="l", lwd=2, xlab="volume number", ylab=ylab, auto.key=list(space="right",text=legend,lines=TRUE,points=FALSE)))
+            
+            ans <- ask("Copy figure to pdf file? [yn]")
+            if (tolower(ans) == "y")
+                dev.print(pdf, file=paste(currentMode,"s.pdf",sep=""))
+        }
+        else
+        {
+            pdf(file=paste(currentMode,"s.pdf",sep=""))
+            print(xyplot(x + y + z ~ index, data, allow.multiple=TRUE, type="l", lwd=2, xlab="volume number", ylab=ylab, auto.key=list(space="right",text=legend,lines=TRUE,points=FALSE)))
+            dev.off()
+        }
     }
-    else
-    {
-        elementName <- switch(mode, translation="translation", scale="scales", skew="skews")
-        values <- sapply(transforms, function (x) decomposeAffineTransform3D(x)[[elementName]])
-    }
-    
-    data <- data.frame(index=seq_len(ncol(values)), x=values[1,], y=values[2,], z=values[3,])
-    
-    print(xyplot(x + y + z ~ index, data, allow.multiple=TRUE, type="l", lwd=2, xlab="volume number", ylab=ylab, auto.key=list(space="right",text=legend,lines=TRUE,points=FALSE)))
-    
-    ans <- ask("Copy figure to pdf file? [yn]")
-    if (tolower(ans) == "y")
-        dev.print(pdf, file=paste(mode,"s.pdf",sep=""))
 }
