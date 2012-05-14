556651db8d78c2ac728de53618e65edc40ba0e58
Jon Clayden <jon.clayden@gmail.com>
2012-05-14 17:27:22 +0100

    Updating calculatePrincipalGraphsForTable; returning scores

--- tractor.graph/R/principal.R
+++ tractor.graph/R/principal.R
@@ -1,32 +1,10 @@
-calculatePrincipalGraphsForTable <- function (table, nComponents = NULL, loadingThreshold = 0.1, correlationThreshold = 0.2, ignoreCorrelationSign = FALSE, pureComponents = FALSE)
+calculatePrincipalGraphsForTable <- function (table, nComponents = NULL, loadingThreshold = 0.1, allVertexNames = NULL)
 {
-    # Scaling the data turns the covariances into correlations
-    pcaResult <- prcomp(table, scale.=TRUE)
+    graph <- newGraphFromTable(table, method="correlation", allVertexNames=allVertexNames)
+    principalGraphs <- calculatePrincipalGraphsForGraph(graph, nComponents=nComponents, loadingThreshold=loadingThreshold)
+    principalGraphs$scores <- scale(table) %*% principalGraphs$eigenvectors
     
-    varianceProportions <- (pcaResult$sdev^2) / sum(pcaResult$sdev^2)
-    
-    if (is.null(nComponents))
-        nComponents <- sum(varianceProportions >= 1/length(varianceProportions))
-    else
-        nComponents <- min(nComponents, length(pcaResult$sdev))
-    
-    report(OL$Info, nComponents, " of ", length(varianceProportions), " components will be kept")
-    
-    verticesToKeep <- abs(pcaResult$rotation) >= loadingThreshold
-    fullCorrelations <- lapply(1:nComponents, function (i) pcaResult$sdev[i]^2 * pcaResult$rotation[,i] %o% pcaResult$rotation[,i])
-    
-    # Calculate residual correlations after subtracting out higher components
-    if (!pureComponents)
-    {
-        fullCorrelations <- Reduce("-", fullCorrelations, init=cor(table), accumulate=TRUE)
-        fullCorrelations <- fullCorrelations[-length(fullCorrelations)]
-    }
-    
-    correlations <- lapply(1:nComponents, function (i) fullCorrelations[[i]][verticesToKeep[,i],verticesToKeep[,i]])
-    
-    graphs <- lapply(correlations, newGraphFromConnectionMatrix, threshold=correlationThreshold, ignoreSign=ignoreCorrelationSign, allVertexNames=colnames(table))
-    
-    return (list(eigenvalues=pcaResult$sdev^2, eigenvectors=pcaResult$rotation, fullCorrelations=fullCorrelations, correlations=correlations, graphs=graphs))
+    return(principalGraphs)
 }
 
 calculatePrincipalGraphsForGraph <- function (graph, nComponents = NULL, loadingThreshold = 0.1)
@@ -76,5 +54,5 @@ calculatePrincipalGraphsForGraph <- function (graph, nComponents = NULL, loading
     componentGraphs <- lapply(matrices, newGraphFromConnectionMatrix, allVertexNames=graph$getVertexNames())
     componentGraphs <- lapply(componentGraphs, function (x) { x$setVertexLocations(graph$getVertexLocations(),graph$getVertexLocationUnit()); x })
     
-    return (list(eigenvalues=eigensystem$values, eigenvectors=eigensystem$vectors, componentGraphs=componentGraphs, residualGraphs=residualGraphs))
+    return (list(eigenvalues=eigensystem$values, eigenvectors=eigensystem$vectors, componentGraphs=componentGraphs, residualGraphs=residualGraphs, scores=NULL))
 }
