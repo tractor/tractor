86a0b6c3771f8e6acd1b175eb33664705e816031
Jon Clayden <jon.clayden@gmail.com>
2012-06-29 17:20:05 +0100

    Adding support for exclusion masks

--- tractor.native/R/streamline_tract.R
+++ tractor.native/R/streamline_tract.R
@@ -1,9 +1,11 @@
-newStreamlineCollectionTractWithWaypointConstraints <- function (tract, waypoints)
+newStreamlineCollectionTractWithWaypointConstraints <- function (tract, waypoints, exclusion = FALSE)
 {
     if (!is(tract, "StreamlineCollectionTract"))
         report(OL$Error, "The specified tract is not a StreamlineCollectionTract object")
     if (!is.list(waypoints))
         report(OL$Error, "Waypoints should be specified in a list")
+    if (length(exclusion) != length(waypoints))
+        exclusion <- rep(exclusion, length.out=length(waypoints))
     
     maskPoints <- lapply(waypoints, function (image) {
         if (!is(image, "MriImage"))
@@ -15,9 +17,9 @@ newStreamlineCollectionTractWithWaypointConstraints <- function (tract, waypoint
     nMaskPoints <- sapply(maskPoints, nrow)
     
     lengths <- tract$getEndIndices() - tract$getStartIndices() + 1
-    matchingIndices <- .Call("find_waypoint_hits", as.integer(round(tract$getPoints())), as.integer(tract$nPoints()), as.integer(tract$getStartIndices()), as.integer(lengths), as.integer(tract$nStreamlines()), maskPoints, as.integer(length(maskPoints)), as.integer(nMaskPoints), PACKAGE="tractor.native")
+    matchingIndices <- .Call("find_waypoint_hits", as.integer(round(tract$getPoints())), as.integer(tract$nPoints()), as.integer(tract$getStartIndices()), as.integer(lengths), as.integer(tract$nStreamlines()), maskPoints, as.integer(length(maskPoints)), as.integer(nMaskPoints), as.integer(exclusion), PACKAGE="tractor.native")
     
-    report(OL$Info, length(matchingIndices), " of ", tract$nStreamlines(), " streamlines pass through the specified waypoint(s)")
+    report(OL$Info, length(matchingIndices), " of ", tract$nStreamlines(), " streamlines meet the specified waypoint constraint(s)")
     
     if (length(matchingIndices) > 0)
     {
