7a56e8d9173d6e8284268f4faf8e39bffbf7ec62
Jon Clayden <jon.clayden@gmail.com>
2012-07-13 12:03:58 +0100

    Splitting newStreamlineCollectionTractWithWaypointConstraints() into two functions
    Conflicts:
    	tractor.native/R/waypoint.R

--- tractor.native/R/waypoint.R
+++ tractor.native/R/waypoint.R
@@ -2,6 +2,23 @@ newStreamlineCollectionTractWithWaypointConstraints <- function (tract, waypoint
 {
     if (!is(tract, "StreamlineCollectionTract"))
         report(OL$Error, "The specified tract is not a StreamlineCollectionTract object")
+    
+    matchingIndices <- findWaypointHits(tract, waypoints)
+    report(OL$Info, length(matchingIndices), " of ", tract$nStreamlines(), " streamlines pass through the specified waypoint(s)")
+    
+    if (length(matchingIndices) > 0)
+    {
+        newTract <- newStreamlineCollectionTractBySubsetting(tract, matchingIndices)
+        invisible(newTract)
+    }
+    else
+        invisible(NULL)
+}
+
+findWaypointHits <- function (tract, waypoints)
+{
+    if (!is(tract, "StreamlineCollectionTract"))
+        report(OL$Error, "The specified tract is not a StreamlineCollectionTract object")
     if (!is.list(waypoints))
         report(OL$Error, "Waypoints should be specified in a list")
     if (length(exclusion) != length(waypoints))
@@ -19,13 +36,5 @@ newStreamlineCollectionTractWithWaypointConstraints <- function (tract, waypoint
     lengths <- tract$getEndIndices() - tract$getStartIndices() + 1
     matchingIndices <- .Call("find_waypoint_hits", as.integer(round(tract$getPoints())), as.integer(tract$nPoints()), as.integer(tract$getStartIndices()), as.integer(lengths), as.integer(tract$nStreamlines()), maskPoints, as.integer(length(maskPoints)), as.integer(nMaskPoints), as.integer(exclusion), PACKAGE="tractor.native")
     
-    report(OL$Info, length(matchingIndices), " of ", tract$nStreamlines(), " streamlines meet the specified waypoint constraint(s)")
-    
-    if (length(matchingIndices) > 0)
-    {
-        newTract <- newStreamlineCollectionTractBySubsetting(tract, matchingIndices)
-        invisible(newTract)
-    }
-    else
-        invisible(NULL)
+    return (matchingIndices)
 }
