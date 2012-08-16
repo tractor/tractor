83c7acbb9bb2bd9fd9d6b44c7a724e30ce20cb17
Jon Clayden <jon.clayden@gmail.com>
2012-08-16 16:51:22 +0100

    Adding SignificantDigits option to value script

--- tests/scripts/value.R
+++ tests/scripts/value.R
@@ -4,9 +4,11 @@ runExperiment <- function ()
 {
     requireArguments("image file", "voxel location")
     
+    digits <- getConfigVariable("SignificantDigits", 6L)
+    
     image <- newMriImageFromFile(Arguments[1])
     loc <- splitAndConvertString(Arguments[-1], ",", "numeric", fixed=TRUE, errorIfInvalid=TRUE)
     
-    cat(paste("Value of image \"", basename(image$getSource()), "\" at location (", implode(loc,","), ") is ", image$getDataAtPoint(loc), "\n", sep=""))
+    cat(paste("Value of image \"", basename(image$getSource()), "\" at location (", implode(loc,","), ") is ", signif(image$getDataAtPoint(loc),digits), "\n", sep=""))
 }
 
