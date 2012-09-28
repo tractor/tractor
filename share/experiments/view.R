479b973436ab5014a670557c209c6d80069e21eb
Jon Clayden <jon.clayden@gmail.com>
2012-09-28 17:27:14 +0100

    Allowing view expt to use Freeview (and introducing TRACTOR_VIEWER env var)

--- share/experiments/view.R
+++ share/experiments/view.R
@@ -1,11 +1,29 @@
 #@args image file(s)
-#@desc Display one or more image files in "fslview". This wrapper script will automatically work around a problem with the viewer and certain file datatypes.
+#@desc Display one or more image files. The viewer used will be taken from the TRACTOR_VIEWER environment variable if it is not specified explicitly, and will default to "fslview" if this is not set. This wrapper script will automatically work around a problem with "fslview" and certain file datatypes.
 
 library(tractor.session)
 
 runExperiment <- function ()
 {
     requireArguments("image file(s)")
-    do.call(showImagesInFslview, c(as.list(Arguments),list(wait=TRUE)))    
+    
+    viewer <- tolower(Sys.getenv("TRACTOR_VIEWER"))
+    if (!(viewer %in% c("fslview","freeview")))
+    {
+        if (viewer != "")
+            report(OL$Warning, "The \"TRACTOR_VIEWER\" environment variable has an invalid value")
+        viewer <- "fslview"
+    }
+    
+    requestedViewer <- getConfigVariable("Viewer", NULL, "character", validValues=c("fslview","freeview"))
+    
+    if (!is.null(requestedViewer))
+        viewer <- requestedViewer
+    
+    if (viewer == "fslview")
+        do.call(showImagesInFslview, c(as.list(Arguments),list(wait=TRUE)))
+    else if (viewer == "freeview")
+        do.call(showImagesInFreeview, c(as.list(Arguments),list(wait=TRUE)))
+    
     invisible(NULL)
 }
