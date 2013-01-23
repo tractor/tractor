af9684a17b32c7842cf99a769c7318d2838fa412
Jon Clayden <jon.clayden@gmail.com>
2013-01-23 17:44:47 +0000

    Setting Tracker option to fsl in mtrack test

--- tests/15_FSL/015_mtrack.sh
+++ tests/15_FSL/015_mtrack.sh
@@ -1,5 +1,5 @@
 #@desc Checking that ROI-based tractography works
 ${TRACTOR} mkroi data/session-12dir 49 58 14 PointType:R Width:1 ROIName:tmp/region
-${TRACTOR} mtrack data/session-12dir SeedMaskFile:tmp/region NumberOfSamples:50 TractName:tmp/tract CreateVolumes:true
+${TRACTOR} mtrack data/session-12dir SeedMaskFile:tmp/region NumberOfSamples:50 TractName:tmp/tract CreateVolumes:true Tracker:fsl
 ${TRACTOR} value tmp/tract 49 58 14
 rm -f tmp/tract* tmp/region.*
