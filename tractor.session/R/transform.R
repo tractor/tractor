922547a085b806ba55596914d6126f43ff2fa2a1
Jon Clayden <jon.clayden@gmail.com>
2013-04-19 21:50:22 +0100

    Tidying up space-to-space transform functions

--- tractor.session/R/transform.R
+++ tractor.session/R/transform.R
@@ -26,45 +26,74 @@ readEddyCorrectTransformsForSession <- function (session, index = NULL)
     invisible (transform)
 }
 
-getTransformationBetweenSpaces <- function (sourceSpace, targetSpace, ...)
+.resolveSpace <- function (space, session)
 {
-    sourcePieces <- unlist(strsplit(sourceSpace, ":", fixed=TRUE))
-    targetPieces <- unlist(strsplit(targetSpace, ":", fixed=TRUE))
-    
-    if (length(sourcePieces) == 2 && length(targetPieces) == 2 && sourcePieces[1] == targetPieces[2])
+    spacePieces <- unlist(strsplit(space, ":", fixed=TRUE))
+    if (length(spacePieces) == 2)
+    {
+        if (spacePieces[1] != session$getDirectory())
+            report(OL$Error, "The current space does not correspond to the specified session")
+        else
+            return (spacePieces[2])
+    }
+    else
+        return (spacePieces[1])
 }
 
-transformImageToSpace <- function (image, session, space, currentSpace = NULL, nonlinear = FALSE, reverse = FALSE, finalInterpolation = 1, ...)
+.constructSpace <- function (space)
 {
+    if (tolower(space) == "mni")
+        return ("mni")
+    else
+        return (paste(session$getDirectory(), space, sep=":"))
+}
+
+transformImageToSpace <- function (image, session, sourceSpace = NULL, targetSpace = NULL, preferAffine = FALSE, reverse = FALSE, finalInterpolation = 1, ...)
+{
+    require("tractor.reg")
     
+    guessSpace <- function ()
+    {
+        if (image$isInternal())
+            return (NULL)
+        else if (dirname(image$getSource()) == .StandardBrainPath)
+            return ("mni")
+        else
+        {
+            for (space in setdiff(names(.RegistrationTargets),"mni"))
+            {
+                if (dirname(image$getSource()) == session$getDirectory(space))
+                    return (space)
+            }
+        }
+        
+        return (NULL)
+    }
+    
+    if (reverse && is.null(targetSpace))
+        targetSpace <- guessSpace()
+    else if (!reverse && is.null(sourceSpace))
+        sourceSpace <- guessSpace()
+    
+    if (is.null(sourceSpace) || is.null(targetSpace))
+        report(OL$Error, "Source and target spaces are not both defined")
+    
+    transform <- session$getTransformation(.resolveSpace(sourceSpace,session), .resolveSpace(targetSpace,session), ...)
+    
+    newImage <- transformImage(transform, image, preferAffine=preferAffine, reverse=reverse, finalInterpolation=finalInterpolation)
+    
+    return (newImage)
 }
 
 transformPointsBetweenSpaces <- function (points, session, sourceSpace = NULL, targetSpace = NULL, pointType = NULL, preferAffine = FALSE, reverse = FALSE, nearest = FALSE, ...)
 {
-    resolveSpace <- function (space)
-    {
-        spacePieces <- unlist(strsplit(space, ":", fixed=TRUE))
-        if (length(spacePieces) == 2)
-        {
-            if (spacePieces[1] != session$getDirectory())
-                report(OL$Error, "The current space does not correspond to the specified session")
-            else
-                return (spacePieces[2])
-        }
-        else
-            return (spacePieces[1])
-    }
-    
-    constructSpace <- function (space)
-    {
-        if 
-    }
+    require("tractor.reg")
     
     if (is.null(pointType))
     {
         pointType <- attr(points, "pointType")
         if (is.null(pointType))
-            report(OL$Error, "Current space is not stored with the points and must be specified")
+            report(OL$Error, "Point type is not stored with the points and must be specified")
     }
     
     pointType <- match.arg(tolower(pointType), c("fsl","r","vox","mm"))
@@ -79,11 +108,17 @@ transformPointsBetweenSpaces <- function (points, session, sourceSpace = NULL, t
     if (is.null(sourceSpace) || is.null(targetSpace))
         report(OL$Error, "Source and target spaces are not both defined")
     
-    transform <- session$getTransformation(resolveSpace(sourceSpace), resolveSpace(targetSpace), ...)
+    transform <- session$getTransformation(.resolveSpace(sourceSpace,session), .resolveSpace(targetSpace,session), ...)
     
     newPoints <- transformPoints(transform, points, voxel=(pointType!="mm"), preferAffine=preferAffine, reverse=reverse, nearest=nearest)
     
     if (reverse)
+        attr(newPoints, "space") <- .constructSpace(sourceSpace)
+    else
+        attr(newPoints, "space") <- .constructSpace(targetSpace)
+    attr(newPoints, "pointType") <- ifelse(pointType=="mm", "mm", "r")
+    
+    return (newPoints)
 }
 
 getNativeSpacePointForSession <- function (session, point, pointType, isStandard, round = TRUE)
