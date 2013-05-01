b2ff2639e5db4b8bf2807119d0c74fa30716658b
Jon Clayden <jon.clayden@gmail.com>
2013-05-01 14:11:58 +0100

    Adding console script

---
+++ share/experiments/console.R
@@ -0,0 +1,9 @@
+#@desc Start up the TractoR console, an interactive R session with all TractoR packages loaded.
+#@nohistory TRUE
+
+library(tractor.reg)
+library(mmand)
+library(tractor.session)
+library(splines)
+library(tractor.nt)
+library(tractor.native)
