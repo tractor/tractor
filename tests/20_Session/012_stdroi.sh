c1a19b8f99319c02186acb9f5a137faaaa221ed9
Jon Clayden <jon.clayden@gmail.com>
2013-07-01 10:35:53 +0100

    More changes to tests

---
+++ tests/20_Session/012_stdroi.sh
@@ -0,0 +1,4 @@
+#@desc Checking implicit registration
+${TRACTOR} mkroi data/session-12dir 0,18,16 PointType:mm CentreInMNISpace:true Width:3 ROIName:tmp/region
+${TRACTOR} which tmp/region eq 1
+rm -f tmp/region.*
--- tests/35_PNT/005_ref.sh
+++ tests/35_PNT/005_ref.sh
@@ -1,4 +1,4 @@
 #@desc Checking that we can create a PNT reference tract
-${TRACTOR} pnt-ref data/session-12dir 49 58 14 PointType:R NumberOfSamples:50 TractName:tmp/tract Tracker:fsl
+${TRACTOR} pnt-ref data/session-12dir 49 58 14 PointType:R NumberOfSamples:50 TractName:tmp/tract
 ${TRACTOR} peek tmp/tract_ref | grep -v session-12dir | grep -v spacing | sed '/seed/s/\.[0-9]*//g'
 rm -f tmp/tract_ref.Rdata
