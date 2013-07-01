a1904b83a99a74bd855fb572b8579156e29fc641
Jon Clayden <jon.clayden@gmail.com>
2013-07-01 11:00:42 +0100

    Tweaking test descriptions

--- tests/10_ImageProcessing/015_smooth.sh
+++ tests/10_ImageProcessing/015_smooth.sh
@@ -1,4 +1,4 @@
-#@desc Checking that image smoothing works (mmand package required)
+#@desc Checking that image smoothing works
 cp data/session-12dir/tractor/diffusion/dti_FA.* tmp/
 ${TRACTOR} smooth tmp/dti_FA 5.0 WidthType:sd
 ${TRACTOR} value tmp/dti_FA_smoothed 54 50 13
--- tests/90_Foreign/005_avscale.sh
+++ tests/90_Foreign/005_avscale.sh
@@ -1,4 +1,4 @@
-#@desc Checking affine decomposition consistency between TractoR and FSL (FSL required)
+#@desc Checking affine decomposition consistency between TractoR and FSL
 ${TRACTOR} -v1 decompose data/session-12dir 2
 echo "---"
 sed -n '12,15p' data/session-12dir/tractor/fdt/data.ecclog >tmp/avscale.mat
