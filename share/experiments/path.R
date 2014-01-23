d4dc9bbebb546025a5200d395910629ba4c1c8e7
Jon Clayden <jon.clayden@gmail.com>
2014-01-23 12:44:05 +0000

    Adding path script

---
+++ share/experiments/path.R
@@ -0,0 +1,18 @@
+#@args session directory, image type
+#@desc Print the full path to the specified image type within a session directory. Some image types, such as "data", can exist in multiple places, and in these cases the Place option can be used to disambiguate the request. The Index option can be use to choose between indexed images, such as diffusion tensor eigenvalue maps.
+#@nohistory TRUE
+
+library(tractor.session)
+
+runExperiment <- function ()
+{
+    requireArguments("session directory", "image type")
+    
+    place <- getConfigVariable("Place", NULL, "character")
+    index <- getConfigVariable("Index", 1L, "integer")
+    
+    session <- newSessionFromDirectory(Arguments[1])
+    path <- session$getImageFileNameByType(Arguments[2], place, index)
+    
+    cat(paste(path, "\n", sep=""))
+}
