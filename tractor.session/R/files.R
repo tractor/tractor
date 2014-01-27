cd45457b3a3a24705bd7ac3d01abbd95a376648f
Jon Clayden <jon.clayden@gmail.com>
2014-01-27 18:12:45 +0000

    Adding functions for reading and writing parcellation information

--- tractor.session/R/files.R
+++ tractor.session/R/files.R
@@ -59,3 +59,51 @@ writeMriImageToCamino <- function (image, fileName, gzipped = FALSE, datatype = 
     
     invisible (fileName)
 }
+
+readParcellation <- function (fileName, ...)
+{
+    image <- readImageFile(fileName, ...)
+    regionFileName <- ensureFileSuffix(image$getSource(), "txt")
+    regions <- read.table(regionFileName, header=TRUE, stringsAsFactors=FALSE)
+    return (list(image=image, regions=regions))
+}
+
+writeParcellation <- function (image, regions, ...)
+{
+    if (!is.data.frame(regions))
+        report(OL$Error, "Regions should be specified as a data frame")
+    else if (nrow(regions) == 0)
+        report(OL$Error, "Regions data frame is empty")
+    
+    info <- writeImageFile(image, ...)
+    regionFileName <- ensureFileSuffix(info$fileStem, "txt")
+    regionMatrix <- as.matrix(format(regions, justify="left"))
+    regionMatrix[,"colour"] <- paste('"', regionMatrix[,"colour"], '"', sep="")
+    
+    nColumns <- ncol(regions)
+    widths <- pmax(nchar(regionMatrix[1,]), nchar(colnames(regions)))
+    
+    padding <- widths - nchar(colnames(regions))
+    headerString <- " "
+    for (i in seq_len(nColumns))
+        headerString <- paste(headerString, colnames(regions)[i], implode(rep(" ",padding[i]+2),sep=""), sep="")
+    headerString <- substr(headerString, 1, nchar(headerString)-2)
+    
+    rightJustify <- sapply(seq_len(nColumns), function(i) is.numeric(regions[,i]))
+    padding <- widths - nchar(regionMatrix[1,])
+    lines <- apply(regionMatrix, 1, function(x) {
+        string <- " "
+        for (i in seq_along(x))
+        {
+            if (rightJustify[i])
+                string <- paste(string, implode(rep(" ",padding[i]),sep=""), x[i], "  ", sep="")
+            else
+                string <- paste(string, x[i], implode(rep(" ",padding[i]+2),sep=""), sep="")
+        }
+        return (string)
+    })
+    lines <- c(headerString, implode(c("#",rep("-",sum(widths)+2*(length(widths)-1)))), lines)
+    lines <- sub("\\s+$", "", lines, perl=TRUE)
+    
+    writeLines(lines, regionFileName)
+}
