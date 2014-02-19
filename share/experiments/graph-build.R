c7ecd7c65250d26d48b3cf8dd18fc0f0ba1a7290
Jon Clayden <jon.clayden@gmail.com>
2014-02-19 23:15:12 +0000

    Various tweaks and fixes

--- share/experiments/graph-build.R
+++ share/experiments/graph-build.R
@@ -7,7 +7,7 @@ runExperiment <- function ()
     targetRegions <- getConfigVariable("TargetRegions", "cerebral_cortex")
     parcellationConfidence <- getConfigVariable("ParcellationConfidence", 0.2)
     graphName <- getConfigVariable("GraphName", "graph")
-    zeroDiagonal <- getConfigVariable("ZeroDiagonal", TRUE)
+    selfConnections <- getConfigVariable("SelfConnections", FALSE)
     tractName <- getConfigVariable("TractName", NULL, "character")
     
     if (!is.null(tractName))
@@ -26,10 +26,10 @@ runExperiment <- function ()
         report(OL$Info, "Using #{nRegions} matched target regions")
         
         report(OL$Info, "Finding streamlines passing through each region")
-        matchingIndices <- vector("list", nRegions)                     # Indices of streamlines passing through each region
-        regionLocations <- matrix(NA, nrow=nRegions, ncol=3)            # Physical location of each region's spatial median, in mm
-        voxelCount      <- integer(nRegions)                            # Number of voxels
-        volume          <- numeric(nRegions)                            # Volume in mm^3
+        matchingIndices <- vector("list", nRegions)             # Indices of streamlines passing through each region
+        regionLocations <- matrix(NA, nrow=nRegions, ncol=3)    # Physical location of each region's spatial median, in mm
+        voxelCount      <- integer(nRegions)                    # Number of voxels
+        volume          <- numeric(nRegions)                    # Volume in mm^3
         for (i in seq_len(nRegions))
         {
     	    report(OL$Verbose, "Matching region \"#{targetMatches[i]}\"")
@@ -37,28 +37,28 @@ runExperiment <- function ()
             regionImage <- newMriImageWithSimpleFunction(parcellation$image, function(x) ifelse(x==index,1,0))
             matchingIndices[[i]] <- findWaypointHits(streamlines, list(regionImage))
     		regionLocations[i,] <- apply(regionImage$getNonzeroIndices(array=TRUE), 2, median)
-    		regionLocations[i,] <- transformVoxelToWorld(regionLocations[i,], regionImage)
+    		regionLocations[i,] <- transformVoxelToWorld(regionLocations[i,], regionImage, simple=TRUE)
             voxelCount[i] <- length(regionImage$getNonzeroIndices(array=FALSE))
-            volume[i] <- voxelCount[i] * prod(regionImage$getVoxelDimensions())
+            volume[i] <- voxelCount[i] * abs(prod(regionImage$getVoxelDimensions()))
         }
         
-        fa <- session$getImageByType("FA")
-        md <- session$getImageByType("MD")
+        fa <- session$getImageByType("FA", "diffusion")
+        md <- session$getImageByType("MD", "diffusion")
         
         report(OL$Info, "Creating connectivity matrix")
-        adjacencyMatrix   <- matrix(0L, nrow=nRegions, ncol=nRegions)   # Binary adjacency matrix: 1 if any streamlines connect regions
-        nStreamlines      <- numeric(0)                                 # Number of streamlines forming the connection
-    	binaryFA          <- numeric(0)                                 # Mean FA, counting voxels only once
-    	weightedFA        <- numeric(0)                                 # Mean FA, counting voxels each time they are visited
-    	binaryMD          <- numeric(0)                                 # Mean MD, counting voxels only once
-    	weightedMD        <- numeric(0)                                 # Mean MD, counting voxels each time they are visited
-    	streamlineLength  <- numeric(0)                                 # Average length of streamlines connecting each pair of regions
-    	uniqueVoxels      <- numeric(0)                                 # Number of unique voxels visited
-    	voxelVisits       <- numeric(0)                                 # Number of voxel visits across all streamlines
+        edgeList         <- matrix(NA, nrow=0, ncol=2)      # Edge list, one edge per row
+        nStreamlines     <- numeric(0)                      # Number of streamlines forming the connection
+        binaryFA         <- numeric(0)                      # Mean FA, counting voxels only once
+        weightedFA       <- numeric(0)                      # Mean FA, counting voxels each time they are visited
+        binaryMD         <- numeric(0)                      # Mean MD, counting voxels only once
+        weightedMD       <- numeric(0)                      # Mean MD, counting voxels each time they are visited
+        streamlineLength <- numeric(0)                      # Average length of streamlines connecting each pair of regions
+        uniqueVoxels     <- numeric(0)                      # Number of unique voxels visited
+        voxelVisits      <- numeric(0)                      # Number of voxel visits across all streamlines
     	
         for (i in seq_len(nRegions))
         {
-            for (j in seq_len(ifelse(zeroDiagonal,i-1,i)))
+            for (j in seq_len(ifelse(selfConnections,i,i-1)))
             {
                 # The subset of streamline indices connecting these two regions
     			currentStreamlineIndices <- intersect(matchingIndices[[i]], matchingIndices[[j]])
@@ -66,7 +66,7 @@ runExperiment <- function ()
     			if (nCurrentStreamlines == 0)
     				next
                 
-                adjacencyMatrix[i,j] <- 1
+                edgeList <- rbind(edgeList, c(j,i))
                 nStreamlines <- c(nStreamlines, nCurrentStreamlines)
                 
                 currentStreamlines <- newStreamlineCollectionTractBySubsetting(streamlines, currentStreamlineIndices)
@@ -84,9 +84,7 @@ runExperiment <- function ()
         }
         
         report(OL$Info, "Creating and writing graph")
-        adjacencyMatrix[upper.tri(adjacencyMatrix)] <- adjacencyMatrix[lower.tri(adjacencyMatrix)]
-        graph <- asGraph(adjacencyMatrix, directed=FALSE, allRegionNames=targetMatches, ignoreSelfConnections=zeroDiagonal)
-        
+        graph <- asGraph(edgeList, edgeList=TRUE, directed=FALSE, selfConnections=selfConnections, allVertexNames=targetMatches)
         graph$setVertexAttributes(voxelCount=voxelCount, volume=volume)
         graph$setVertexLocations(regionLocations, "mm")
         graph$setEdgeAttributes(nStreamlines=nStreamlines, binaryFA=binaryFA, weightedFA=weightedFA, binaryMD=binaryMD, weightedMD=weightedMD, streamlineLength=streamlineLength, uniqueVoxels=uniqueVoxels, voxelVisits=voxelVisits)
