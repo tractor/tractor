eb885d4cb15ae40600d42bfc1b4317d2331718f2
Jon Clayden <jon.clayden@gmail.com>
2014-02-28 17:55:27 +0000

    Updating graph2csv

--- share/experiments/graph2csv.R
+++ share/experiments/graph2csv.R
@@ -1,14 +1,18 @@
-#@args file name
-#@desc Convert a Graph object stored in an ".Rdata" file into a comma-separated list of numbers representing the corresponding association or adjacency matrix.
+#@args [graph file]
+#@desc Convert a Graph object, stored in an ".Rdata" file, into a comma-separated list of numbers representing the corresponding association or adjacency matrix. The object may be specified as an argument (which takes priority), or through the GraphName option.
 
 library(tractor.base)
 library(tractor.graph)
 
 runExperiment <- function ()
 {
-    requireArguments("file name")
+    graphName <- getConfigVariable("GraphName", NULL, "character")
     
-    fileName <- ensureFileSuffix(Arguments[1], "Rdata")
+    if (nArguments() > 0)
+        fileName <- ensureFileSuffix(implode(Arguments[1]," "), "Rdata")
+    else
+        fileName <- ensureFileSuffix(graphName, "Rdata")
+    
     graph <- deserialiseReferenceObject(fileName)
     fileName <- ensureFileSuffix(fileName, "csv", strip="Rdata")
     write.table(graph$getAssociationMatrix(), fileName, sep=",", row.names=FALSE, col.names=FALSE)
