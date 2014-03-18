6860d6cea72ea4381c9d6249747d1ddf9a0e60f6
Jon Clayden <code@clayden.org>
2014-03-18 11:44:29 +0000

    NT test updates

--- tests/35_PNT/010_data.sh
+++ tests/35_PNT/010_data.sh
@@ -1,3 +1,3 @@
 #@desc Checking that we can create candidate tracts for PNT
-${TRACTOR} pnt-data TractName:genu SessionList:data/session SeedPoint:50,59,33 PointType:R NumberOfSamples:50 DatasetName:tmp/pnt_test_data
+${TRACTOR} pnt-data TractName:genu SessionList:data/session NumberOfSamples:50 DatasetName:tmp/pnt_test_data
 rm -f tmp/pnt_test_data.txt
