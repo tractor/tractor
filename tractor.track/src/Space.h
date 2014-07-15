cb281e5f16411f589b15ee95f2821bb4c1672475
Jon Clayden <code@clayden.org>
2014-07-15 14:53:40 +0100

    Changes to Space; adding sphericalToCartesian static method

--- tractor.track/src/Space.h
+++ tractor.track/src/Space.h
@@ -3,10 +3,37 @@
 
 #include "RcppArmadillo.h"
 
-template <int Dimensionality> struct Space
+template <int Dimensionality, typename DataType = float> class Space
 {
-    typedef arma::fvec::fixed<Dimensionality> Point;
-    typedef arma::fvec::fixed<Dimensionality> Vector;
+public:
+    // This syntax boggles my mind, but seems to be correct...
+    typedef typename arma::Col<DataType>::template fixed<Dimensionality> Point;
+    typedef typename arma::Col<DataType>::template fixed<Dimensionality> Vector;
+    
+    static const Vector sphericalToCartesian (const Vector &spherical)
+    {
+        if (Dimensionality != 3)
+            throw new std::domain_error("Spherical coordinates are only defined in 3D spaces");
+        
+        Vector cartesian;
+        cartesian[0] = spherical[0] * sin(spherical[1]) * cos(spherical[2]);
+        cartesian[1] = spherical[0] * sin(spherical[1]) * sin(spherical[2]);
+        cartesian[2] = spherical[0] * cos(spherical[1]);
+        return cartesian;
+    }
 };
+
+// template <> class Space<3>
+// {
+// public:
+//     static const Vector sphericalToCartesian (const Vector &spherical)
+//     {
+//         Vector cartesian;
+//         cartesian[0] = spherical[0] * sin(spherical[1]) * cos(spherical[2]);
+//         cartesian[1] = spherical[0] * sin(spherical[1]) * sin(spherical[2]);
+//         cartesian[2] = spherical[0] * cos(spherical[1]);
+//         return cartesian;
+//     }
+// };
 
 #endif
