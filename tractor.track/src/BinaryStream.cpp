40b15bfb28caeba5f37002fc1f10299d5d5fa5bb
Jon Clayden <code@clayden.org>
2014-08-20 17:55:50 +0100

    Adding BinaryOutputStream

--- tractor.track/src/BinaryStream.cpp
+++ tractor.track/src/BinaryStream.cpp
@@ -37,7 +37,7 @@ SourceType BinaryInputStream::readValue ()
 }
 
 template <typename SourceType, typename FinalType>
-void BinaryInputStream::readValues (std::vector<FinalType> &values, size_t n)
+void BinaryInputStream::readVector (std::vector<FinalType> &values, size_t n)
 {
     values.resize(n);
     SourceType value;
@@ -51,7 +51,7 @@ void BinaryInputStream::readValues (std::vector<FinalType> &values, size_t n)
 }
 
 template <typename SourceType, typename FinalType>
-void BinaryInputStream::readValues (arma::Col<FinalType> &values, size_t n)
+void BinaryInputStream::readVector (arma::Col<FinalType> &values, size_t n)
 {
     values.set_size(n);
     SourceType value;
@@ -73,11 +73,88 @@ std::string BinaryInputStream::readString (size_t n)
     return finalValue;
 }
 
+template <typename TargetType>
+void BinaryOutputStream::writeValue (const TargetType value)
+{
+    if (swapEndian)
+        swap(&value);
+    stream->write((const char *) &value, sizeof(TargetType));
+}
+
+template <typename TargetType>
+void BinaryOutputStream::writeValues (const TargetType value, size_t n)
+{
+    if (swapEndian)
+        swap(&value);
+    for (size_t i=0; i<n; i++)
+        stream->write((const char *) &value, sizeof(TargetType));
+}
+
+template <typename TargetType>
+void BinaryOutputStream::writeValues (TargetType * const pointer, size_t n)
+{
+    for (TargetType *movingPointer=pointer; movingPointer<(pointer+n); movingPointer++)
+    {
+        if (swapEndian)
+            swap(movingPointer);
+        stream->write((const char *) movingPointer, sizeof(TargetType));
+    }
+}
+
+template <typename TargetType, typename OriginalType>
+void BinaryOutputStream::writeVector (const std::vector<OriginalType> &values, size_t n)
+{
+    if (n == 0)
+        n = values.size();
+    
+    TargetType value;
+    for (size_t i=0; i<n; i++)
+    {
+        value = static_cast<TargetType>(values[i]);
+        if (swapEndian)
+            swap(&value);
+        stream->write((const char *) &value, sizeof(TargetType));
+    }
+}
+
+template <typename TargetType, typename OriginalType>
+void BinaryOutputStream::writeVector (const arma::Col<OriginalType> &values, size_t n)
+{
+    if (n == 0)
+        n = values.n_elem;
+    
+    TargetType value;
+    for (size_t i=0; i<n; i++)
+    {
+        value = static_cast<TargetType>(values[i]);
+        if (swapEndian)
+            swap(&value);
+        stream->write((const char *) &value, sizeof(TargetType));
+    }
+}
+
+void BinaryOutputStream::writeString (const std::string &value)
+{
+    stream->write(value.c_str(), value.length());
+}
+
 // Explicit declarations to ensure code is generated
 template int16_t BinaryInputStream::readValue<int16_t> ();
 template int32_t BinaryInputStream::readValue<int32_t> ();
 
-template void BinaryInputStream::readValues<float,float> (std::vector<float> &values, size_t n);
-template void BinaryInputStream::readValues<int16_t,int> (std::vector<int> &values, size_t n);
+template void BinaryInputStream::readVector<float,float> (std::vector<float> &values, size_t n);
+template void BinaryInputStream::readVector<int16_t,int> (std::vector<int> &values, size_t n);
 
-template void BinaryInputStream::readValues<float,float> (arma::Col<float> &values, size_t n);
+template void BinaryInputStream::readVector<float,float> (arma::Col<float> &values, size_t n);
+
+template void BinaryOutputStream::writeValue<int16_t> (int16_t value);
+template void BinaryOutputStream::writeValue<int32_t> (int32_t value);
+
+template void BinaryOutputStream::writeValues<char> (char * const pointer, size_t n);
+template void BinaryOutputStream::writeValues<float> (float * const pointer, size_t n);
+
+template void BinaryOutputStream::writeValues<float> (float value, size_t n);
+
+template void BinaryOutputStream::writeVector<float,float>(arma::Col<float> const &values, size_t n);
+template void BinaryOutputStream::writeVector<float,float>(std::vector<float> const &values, size_t n);
+template void BinaryOutputStream::writeVector<int16_t,int>(std::vector<int> const &values, size_t n);
--- tractor.track/src/BinaryStream.h
+++ tractor.track/src/BinaryStream.h
@@ -29,9 +29,31 @@ public:
     void detach () { this->stream = NULL; }
     
     template <typename SourceType> SourceType readValue ();
-    template <typename SourceType, typename FinalType> void readValues (std::vector<FinalType> &values, size_t n);
-    template <typename SourceType, typename FinalType> void readValues (arma::Col<FinalType> &values, size_t n);
+    template <typename SourceType, typename FinalType> void readVector (std::vector<FinalType> &values, size_t n);
+    template <typename SourceType, typename FinalType> void readVector (arma::Col<FinalType> &values, size_t n);
     std::string readString (size_t n);
+};
+
+class BinaryOutputStream : public BinaryStream
+{
+private:
+    std::ofstream *stream;
+    
+public:
+    BinaryOutputStream ()
+        : stream(NULL) {}
+    BinaryOutputStream (std::ofstream *stream)
+        : stream(stream) {}
+    
+    void attach (std::ofstream *stream) { this->stream = stream; }
+    void detach () { this->stream = NULL; }
+    
+    template <typename TargetType> void writeValue (const TargetType value);
+    template <typename TargetType> void writeValues (const TargetType value, size_t n);
+    template <typename TargetType> void writeValues (TargetType * const pointer, size_t n);
+    template <typename TargetType, typename OriginalType> void writeVector (const std::vector<OriginalType> &values, size_t n = 0);
+    template <typename TargetType, typename OriginalType> void writeVector (const arma::Col<OriginalType> &values, size_t n = 0);
+    void writeString (const std::string &value);
 };
 
 #endif
