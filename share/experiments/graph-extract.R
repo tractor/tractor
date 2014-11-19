fc0f9617447c82cd01fb2aba75021a77d7edf8cb
Jon Clayden <code@clayden.org>
2014-11-19 17:55:59 +0000

    Adding graph-extract script

---
+++ share/experiments/graph-extract.R
@@ -0,0 +1,28 @@
+#@desc Create a subgraph by extracting only a subset of the nodes of the specified graph, and their interconnections. Nodes may be specified by name or parcellation index, separated by spaces or commas.
+#@args session directory, regions to retain
+
+library(tractor.reg)
+library(tractor.session)
+library(tractor.graph)
+
+runExperiment <- function ()
+{
+    graphName <- getConfigVariable("GraphName", NULL, "character", errorIfMissing=TRUE)
+    subgraphName <- getConfigVariable("SubgraphName", NULL, "character", errorIfMissing=TRUE)
+    
+    graph <- deserialiseReferenceObject(graphName)
+    session <- newSessionFromDirectory(Arguments[1])
+    parcellation <- session$getParcellation()
+    regionNames <- matchRegions(splitAndConvertString(Arguments[-1],",",fixed=TRUE), parcellation, labels=TRUE)
+    
+    nodeNames <- graph$getVertexAttributes("name")
+    if (is.null(nodeNames))
+        report(OL$Error, "The specified graph does not contain vertex names")
+    
+    indices <- match(regionNames, nodeNames)
+    if (any(is.na(indices)))
+        report(OL$Warning, "Regions ", implode(regionNames[is.na(indices)],",",finalSep=" and "), " are not present in the graph")
+    
+    subgraph <- inducedSubgraph(graph, na.omit(indices))
+    subgraph$serialise(subgraphName)
+}
