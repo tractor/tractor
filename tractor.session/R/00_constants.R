df35c79bfd91b39cd3444f613d983571564180f3
Jon Clayden <code@clayden.org>
2015-02-18 21:26:52 +0000

    Adding parcellation image to functional session map

--- tractor.session/R/00_constants.R
+++ tractor.session/R/00_constants.R
@@ -21,7 +21,7 @@
     bedpost=list(dyads="dyads%", avf="mean_f%samples", theta="mean_th%samples", phi="mean_ph%samples", avfsamples="merged_f%samples", thetasamples="merged_th%samples", phisamples="merged_ph%samples"),
     structural=list(t1="T1w_vol%", t2="T2w_vol%", pd="PDw_vol%", reft1="refT1", parcellation="parcellation"),
     freesurfer=list(reft1=file.path("mri","rawavg"), normT1=file.path("mri","T1"), "desikan-killiany"=file.path("mri","aparc+aseg"), destrieux=file.path("mri","aparc.a2009s+aseg")),
-    functional=list(data="data", mean="mean_vol"))
+    functional=list(data="data", mean="mean_vol", parcellation="parcellation"))
 
 .FdtDiffusionMap <- list(rawdata="basic", data="data", refb0="nodif", mask="nodif_brain_mask", maskedb0="nodif_brain", s0="dti_S0", fa="dti_FA", md="dti_MD", eigenvalue="dti_L%", eigenvector="dti_V%", axialdiff="dti_L1", radialdiff="dti_Lrad")
 
