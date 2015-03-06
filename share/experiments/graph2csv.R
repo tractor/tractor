3b0ce99d51370b6edf1707ae5d10a1b13a727f9c
Jon Clayden <code@clayden.org>
2015-03-06 13:40:29 +0000

    The graph2csv file now writes out vertex attributes in comments

--- share/experiments/graph2csv.R
+++ share/experiments/graph2csv.R
@@ -1,5 +1,5 @@
 #@args [graph file]
-#@desc Convert a Graph object, stored in an ".Rdata" file, into a comma-separated list of numbers representing the corresponding association or adjacency matrix. The object may be specified as an argument (which takes priority), or through the GraphName option.
+#@desc Convert a Graph object, stored in an ".Rdata" file, into a comma-separated list of numbers representing the corresponding association or adjacency matrix. The diagonal runs top-left to bottom-right in this case. Vertex attributes are included as comments at the top of the file. The object may be specified as an argument (which takes priority), or through the GraphName option.
 
 library(tractor.base)
 library(tractor.graph)
@@ -15,5 +15,16 @@ runExperiment <- function ()
     
     graph <- deserialiseReferenceObject(fileName)
     fileName <- ensureFileSuffix(fileName, "csv", strip="Rdata")
-    write.table(graph$getAssociationMatrix(), fileName, sep=",", row.names=FALSE, col.names=FALSE)
+    connection <- file(fileName, "w")
+    
+    attribStrings <- sapply(graph$getVertexAttributes(), function(attrib) paste0(": ", implode(attrib,",")))
+    if (length(attribStrings) > 0)
+    {
+        attribStrings <- paste0("# ", names(graph$getVertexAttributes()), attribStrings)
+        writeLines(attribStrings, connection)
+    }
+    
+    write.table(graph$getAssociationMatrix(), connection, sep=",", row.names=FALSE, col.names=FALSE)
+    
+    close(connection)
 }
