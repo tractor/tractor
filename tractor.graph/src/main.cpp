bd6f94bcdce0101106a9564d17626869753d0714
Jon Clayden <code@clayden.org>
2015-12-16 14:55:06 +0000

    Tidying up shortest paths C++ code

--- tractor.graph/src/main.cpp
+++ tractor.graph/src/main.cpp
@@ -8,45 +8,41 @@
 using namespace Rcpp;
 using namespace lemon;
 
-template <class GraphType>
-typename GraphType::Arc addEdge (GraphType &graph, const typename GraphType::Node &source, const typename GraphType::Node &target)
-{
-    return GraphType::Arc(INVALID);
-}
+typedef SmartDigraph::NodeMap<int> IndexMap;
+typedef SmartDigraph::ArcMap<double> WeightMap;
 
-template <>
-SmartDigraph::Arc addEdge (SmartDigraph &graph, const SmartDigraph::Node &source, const SmartDigraph::Node &target)
+struct RGraph
 {
-    return graph.addArc(source, target);
-}
+    SmartDigraph graph;
+    IndexMap indices;
+    WeightMap weights;
+    bool directed;
+    bool weighted;
+    bool negativeWeights;
+    
+    RGraph ()
+        : indices(graph), weights(graph), directed(false), weighted(false), negativeWeights(false) {}
+};
 
-template <>
-SmartGraph::Arc addEdge (SmartGraph &graph, const SmartGraph::Node &source, const SmartGraph::Node &target)
+template <class Algorithm>
+void searchPaths (const RGraph &g, Algorithm &algorithm, NumericMatrix &result)
 {
-    SmartGraph::Edge edge = graph.addEdge(source, target);
-    return graph.direct(edge, true);
-}
-
-template <class GraphType, class Algorithm>
-void searchPaths (const GraphType &graph, Algorithm &algorithm, std::map<typename GraphType::Node, int> &indices, NumericMatrix &result)
-{
-    for (typename GraphType::NodeIt source(graph); source != INVALID; ++source)
+    for (SmartDigraph::NodeIt source(g.graph); source != INVALID; ++source)
     {
         algorithm.run(source);
-        for (typename GraphType::NodeIt target(graph); target != INVALID; ++target)
+        for (SmartDigraph::NodeIt target(g.graph); target != INVALID; ++target)
         {
             if (!algorithm.reached(target))
-                result(indices[source], indices[target]) = R_PosInf;
+                result(g.indices[source], g.indices[target]) = R_PosInf;
             else
-                result(indices[source], indices[target]) = algorithm.dist(target);
+                result(g.indices[source], g.indices[target]) = algorithm.dist(target);
         }
     }
 }
 
-template <class GraphType>
-NumericMatrix findShortestPaths (const int nVertices, const IntegerMatrix &edges, const NumericVector &weights)
+void createGraph (const int nVertices, const IntegerMatrix &edges, const NumericVector &weights, const bool directed, RGraph &g)
 {
-    bool weighted = false, negativeWeights = false;
+    g.directed = directed;
     double nonzeroValue = NA_REAL;
     
     for (int i=0; i<weights.size(); i++)
@@ -55,31 +51,26 @@ NumericMatrix findShortestPaths (const int nVertices, const IntegerMatrix &edges
         if (!ISNA(weight) && weight != 0.0)
         {
             if (weight < 0.0)
-                negativeWeights = true;
+                g.negativeWeights = true;
             
             // The graph is classified as weighted if there is more than one unique nonzero weight
             if (ISNA(nonzeroValue))
                 nonzeroValue = weight;
             else if (weight != nonzeroValue)
-                weighted = true;
+                g.weighted = true;
             
             // No need to keep looking
-            if (weighted && negativeWeights)
+            if (g.weighted && g.negativeWeights)
                 break;
         }
     }
     
-    std::vector<typename GraphType::Node> nodes(nVertices);
-    std::map<typename GraphType::Node, int> indices;
-    GraphType graph;
-    typedef typename GraphType::template ArcMap<double> WeightMap;
-    WeightMap weightMap(graph);
-    
+    std::vector<SmartDigraph::Node> nodes(nVertices);
     for (int i=0; i<nVertices; i++)
     {
-        typename GraphType::Node node = graph.addNode();
+        SmartDigraph::Node node = g.graph.addNode();
         nodes[i] = node;
-        indices[node] = i;
+        g.indices[node] = i;
     }
     
     for (int i=0; i<edges.nrow(); i++)
@@ -88,44 +79,42 @@ NumericMatrix findShortestPaths (const int nVertices, const IntegerMatrix &edges
         if (ISNA(weight) || weight == 0.0)
             continue;
         
-        typename GraphType::Arc arc = addEdge(graph, nodes[edges(i,0)-1], nodes[edges(i,1)-1]);
-        if (weighted)
-            weightMap[arc] = weight;
+        SmartDigraph::Arc arc = g.graph.addArc(nodes[edges(i,0)-1], nodes[edges(i,1)-1]);
+        if (g.weighted)
+            g.weights[arc] = weight;
+        
+        if (!g.directed)
+        {
+            SmartDigraph::Arc arc = g.graph.addArc(nodes[edges(i,1)-1], nodes[edges(i,0)-1]);
+            if (g.weighted)
+                g.weights[arc] = weight;
+        }
     }
-    
-    NumericMatrix result(nVertices, nVertices);
-    if (!weighted)
-    {
-        Bfs<GraphType> bfs(graph);
-        searchPaths(graph, bfs, indices, result);
-    }
-    else if (!negativeWeights)
-    {
-        Dijkstra<GraphType,WeightMap> dijkstra(graph, weightMap);
-        searchPaths(graph, dijkstra, indices, result);
-    }
-    else
-    {
-        BellmanFord<GraphType,WeightMap> bellmanFord(graph, weightMap);
-        searchPaths(graph, bellmanFord, indices, result);
-    }
-    
-    return result;
 }
 
 RcppExport SEXP shortestPaths (SEXP _nVertices, SEXP _edges, SEXP _weights, SEXP _directed)
 {
 BEGIN_RCPP
-    const bool directed = as<bool>(_directed);
     const int nVertices = as<int>(_nVertices);
-    IntegerMatrix edges(_edges);
-    NumericVector weights(_weights);
+    RGraph g;
+    createGraph(nVertices, IntegerMatrix(_edges), NumericVector(_weights), as<bool>(_directed), g);
     
-    NumericMatrix result;
-    if (directed)
-        result = findShortestPaths<SmartDigraph>(nVertices, edges, weights);
+    NumericMatrix result(nVertices, nVertices);
+    if (!g.weighted)
+    {
+        Bfs<SmartDigraph> bfs(g.graph);
+        searchPaths(g, bfs, result);
+    }
+    else if (!g.negativeWeights)
+    {
+        Dijkstra<SmartDigraph,WeightMap> dijkstra(g.graph, g.weights);
+        searchPaths(g, dijkstra, result);
+    }
     else
-        result = findShortestPaths<SmartGraph>(nVertices, edges, weights);
+    {
+        BellmanFord<SmartDigraph,WeightMap> bellmanFord(g.graph, g.weights);
+        searchPaths(g, bellmanFord, result);
+    }
     
     return result;
 END_RCPP
