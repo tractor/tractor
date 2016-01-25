4233a34f5afd131c4510c2bd528f099aa9999a06
Jon Clayden <code@clayden.org>
2016-01-25 16:07:39 +0000

    Conversions between MriImage and niftiImage

---
+++ tractor.reg/R/02_niftiImage.R
@@ -0,0 +1,55 @@
+setOldClass("niftiImage")
+
+setAs("MriImage", "niftiImage", function(from) {
+    if (from$isEmpty())
+        data <- array(0L, dim=from$getDimensions())
+    else
+        data <- as(from, "array")
+    
+    pixdim(data) <- from$getVoxelDimensions()
+    pixunits(data) <- from$getVoxelUnits()
+    
+    if (from$nTags() > 0)
+        image <- updateNifti(data, from$getTags())
+    else
+        image <- data
+    
+    if (from$isReordered())
+    {
+        origin <- (from$getOrigin() - 1) * abs(from$getVoxelDimensions())
+        xform <- diag(4)
+        if (from$getDimensionality() == 2)
+            xform[c(1,6)] <- c(-1,1) * abs(from$getVoxelDimensions())
+        else
+            xform[c(1,6,11)] <- c(-1,1,1) * abs(from$getVoxelDimensions()[1:3])
+        xform[1:3,4] <- c(1,-1,-1) * origin
+    }
+    else
+        xform <- from$getStoredXformMatrix()
+    
+    sform(image) <- qform(image) <- structure(xform, code=2L)
+    class(image) <- "niftiImage"
+    
+    return (image)
+})
+
+setAs("niftiImage", "MriImage", function(from) {
+    metadata <- dumpNifti(from)
+    defaults <- list(dim_info=0, intent_p1=0, intent_p2=0, intent_p3=0, intent_code=0, intent_name="", slice_start=0, slice_end=0, slice_code=0, cal_min=0, cal_max=0, slice_duration=0, toffset=0, aux_file="")
+    
+    tags <- list()
+    for (key in names(defaults))
+    {
+        if (metadata[[key]] != defaults[[key]])
+            tags[[key]] <- metadata[[key]]
+    }
+    
+    data <- as.array(from)
+    if (metadata$scl_slope != 1 || metadata$scl_inter != 0)
+        data <- data * metadata$scl_slope + metadata$scl_inter
+    
+    image <- asMriImage(unclass(data), imageDims=dim(from), voxelDims=pixdim(from), voxelDimUnits=pixunits(from), origin=worldToVoxel(c(0,0,0),from), tags=tags, reordered=FALSE)
+    image$setXform(xform(from))
+    
+    return (image)
+})
