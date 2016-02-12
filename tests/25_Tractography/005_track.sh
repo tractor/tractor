0993c89aacca754abeed416f01c436d969066e65
Jon Clayden <code@clayden.org>
2016-02-12 15:52:04 +0000

    Updating tractography tests

--- tests/25_Tractography/005_track.sh
+++ tests/25_Tractography/005_track.sh
@@ -1,4 +1,4 @@
 #@desc Checking that we can run tractography
-${TRACTOR} track data/session 50 59 33 PointType:R NumberOfSamples:50 TractName:tmp/tract CreateVolumes:true
+${TRACTOR} track data/session 50 59 33 PointType:R Streamlines:50 TractName:tmp/tract RequireMap:true
 ${TRACTOR} value tmp/tract 50 59 33
 rm -f tmp/tract*
