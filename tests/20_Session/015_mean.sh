3209cb1fbe8854d20b11d6fc801e49d6177d5bbb
Jon Clayden <code@clayden.org>
2016-02-12 15:30:10 +0000

    Test updates

--- tests/20_Session/015_mean.sh
+++ tests/20_Session/015_mean.sh
@@ -1,4 +1,5 @@
 #@desc Checking that we can extract the mean FA in a region
-${TRACTOR} mkroi data/session 50 59 33 PointType:R Width:3 ROIName:tmp/region
-${TRACTOR} mean tmp/region data/session Metric:FA AveragingMode:binary
+${TRACTOR} mkroi data/session@FA 50 59 33 PointType:R Width:3 ROIName:tmp/region
+${TRACTOR} mean data/session@FA tmp/region AveragingMode:binary
+${TRACTOR} mean data/session@FA AveragingMode:binary ThresholdLevel:0.2
 rm -f tmp/region.*
