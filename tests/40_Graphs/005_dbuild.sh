685abd9a8574e0bd1ded8243274c6eafa29992ea
Jon Clayden <code@clayden.org>
2016-02-13 22:42:14 +0000

    Replacing test streamline data with new-style .trk and .trkl files

--- tests/40_Graphs/005_dbuild.sh
+++ tests/40_Graphs/005_dbuild.sh
@@ -1,4 +1,4 @@
 #@desc Checking that we can build a graph from streamlines
-${TRACTOR} graph-build data/session TractName:data/streamlines/all GraphName:tmp/graph
+${TRACTOR} graph-build data/session TractName:data/streamlines/wm2gm GraphName:tmp/graph
 ${TRACTOR} peek tmp/graph
 rm -f tmp/graph.Rdata
