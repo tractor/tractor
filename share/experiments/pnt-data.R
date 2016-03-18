4973f57a079ada9d5aea84d5c6f525ad9eb03617
Jon Clayden <code@clayden.org>
2016-03-18 22:19:23 +0000

    Single-session/plough-compatible reworking of pnt-data (untested)

--- share/experiments/pnt-data.R
+++ share/experiments/pnt-data.R
@@ -1,91 +1,47 @@
 #@desc Create B-spline tract representations and calculate characteristics of interest for a set of seed points in one or more brain volumes. This is a prerequisite for training or using a tract matching model. For training, a specific list of seed points will likely be required, in which case PointType should also be set. For using a model, the SeedPointList need not be given, in which case a region of width SearchWidth voxels around the reference tract seed point will be used, subject to the specified AnisotropyThreshold. The TractName specified must match that given to the "pnt-ref" experiment. If the SessionNumbers option is used to select a subset of the data sets, manual collation using "pnt-collate" will be required.
+#@args session directory, [seed point]
 
-suppressPackageStartupMessages(require(tractor.session))
-suppressPackageStartupMessages(require(tractor.nt))
+library(tractor.session)
+library(tractor.nt)
 
 runExperiment <- function ()
 {
     tractName <- getConfigVariable("TractName", NULL, "character", errorIfMissing=TRUE)
-    sessionList <- getConfigVariable("SessionList", NULL, "character", errorIfMissing=TRUE)
-    seedPoint <- getConfigVariable("SeedPoint", NULL, "character")
-    seedList <- getConfigVariable("SeedPointList", NULL, "integer")
     pointType <- getConfigVariable("PointType", NULL, "character", validValues=c("fsl","r","mm"), errorIfInvalid=TRUE)
     searchWidth <- getConfigVariable("SearchWidth", 1)
     faThreshold <- getConfigVariable("AnisotropyThreshold", 0.2)
     nStreamlines <- getConfigVariable("Streamlines", 1000)
     datasetName <- getConfigVariable("DatasetName", "data")
-    sessionNumbers <- getConfigVariable("SessionNumbers", NULL, "character")
-    resume <- getConfigVariable("Resume", FALSE)
+    overwrite <- getConfigVariable("Overwrite", FALSE)
     
     reference <- getNTResource("reference", "pnt", list(tractName=tractName))
     
-    collateData <- TRUE
+    requireArguments("session directory")
     
-    if (!is.null(seedPoint) && !is.null(seedList))
-        report(OL$Error, "Only one of \"SeedPoint\" and \"SeedPointList\" should be given")
+    session <- attachMriSession(Arguments[1])
     
-    if (!is.null(seedPoint) || !is.null(seedList))
-    {
-        if (is.null(pointType))
-            report(OL$Error, "Point type must be specified with the seed point(s)")
-        
-        if (!is.null(seedList))
-            seedMatrix <- matrix(seedList, ncol=3, byrow=TRUE)
-        else
-        {
-            seedPoint <- splitAndConvertString(seedPoint, ",", "numeric", fixed=TRUE, errorIfInvalid=TRUE)
-            seedMatrix <- promote(seedPoint, byrow=TRUE)
-        }
-    }
+    if (isValidAs(Sys.getenv("TRACTOR_PLOUGH_ID"), "integer"))
+        fileName <- ensureFileSuffix(paste(datasetName,Sys.getenv("TRACTOR_PLOUGH_ID"),sep="."), "txt")
+    else
+        fileName <- ensureFileSuffix(datasetName, "txt")
     
-    if (is.null(sessionNumbers))
-        sessionNumbers <- seq_along(sessionList)
+    if (file.exists(fileName) && !overwrite)
+        report(OL$Info, "Dataset file exists - not overwriting it")
     else
     {
-        sessionNumbers <- splitAndConvertString(sessionNumbers, ",", "integer", fixed=TRUE, errorIfInvalid=TRUE)
-        collateData <- FALSE
-    }
-    
-    parallelApply(sessionNumbers, function (i) {
-        sessionDatasetName <- ensureFileSuffix(paste(datasetName,"_session",i,sep=""), "txt")
-        
-        if (resume && file.exists(sessionDatasetName))
-            report(OL$Info, "Using existing output for session ", i)
+        if (nArguments() > 1)
+        {
+            seed <- splitAndConvertString(Arguments[-1], ",", "numeric", fixed=TRUE, errorIfInvalid=TRUE)
+            if (is.null(pointType))
+                report(OL$Error, "Point type must be specified with the seed point")
+            seed <- round(changePointType(seed, session$getRegistrationTarget("diffusion",metadataOnly=TRUE), "r", pointType))
+        }
         else
-        {
-            report(OL$Info, "Calculating splines for session ", i)
-            
-            # Allow for out of bounds session numbers (used by pnt-data-sge)
-            if (length(sessionList) == 1)
-                currentSession <- attachMriSession(sessionList)
-            else
-                currentSession <- attachMriSession(sessionList[i])
-            
-            if (exists("seedMatrix"))
-                currentSeed <- round(changePointType(seedMatrix[i,], session$getRegistrationTarget("diffusion",metadataOnly=TRUE), "r", pointType))
-            else
-                currentSeed <- transformPointsToSpace(reference$getStandardSpaceSeedPoint(), currentSession, "diffusion", oldSpace="mni", pointType=reference$getSeedUnit(), outputVoxel=TRUE, nearest=TRUE)
-            
-            neighbourhood <- createNeighbourhoodInfo(centre=currentSeed, width=searchWidth)
-            splines <- calculateSplinesForNeighbourhood(currentSession, neighbourhood, reference, faThreshold, nStreamlines)
-            data <- createDataTableForSplines(splines, reference$getTract(), "knot", subjectId=i, neighbourhood=neighbourhood)
-            write.table(data, file=sessionDatasetName)
-        }
-    })
-    
-    if (collateData)
-    {
-        allData <- NULL
-        for (i in seq_along(sessionList))
-        {
-            sessionDatasetName <- ensureFileSuffix(paste(datasetName,"_session",i,sep=""), "txt")
-            data <- read.table(sessionDatasetName)
-            data$subject <- i
-
-            allData <- rbind(allData, data)
-        }
-
-        write.table(allData, file=ensureFileSuffix(datasetName,"txt"))
-        unlink(ensureFileSuffix(paste(datasetName,"_session",seq_along(sessionList),sep=""), "txt"))
+            seed <- transformPointsToSpace(reference$getStandardSpaceSeedPoint(), session, "diffusion", oldSpace="mni", pointType=reference$getSeedUnit(), outputVoxel=TRUE, nearest=TRUE)
+        
+        neighbourhood <- createNeighbourhoodInfo(centre=seed, width=searchWidth)
+        splines <- calculateSplinesForNeighbourhood(session, neighbourhood, reference, faThreshold, nStreamlines)
+        data <- createDataTableForSplines(splines, reference$getTract(), "knot", subjectId=i, neighbourhood=neighbourhood)
+        write.table(data, file=fileName)
     }
 }
