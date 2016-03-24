0f39e5b46e81dac63660932c94e4d1aadf2dbb09
Jon Clayden <code@clayden.org>
2016-03-24 18:12:28 +0000

    Switching parallel test to use plough

--- tests/00_Basics/015_parallel.sh
+++ tests/00_Basics/015_parallel.sh
@@ -1,2 +1,4 @@
-#@desc Checking that parallelised code works
-${TRACTOR} -p 2 hello-parallel | grep -v WARNING
+#@desc Checking that plough works
+echo "Run: [1, 2]" >tmp/config.yaml
+${PLOUGH} -C tmp/config.yaml -P 2 -q -z -i hello | grep -v WARNING
+rm -f tmp/config.yaml
--- tests/scripts/hello-parallel.R
+++
@@ -1,6 +0,0 @@
-runExperiment <- function ()
-{
-    parallelApply(1:2, function(i) {
-        cat("Hello, world!\n")
-    })
-}
