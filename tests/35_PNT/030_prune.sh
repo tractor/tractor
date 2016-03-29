08d632dde6fb6856d67d3b57526762d9581503a7
Jon Clayden <code@clayden.org>
2016-03-29 15:32:10 +0100

    Fix

--- tests/35_PNT/030_prune.sh
+++ tests/35_PNT/030_prune.sh
@@ -2,4 +2,4 @@
 ${TRACTOR} pnt-eval DatasetName:data/pnt/pnt_test_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results
 ${TRACTOR} pnt-prune data/session TractName:genu DatasetName:data/pnt/pnt_test_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results Streamlines:50 RandomSeed:1 | grep -v outside | grep -v rejected
 rm -f tmp/pnt_results.Rdata
-rm -f genu_session1.*
+rm -f genu.1.*
