3c6a4fac0519b684528ede39be84ee539897bd2a
Jon Clayden <code@clayden.org>
2016-03-29 11:53:42 +0100

    Updating tests and data

--- tests/35_PNT/020_eval.sh
+++ tests/35_PNT/020_eval.sh
@@ -1,6 +1,6 @@
 #@desc Checking that we can evaluate data against a model
 ${TRACTOR} pnt-eval DatasetName:data/pnt/pnt_test_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results
 ${TRACTOR} peek tmp/pnt_results
-${TRACTOR} pnt-interpret TractName:genu SessionList:data/session ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_test_data ResultsName:tmp/pnt_results Mode:location
-${TRACTOR} pnt-interpret TractName:genu SessionList:data/session ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_test_data ResultsName:tmp/pnt_results Mode:ratio | awk '{ print substr($1,1,6) }'
+${TRACTOR} pnt-interpret data/session TractName:genu ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_test_data ResultsName:tmp/pnt_results Mode:location
+${TRACTOR} pnt-interpret TractName:genu ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_test_data ResultsName:tmp/pnt_results Mode:ratio | awk '{ print substr($1,1,6) }'
 rm -f tmp/pnt_results.Rdata
--- tests/35_PNT/021_manual.sh
+++
@@ -1,6 +0,0 @@
-#@desc Checking evaluation against data without stored seed points
-${TRACTOR} pnt-eval DatasetName:data/pnt/pnt_noseed_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results
-${TRACTOR} peek tmp/pnt_results
-${TRACTOR} pnt-interpret TractName:genu SessionList:data/session ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_noseed_data ResultsName:tmp/pnt_results Mode:location
-${TRACTOR} pnt-interpret TractName:genu SessionList:data/session ModelName:data/pnt/pnt_model DatasetName:data/pnt/pnt_noseed_data ResultsName:tmp/pnt_results Mode:ratio | awk '{ print substr($1,1,6) }'
-rm -f tmp/pnt_results.Rdata
