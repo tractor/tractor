dda8603da1cd5e5ffd2d068a5c0dd9966f3cea97
Jon Clayden <code@clayden.org>
2016-04-25 12:11:50 +0100

    Replacing gmap usage in test

--- tests/35_PNT/022_viz.sh
+++ tests/35_PNT/022_viz.sh
@@ -1,8 +1,8 @@
-#@desc Checking visualisation and group mapping
+#@desc Checking visualisation and projection into MNI space
 ${TRACTOR} pnt-eval DatasetName:data/pnt/pnt_test_data ModelName:data/pnt/pnt_model ResultsName:tmp/pnt_results
 ${TRACTOR} pnt-viz data/session TractName:genu DatasetName:data/pnt/pnt_test_data ResultsName:tmp/pnt_results Streamlines:50
-${TRACTOR} gmap genu. SessionList:data/session TractName:genu ThresholdLevel:0.01 ThresholdRelativeTo:max
-${TRACTOR} value genu_group_map 46 78 42
+${TRACTOR} transform data/session genu.1 SourceSpace:diffusion TargetSpace:MNI
+${TRACTOR} apply genu.1_transformed 'ifelse(a>=0.01*max(a),1L,0L)' ResultName:genu.1_thresholded
+${TRACTOR} value genu.1_thresholded 46 78 42
 rm -f tmp/pnt_results.Rdata
-rm -f genu.1.*
-rm -f genu_group_map.*
+rm -f genu.1*
