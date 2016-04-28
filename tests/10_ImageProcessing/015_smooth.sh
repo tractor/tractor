af38df9c342b38b19324a279f904f0d860e27a64
Jon Clayden <code@clayden.org>
2016-04-28 18:02:11 +0100

    Test updates

--- tests/10_ImageProcessing/015_smooth.sh
+++ tests/10_ImageProcessing/015_smooth.sh
@@ -1,7 +1,6 @@
 #@desc Checking that image smoothing works
-cp data/session/tractor/diffusion/dti_FA.* tmp/
-${TRACTOR} smooth tmp/dti_FA 5.0 WidthType:sd
-${TRACTOR} value tmp/dti_FA_smoothed 61 55 30
-${TRACTOR} smooth tmp/dti_FA 5.0 WidthType:fwhm
-${TRACTOR} value tmp/dti_FA_smoothed 61 55 30
-rm -f tmp/dti_FA*
+${TRACTOR} smooth data/session@FA 5.0 WidthType:sd
+${TRACTOR} value dti_FA_smoothed 61 55 30
+${TRACTOR} smooth data/session@FA 5.0 WidthType:fwhm
+${TRACTOR} value dti_FA_smoothed 61 55 30
+rm -f dti_FA_smoothed.*
--- tests/10_ImageProcessing/020_morph.sh
+++ tests/10_ImageProcessing/020_morph.sh
@@ -1,7 +1,7 @@
 #@desc Checking mathematical morphology operations
-${TRACTOR} binarise data/session/tractor/diffusion/dti_FA tmp/dti_FA ThresholdLevel:0.5 ThresholdRelativeTo:nothing
+${TRACTOR} binarise data/session@FA tmp/dti_FA ThresholdLevel:0.5 ThresholdRelativeTo:nothing
 ${TRACTOR} morph tmp/dti_FA 3 Operation:dilate KernelShape:box WidthUnit:vox
-${TRACTOR} imagestats tmp/dti_FA_morphed Scope:nonzero
+${TRACTOR} imagestats dti_FA_morphed Scope:nonzero
 ${TRACTOR} morph tmp/dti_FA 3 Operation:closing KernelShape:diamond WidthUnit:vox
-${TRACTOR} imagestats tmp/dti_FA_morphed Scope:nonzero
-rm -f tmp/dti_FA*
+${TRACTOR} imagestats dti_FA_morphed Scope:nonzero
+rm -f dti_FA_morphed.* tmp/dti_FA.*
