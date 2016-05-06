84166f95f904ffd5f704db93dc2e99553a0d560f
Jon Clayden <code@clayden.org>
2016-05-06 14:50:38 +0100

    Dropping createCombinedGraphics(), which is no longer being used

--- tractor.base/R/zzz.R
+++ tractor.base/R/zzz.R
@@ -1,4 +1,7 @@
 #' @import methods ore reportr
+#' @importFrom grDevices col2rgb colorRamp dev.cur dev.off gray heat.colors rainbow
+#' @importFrom graphics image layout lines locator par plot strwidth text
+#' @importFrom stats na.omit
 NULL
 
 .onLoad <- function (libname, pkgname)
