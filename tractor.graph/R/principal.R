tractor/tractor

commit 39f11830448f21ba9d0016ba8a1f64e213dd5867

asGraph.matrix() no longer set vertex names

tractor.graph/R/principal.R
@@ -21,9 +21,9 @@ principalNetworks <- function (x, ...)
 }
 
 # Table of raw data, e.g. cortical thickness measurements
-principalNetworks.matrix <- function (x, ..., allVertexNames = NULL)
+principalNetworks.matrix <- function (x, ...)
 {
-    graph <- asGraph(cor(x), edgeList=FALSE, directed=FALSE, allVertexNames=allVertexNames)
+    graph <- asGraph(cor(x), edgeList=FALSE, directed=FALSE)
     principalGraphs <- calculatePrincipalGraphsForGraphs(graph, ...)
     principalGraphs$scores <- scale(x) %*% principalGraphs$eigenvectors
     
@@ -111,13 +111,13 @@ principalNetworks.list <- function (x, components = NULL, eigenvalueThreshold = 
     # Calculate residual association matrices after subtracting out higher components
     residualMatrices <- Reduce("-", fullMatrices, init=associationMatrix, accumulate=TRUE)
     residualMatrices <- residualMatrices[-1]
-    residualGraphs <- lapply(residualMatrices[components], asGraph, edgeList=FALSE, allVertexNames=x[[1]]$getVertexAttributes("name"))
+    residualGraphs <- lapply(residualMatrices[components], asGraph, edgeList=FALSE)
     residualGraphs <- lapply(residualGraphs, function(r) { r$setVertexLocations(x[[1]]$getVertexLocations(),x[[1]]$getVertexLocationUnit(),x[[1]]$getVertexLocationSpace()); r })
     names(residualGraphs) <- paste("PN", components, sep="")
     
-    verticesToKeep <- attr(loadings, "salient")
-    matrices <- lapply(components, function(i) fullMatrices[[i]][verticesToKeep[,i],verticesToKeep[,i]])
-    componentGraphs <- lapply(matrices, asGraph, edgeList=FALSE, allVertexNames=x[[1]]$getVertexAttributes("name"))
+    verticesToDrop <- setdiff(seq_len(x[[1]]$nVertices()), attr(loadings,"salient"))
+    matrices <- lapply(components, function(i) { x <- fullMatrices[[i]]; x[verticesToDrop,] <- x[,verticesToDrop] <- 0; x })
+    componentGraphs <- lapply(matrices, asGraph, edgeList=FALSE)
     componentGraphs <- lapply(componentGraphs, function(r) { r$setVertexLocations(x[[1]]$getVertexLocations(),x[[1]]$getVertexLocationUnit(),x[[1]]$getVertexLocationSpace()); r })
     names(componentGraphs) <- paste("PN", components, sep="")
     

share/experiments/graph-build.R
@@ -94,8 +94,8 @@ runExperiment <- function ()
         }
         
         report(OL$Info, "Creating and writing graph")
-        graph <- asGraph(edgeList, edgeList=TRUE, directed=FALSE, selfConnections=selfConnections, allVertexNames=targetMatches)
-        graph$setVertexAttributes(voxelCount=voxelCount, volume=volume)
+        graph <- asGraph(edgeList, edgeList=TRUE, directed=FALSE, selfConnections=selfConnections, nVertices=nRegions)
+        graph$setVertexAttributes(name=targetMatches, voxelCount=voxelCount, volume=volume)
         graph$setVertexLocations(regionLocations, "mm", paste(session$getDirectory(),"diffusion",sep=":"))
         graph$setEdgeAttributes(nStreamlines=nStreamlines, binaryFA=binaryFA, weightedFA=weightedFA, binaryMD=binaryMD, weightedMD=weightedMD, streamlineLength=streamlineLength, uniqueVoxels=uniqueVoxels, voxelVisits=voxelVisits)
         graph$serialise(graphName)
@@ -163,9 +163,9 @@ runExperiment <- function ()
         dimnames(adjacencyMatrix) <- list(targetMatches, targetMatches)
         
         report(OL$Info, "Creating and writing graph")
-        graph <- asGraph(adjacencyMatrix, directed=FALSE, selfConnections=selfConnections, allVertexNames=targetMatches)
+        graph <- asGraph(adjacencyMatrix, edgeList=FALSE, directed=FALSE, selfConnections=selfConnections)
         edges <- graph$getEdges()
-        graph$setVertexAttributes(voxelCount=voxelCount, volume=volume)
+        graph$setVertexAttributes(name=targetMatches, voxelCount=voxelCount, volume=volume)
         graph$setVertexLocations(regionLocations, "mm", paste(session$getDirectory(),"functional",sep=":"))
         graph$setEdgeAttributes(covariance=covariance[edges], correlation=correlation[edges], precision=precision[edges], partialCorrelation=partialCorrelation[edges])
         graph$serialise(graphName)

tractor.graph/R/05_graph.R
@@ -251,7 +251,7 @@ asGraph <- function (x, ...)
     UseMethod("asGraph")
 }
 
-asGraph.matrix <- function (x, edgeList = NULL, directed = NULL, selfConnections = TRUE, allVertexNames = NULL, ...)
+asGraph.matrix <- function (x, edgeList = NULL, directed = NULL, selfConnections = TRUE, nVertices = NULL, ...)
 {
     if (is.null(edgeList))
         edgeList <- (ncol(x) == 2)
@@ -261,7 +261,10 @@ asGraph.matrix <- function (x, edgeList = NULL, directed = NULL, selfConnections
         if (is.null(directed))
             report(OL$Error, "Directedness must be specified when creating a graph from an edge list")
         
-        nVertices <- max(x, length(allVertexNames))
+        if (is.null(nVertices))
+            nVertices <- max(x)
+        else if (max(x) > nVertices)
+            report(OL$Error, "At least one edge connects a nonexistent vertex")
         edges <- structure(x, dimnames=NULL)
         
         if (!directed)
@@ -286,38 +289,23 @@ asGraph.matrix <- function (x, edgeList = NULL, directed = NULL, selfConnections
             directed <- !isSymmetric(x)
         else if (directed == isSymmetric(x))
             flag(OL$Warning, "The \"directed\" argument does not match the symmetry of the matrix")
+        
+        if (is.null(nVertices))
+            nVertices <- ncol(x)
+        else if (ncol(x) != nVertices)
+            report(OL$Error, "The association matrix size does not match the number of vertices")
     
         if (!directed)
             x[lower.tri(x,diag=FALSE)] <- NA
         if (!selfConnections)
             diag(x) <- NA
     
-        if (is.null(allVertexNames))
-            allVertexNames <- union(rownames(x), colnames(x))
-        
-        nVertices <- max(ncol(x), length(allVertexNames))
-    
-        if (is.null(allVertexNames))
-        {
-            rowVertexLocs <- 1:nrow(x)
-            colVertexLocs <- 1:ncol(x)
-        }
-        else
-        {
-            rowVertexLocs <- match(rownames(x), allVertexNames)
-            colVertexLocs <- match(colnames(x), allVertexNames)
-        }
-    
         edges <- which(!is.na(x) & x != 0, arr.ind=TRUE)
         edgeWeights <- x[edges]
-        edges[,1] <- rowVertexLocs[edges[,1]]
-        edges[,2] <- colVertexLocs[edges[,2]]
         dimnames(edges) <- NULL
     }
     
     graph <- Graph$new(vertexCount=nVertices, edges=edges, edgeWeights=edgeWeights, directed=directed)
-    if (!is.null(allVertexNames))
-        graph$setVertexAttributes(name=allVertexNames)
     
     return (graph)
 }

commit 3c1aa8642c7e479f44181c1a154675d989cd75b8

Fixing principal networks bug introduced in commit 39f1183

tractor.graph/R/principal.R
@@ -103,22 +103,17 @@ principalNetworks.list <- function (x, components = NULL, eigenvalueThreshold = 
     fullMatrices <- lapply(1:nComponents, function(i) {
         m <- eigensystem$values[i] * (eigensystem$vectors[,i] %o% eigensystem$vectors[,i])
         m[is.na(associationMatrix)] <- NA
-        rownames(m) <- rownames(associationMatrix)
-        colnames(m) <- colnames(associationMatrix)
         return (m)
     })
     
     # Calculate residual association matrices after subtracting out higher components
     residualMatrices <- Reduce("-", fullMatrices, init=associationMatrix, accumulate=TRUE)
     residualMatrices <- residualMatrices[-1]
-    residualGraphs <- lapply(residualMatrices[components], asGraph, edgeList=FALSE)
-    residualGraphs <- lapply(residualGraphs, function(r) { r$setVertexLocations(x[[1]]$getVertexLocations(),x[[1]]$getVertexLocationUnit(),x[[1]]$getVertexLocationSpace()); r })
+    residualGraphs <- lapply(residualMatrices[components], function(m) x[[1]]$copy()$setAssociationMatrix(m))
     names(residualGraphs) <- paste("PN", components, sep="")
     
-    verticesToDrop <- setdiff(seq_len(x[[1]]$nVertices()), attr(loadings,"salient"))
-    matrices <- lapply(components, function(i) { x <- fullMatrices[[i]]; x[verticesToDrop,] <- x[,verticesToDrop] <- 0; x })
-    componentGraphs <- lapply(matrices, asGraph, edgeList=FALSE)
-    componentGraphs <- lapply(componentGraphs, function(r) { r$setVertexLocations(x[[1]]$getVertexLocations(),x[[1]]$getVertexLocationUnit(),x[[1]]$getVertexLocationSpace()); r })
+    verticesToKeep <- attr(loadings, "salient")
+    componentGraphs <- lapply(components, function(i) inducedSubgraph(x[[1]]$copy()$setAssociationMatrix(fullMatrices[[i]]), which(verticesToKeep[,i])))
     names(componentGraphs) <- paste("PN", components, sep="")
     
     loadings <- structure(loadings[,components], salient=attr(loadings,"salient")[,components])