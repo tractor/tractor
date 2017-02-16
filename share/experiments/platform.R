bd018a72c0eaa6871057268ec952c06e95cb2ad0
Jon Clayden <code@clayden.org>
2017-02-16 13:09:45 +0000

    Platform script now also lists all R packages and their versions

--- share/experiments/platform.R
+++ share/experiments/platform.R
@@ -24,8 +24,33 @@ runExperiment <- function ()
         tractorVersion <- readLines(file.path(Sys.getenv("TRACTOR_HOME"), "VERSION"))
     
     rBuild <- R.Version()
-    labels <- c(labels, "TractoR home directory", "TractoR version", "R version", "R build platform", "R package library")
+    labels <- c(labels, "TractoR home directory", "TractoR version", "R version", "R build platform", "R package library", "R packages")
     values <- c(values, Sys.getenv("TRACTOR_HOME"), tractorVersion[1], paste(rBuild$major,rBuild$minor,sep="."), rBuild$platform, .libPaths()[1])
+    
+    packageVersions <- installed.packages(.libPaths()[1])[,"Version"]
+    packageVersionStrings <- paste0(names(packageVersions), " (v", packageVersions, ")")
+    starting <- TRUE
+    for (i in seq_along(packageVersionStrings))
+    {
+        currentWidth <- nchar(packageVersionStrings[i], "width")
+        if (starting || width + currentWidth > 60)
+        {
+            if (!starting)
+            {
+                labels <- c(labels, "")
+                values <- c(values, paste0(currentLine,","))
+            }
+            width <- currentWidth
+            currentLine <- packageVersionStrings[i]
+        }
+        else
+        {
+            width <- width + currentWidth
+            currentLine <- paste0(currentLine, ", ", packageVersionStrings[i])
+        }
+        starting <- FALSE
+    }
+    values <- c(values, currentLine)
     
     labels <- c(labels, "FSL version")
     
