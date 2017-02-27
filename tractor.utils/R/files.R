018e67b4c11c54e7ef6ea4c9033e7511ef3037a7
Jon Clayden <code@clayden.org>
2017-02-27 11:46:21 +0000

    New option, more checking in copyDirectory()

--- tractor.utils/R/files.R
+++ tractor.utils/R/files.R
@@ -9,11 +9,13 @@
 #'   be created and filled with the contents of the source directory. If it
 #'   does exist, a new subdirectory with the same \code{\link{basename}} as the
 #'   source directory will be created, and the files put there (as with
-#'   \code{\link{file.copy}}).
-#' @param allFiles Logical value: if \code{TRUE}, hidden files (with names
-#'   beginning with a period) will be included in the copy.
-#' @param deleteOriginal Logical value: if \code{TRUE}, the source directory
-#'   will be deleted if the copy is successful.
+#'   \code{\link{file.copy}}, but see the \code{overwrite} parameter below).
+#' @param allFiles If \code{TRUE}, hidden files (with names beginning with a
+#'   period) will be included in the copy.
+#' @param deleteOriginal If \code{TRUE}, the source directory will be deleted
+#'   if the copy is successful.
+#' @param overwrite If \code{TRUE}, an existing target directory will be
+#'   removed and replaced with the contents of the source directory.
 #' @return \code{TRUE} if all files were copied successfully; \code{FALSE}
 #'   otherwise.
 #' 
@@ -27,7 +29,7 @@
 #' Journal of Statistical Software 44(8):1-18.
 #' \url{http://www.jstatsoft.org/v44/i08/}.
 #' @export
-copyDirectory <- function (from, to, allFiles = TRUE, deleteOriginal = FALSE)
+copyDirectory <- function (from, to, allFiles = TRUE, deleteOriginal = FALSE, overwrite = FALSE)
 {
     if (length(from) != 1 || length(to) != 1)
         report(OL$Error, "Source and target paths must be single strings")
@@ -35,9 +37,19 @@ copyDirectory <- function (from, to, allFiles = TRUE, deleteOriginal = FALSE)
     from <- expandFileName(from)
     to <- expandFileName(to)
     
+    if (!file.exists(from))
+        report(OL$Error, "The source directory does not exist")
+    else if (!file.info(from)$isdir)
+        report(OL$Error, "The source path does not point to a directory")
+    
     if (file.exists(to))
     {
-        if (!file.info(to)$isdir)
+        if (overwrite)
+        {
+            if (unlink(to, recursive=TRUE) != 0)
+                report(OL$Error, "Failed to delete the old directory")
+        }
+        else if (!file.info(to)$isdir)
             report(OL$Error, "Target path already exists but isn't a directory")
         else if (file.exists(file.path(to, basename(from))))
             report(OL$Error, "Subdirectory of the target directory matching the source name already exists - it will not be overwritten")
