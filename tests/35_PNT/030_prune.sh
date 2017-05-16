1bdaa5b9916d7c1293c7c778aed94e09d0d16793
Jon Clayden <code@clayden.org>
2017-05-16 14:09:12 +0100

    Test fixes

--- tests/35_PNT/030_prune.sh
+++ tests/35_PNT/030_prune.sh
@@ -1,3 +1,3 @@
 #@desc Checking streamline pruning
 ${TRACTOR} pnt-eval DatasetName:$TRACTOR_TEST_DATA/pnt/pnt_test_data ModelName:$TRACTOR_TEST_DATA/pnt/pnt_model ResultsName:pnt_results
-${TRACTOR} pnt-prune $TRACTOR_TEST_DATA/session TractName:genu DatasetName:$TRACTOR_TEST_DATA/pnt/pnt_test_data ModelName:$TRACTOR_TEST_DATA/pnt/pnt_model ResultsName:pnt_results Streamlines:50 RandomSeed:1 | grep -v outside | grep -v rejected
+${TRACTOR} pnt-prune $TRACTOR_TEST_DATA/session TractName:genu DatasetName:$TRACTOR_TEST_DATA/pnt/pnt_test_data ModelName:$TRACTOR_TEST_DATA/pnt/pnt_model ResultsName:pnt_results Streamlines:50 RandomSeed:1
--- tests/90_Foreign/010_avscale.sh
+++ tests/90_Foreign/010_avscale.sh
@@ -1,5 +1,7 @@
 #@desc Checking affine decomposition consistency between TractoR and FSL
 ${TRACTOR} -v1 decompose $TRACTOR_TEST_DATA/session 2
 echo "---"
+# This is slightly fragile, because we have to hard-code the path for FSL
+cd tmp/90_Foreign/010_avscale
 sed -n '12,15p' $TRACTOR_TEST_DATA/session/tractor/fdt/data.ecclog >avscale.mat
 ${FSLDIR}/bin/avscale --allparams avscale.mat | head -n 18
