23ca35ea5dd0780e38afde98df498ad2ddfff7b5
Jon Clayden <code@clayden.org>
2017-08-15 17:01:23 +0100

    The gradread script will now accept a bvals file as its third argument

--- share/tractor/experiments/gradread.R
+++ share/tractor/experiments/gradread.R
@@ -1,5 +1,5 @@
 #@args session directory, text file, b-values
-#@desc Read diffusion gradient directions from a text file, and update the specified session directory. The text file should contain the diffusion gradient vectors applied to the data set, given either one-per-column or one-per-row, normalised or unnormalised, and with or without zeroes for b=0 measurements. If exactly one b-value is given then zero is assumed to be used in addition; otherwise all b-values must be given.
+#@desc Read diffusion gradient directions from a text file, and update the specified session directory. The text file should contain the diffusion gradient vectors applied to the data set, given either one-per-column or one-per-row, normalised or unnormalised, and with or without zeroes for b=0 measurements. If exactly one b-value is given then zero is assumed to be used in addition; otherwise all b-values must be given. A file containing the b-values for each volume in order may also be given as the third argument.
 
 suppressPackageStartupMessages(require(tractor.session))
 
@@ -8,11 +8,20 @@ runExperiment <- function ()
     requireArguments("session directory", "text file", "nonzero b-value")
     session <- attachMriSession(Arguments[1])
     fileName <- Arguments[2]
-    bValues <- sort(as.numeric(Arguments[-(1:2)]))
     
-    if (length(bValues) == 1)
-        bValues <- c(0, bValues)
-    nBValues <- length(unique(bValues))
+    bValueFile <- FALSE
+    if (nArguments() == 3 && file.exists(Arguments[3]))
+    {
+        bValueFile <- TRUE
+        bvals <- as.vector(read.table(Arguments[3]))
+    }
+    else
+    {
+        bValues <- sort(as.numeric(Arguments[-(1:2)]))
+        if (length(bValues) == 1)
+            bValues <- c(0, bValues)
+        nBValues <- length(unique(bValues))
+    }
     
     metadata <- session$getImageByType("rawdata", "diffusion", metadataOnly=TRUE)
     
@@ -32,45 +41,48 @@ runExperiment <- function ()
     nVectorsImage <- metadata$getDimensions()[4]
     report(OL$Info, "Gradient vector matrix has #{nVectorsFile} columns; data image contains #{nVectorsImage} volumes")
     
-    bvals <- rep(NA, nVectorsImage)
-    report(OL$Info, "Loading raw data image")
-    dataImage <- session$getImageByType("rawdata", "diffusion")
-    
-    meanIntensities <- dataImage$apply(4, mean, na.rm=TRUE)
-    kMeansResult <- kmeans(meanIntensities, nBValues, nstart=3)
-    
-    if (nVectorsFile != nVectorsImage)
+    if (!bValueFile)
     {
-        if (min(bValues) != 0 || nBValues > 2)
-            report(OL$Error, "Partial gradient vector matrices can only be used when there is one nonzero b-value")
-        else if (!(nVectorsFile %in% kMeansResult$size))
-            report(OL$Error, "K-means cluster sizes are #{kMeansResult$size[1]} and #{kMeansResult$size[2]} - neither matches your text file")
+        bvals <- rep(NA, nVectorsImage)
+        report(OL$Info, "Loading raw data image")
+        dataImage <- session$getImageByType("rawdata", "diffusion")
+    
+        meanIntensities <- dataImage$apply(4, mean, na.rm=TRUE)
+        kMeansResult <- kmeans(meanIntensities, nBValues, nstart=3)
+    
+        if (nVectorsFile != nVectorsImage)
+        {
+            if (min(bValues) != 0 || nBValues > 2)
+                report(OL$Error, "Partial gradient vector matrices can only be used when there is one nonzero b-value")
+            else if (!(nVectorsFile %in% kMeansResult$size))
+                report(OL$Error, "K-means cluster sizes are #{kMeansResult$size[1]} and #{kMeansResult$size[2]} - neither matches your text file")
+            else
+            {
+                highBValueCluster <- which(kMeansResult$size == nVectorsFile)
+                lowBValueCluster <- ifelse(highBValueCluster==1, 2, 1)
+                if (kMeansResult$centers[highBValueCluster] > kMeansResult$centers[lowBValueCluster])
+                    report(OL$Warning, "High b-value images seem to have higher mean signal")
+            
+                highBValueIndices <- which(kMeansResult$cluster == highBValueCluster)
+                lowBValueIndices <- which(kMeansResult$cluster == lowBValueCluster)
+                report(OL$Info, "Image volume(s) ", implode(lowBValueIndices,sep=", ",finalSep=" and "), " appear(s) to have lower diffusion weighting")
+            
+                newBVecs <- matrix(NA, nrow=3, ncol=nVectorsImage)
+                newBVecs[,highBValueIndices] <- bvecs
+                newBVecs[,lowBValueIndices] <- 0
+                bvecs <- newBVecs
+                bvals[highBValueIndices] <- max(bValues)
+                bvals[lowBValueIndices] <- 0
+            }
+        }
         else
         {
-            highBValueCluster <- which(kMeansResult$size == nVectorsFile)
-            lowBValueCluster <- ifelse(highBValueCluster==1, 2, 1)
-            if (kMeansResult$centers[highBValueCluster] > kMeansResult$centers[lowBValueCluster])
-                report(OL$Warning, "High b-value images seem to have higher mean signal")
-            
-            highBValueIndices <- which(kMeansResult$cluster == highBValueCluster)
-            lowBValueIndices <- which(kMeansResult$cluster == lowBValueCluster)
-            report(OL$Info, "Image volume(s) ", implode(lowBValueIndices,sep=", ",finalSep=" and "), " appear(s) to have lower diffusion weighting")
-            
-            newBVecs <- matrix(NA, nrow=3, ncol=nVectorsImage)
-            newBVecs[,highBValueIndices] <- bvecs
-            newBVecs[,lowBValueIndices] <- 0
-            bvecs <- newBVecs
-            bvals[highBValueIndices] <- max(bValues)
-            bvals[lowBValueIndices] <- 0
-        }
-    }
-    else
-    {
-        clusterOrder <- order(kMeansResult$centers, decreasing=TRUE)
-        for (i in seq_len(nBValues))
-        {
-            indices <- which(kMeansResult$cluster == clusterOrder[i])
-            bvals[indices] <- bValues[i]
+            clusterOrder <- order(kMeansResult$centers, decreasing=TRUE)
+            for (i in seq_len(nBValues))
+            {
+                indices <- which(kMeansResult$cluster == clusterOrder[i])
+                bvals[indices] <- bValues[i]
+            }
         }
     }
     
