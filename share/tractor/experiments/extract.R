9ea9b3d13bcd7857cd8cb5fc1aed1cf7002c9fea
Jon Clayden <code@clayden.org>
2017-09-18 17:02:14 +0100

    Allowing regions to be specified by name in extract script

--- share/tractor/experiments/extract.R
+++ share/tractor/experiments/extract.R
@@ -1,23 +1,32 @@
 #@args image file, data value(s) to extract
 #@desc Create a volume which is the same as the input volume, except that all voxels whose value is in the list of data values to extract are set to one, and all other voxels are given the value zero. The base name of the output files is specified with the RegionName option.
 
-suppressPackageStartupMessages(require(tractor.session))
+library(tractor.session)
 
 runExperiment <- function ()
 {
     requireArguments("image file", "data value(s) to extract")
-    image <- readImageFile(Arguments[1])
-    values <- splitAndConvertString(Arguments[-1], ",", "numeric", fixed=TRUE, errorIfInvalid=TRUE)
+    values <- splitAndConvertString(Arguments[-1], ",", fixed=TRUE)
     
     regionName <- getConfigVariable("RegionName", "region")
     
-    selectionFunction <- function (x)
+    if (isValidAs(values, "numeric"))
     {
-        dims <- dim(x)
-        data <- ifelse(x %in% values, 1, 0)
-        dim(data) <- dims
-        return (data)
+        image <- readImageFile(Arguments[1])
+        values <- as.numeric(values)
+    }
+    else
+    {
+        imagePath <- identifyImageFileNames(Arguments[1])
+        if (file.exists(ensureFileSuffix(imagePath$fileStem, "lut")))
+        {
+            parcellation <- tractor.reg::readParcellation(Arguments[1])
+            image <- parcellation$image
+            values <- tractor.reg::matchRegions(values, parcellation)
+        }
+        else
+            report(OL$Error, "Data values are not numeric and there is no image lookup table")
     }
     
-    writeImageFile(image$map(selectionFunction), regionName)
+    writeImageFile(image$map(function(x) ifelse(x %in% values, 1L, 0L)), regionName)
 }
