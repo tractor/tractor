a8bd2275b82081521c1959b0397890f577aaf07f
Jon Clayden <code@clayden.org>
2017-12-14 17:47:10 +0000

    Rejigging file read/write tests

--- tractor.graph/tests/testthat/test-10-files.R
+++ tractor.graph/tests/testthat/test-10-files.R
@@ -1,26 +1,26 @@
-library(tractor.graph)
-library(igraph)
-context("files")
+context("Files")
 
-test_that("Graphs are saved and loaded successfully", {
-    createSymMatrix <- function(vector, dim)
-    {
-        m <- matrix(rep(0,dim*dim), nrow=dim)
-        m[lower.tri(m)] <- vector
-        m <- t(m)
-        m[lower.tri(m)] <- vector
-        return(m)
-    }
-    vector <- sample(c(runif(20,0,2), rep(0,25)))
-    m <- createSymMatrix(vector, 10)
-    t_graph <- asGraph.matrix(m)
-    volume <- runif(10, 0,5)
-    voxelCount <- round(runif(10,0,20))
-    names <- c("a","b","c","d","e","f","g","h","i","j")
+reportr::setOutputLevel(Warning)
+
+test_that("graphs are saved and loaded successfully", {
+    vertexAttribNames <- c("voxelCount", "volume", "name")
+    graph <- readGraphFile("graph")
     
-    t_graph$setVertexAttributes("voxelCount"=voxelCount, "volume"=volume, "names"=names)
+    expect_false(graph$isDirected())
+    expect_true(graph$isWeighted())
+    expect_true(graph$isSelfConnected())
+    expect_equal(round(graph$getEdgeDensity()*100), 62)
+    expect_true(all(vertexAttribNames %in% names(graph$getVertexAttributes())))
+    
     fileName <- tempfile()
-    writeGraphFile(t_graph, fileName, fileType = "csv")
-    t_retrieved <- readGraphFile(fileName)
-    expect_equivalent(t_retrieved, t_graph)
+    writeGraphFile(graph, fileName, fileType="csv")
+    retrievedGraph <- readGraphFile(fileName)
+    
+    expect_false(retrievedGraph$isDirected())
+    expect_true(retrievedGraph$isWeighted())
+    expect_true(retrievedGraph$isSelfConnected())
+    expect_equal(round(retrievedGraph$getEdgeDensity()*100), 62)
+    expect_true(all(vertexAttribNames %in% names(retrievedGraph$getVertexAttributes())))
+    
+    expect_equivalent(graph$getAssociationMatrix(), retrievedGraph$getAssociationMatrix())
 })
