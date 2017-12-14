f83271632a80dd99413f1fdaa403670bfb7fcb57
Jon Clayden <code@clayden.org>
2017-12-14 17:45:57 +0000

    Avoiding negative loops

--- tractor.graph/tests/testthat/test-32-negative.R
+++ tractor.graph/tests/testthat/test-32-negative.R
@@ -3,7 +3,8 @@ context("Fuzz (negative)")
 source("metrics.R")
 
 weights <- runif(20)
-weights[1] <- -weights[1]
+# All positive costs (inverse weights) must be at least as high in magnitude as this, to avoid negative loops
+weights[1] <- -1
 graph <- randomGraph(10, weights=weights, directed=TRUE)
 
 testMetricAgreement(graph)
