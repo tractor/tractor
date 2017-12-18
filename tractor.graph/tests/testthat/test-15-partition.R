366dd717de4c898d46db57e0e7474ba6bf444b68
Jon Clayden <code@clayden.org>
2017-12-18 16:35:07 +0000

    Tweaking subnetwork tests; merging into one file

--- tractor.graph/tests/testthat/test-15-partition.R
+++ tractor.graph/tests/testthat/test-15-partition.R
@@ -1,28 +1,38 @@
-library(tractor.graph)
-library(igraph)
-context("files")
+context("Subnetworks")
 
-test_that("Partitioning works fine", {
-    createSymMatrix <- function(vector, dim)
-    {
-        m <- matrix(rep(0,dim*dim), nrow=dim)
-        m[lower.tri(m)] <- vector
-        m <- t(m)
-        m[lower.tri(m)] <- vector
-        return(m)
-    }
-    vector <- sample(c(rep(1,20), rep(0,25)))
-    m <- createSymMatrix(vector, 10)
-    t_graph <- asGraph.matrix(m)
-    i_graph <- as(t_graph, "igraph")
-    t_partitioning <- partitionGraph(t_graph)
-    i_partitioning <- cluster_leading_eigen(i_graph)
+reportr::setOutputLevel(Warning)
+
+test_that("partitioning works as expected", {
+    graph <- readGraphFile("graph")
+    graph$map(function(x) ifelse(x==0,0L,1L))
     
-    matchList <- rep(NA, length(t_partitioning))
-    for(i in 1:length(t_partitioning)){
-        vector <- i_partitioning[[i]]
-        matchList[i] <- which(lapply(t_partitioning, function(i) all(match(i, vector))) == TRUE)
+    membershipMatches <- function (l, v) {
+        all(sapply(l, function(i) allEqual(v[i])))
     }
-    expect_false(anyNA(matchList))
-    expect_true(length(matchList)==length(unique(matchList)))
+    
+    t_partition <- partitionGraph(graph)
+    i_partition <- igraph::cluster_leading_eigen(as(graph, "igraph"))
+    expect_true(membershipMatches(t_partition, i_partition$membership))
+    
+    graph <- randomGraph(10, M=20)
+    t_partition <- partitionGraph(graph)
+    i_partition <- igraph::cluster_leading_eigen(as(graph, "igraph"))
+    expect_true(membershipMatches(t_partition, i_partition$membership))
+})
+
+test_that("principal network decomposition works", {
+    graph <- readGraphFile("graph")
+    pn <- principalNetworks(graph)
+    ref <- readRDS("p_network.rds")
+    
+    expect_equal(pn$eigenvalues, ref$eigenvalues)
+    expect_equal(pn$eigenvectors, ref$eigenvectors)
+    expect_equal(pn$loadings, ref$loadings)
+    expect_equal(pn$eigenvalueThreshold, ref$eigenvalueThreshold)
+    expect_equal(pn$edgeWeightThreshold, ref$edgeWeightThreshold)
+    expect_equal(pn$components, ref$components)
+    expect_equal(pn$scores, ref$scores)
+    expect_equal(pn$componentGraphs[[1]]$getConnectedVertices(), ref$componentGraphs[[1]]$getConnectedVertices())
+    expect_equal(pn$componentGraphs[[1]]$getEdgeDensity(), ref$componentGraphs[[1]]$getEdgeDensity())
+    expect_equal(pn$residualGraphs[[1]]$getConnectedVertices(), ref$residualGraphs[[1]]$getConnectedVertices())
 })
