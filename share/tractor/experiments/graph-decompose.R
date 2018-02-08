73bbf7022a71eed63787d8d5f26ee9bf3d404354
Jon Clayden <code@clayden.org>
2018-02-08 18:02:23 +0000

    Getting graph-decompose to also output a PartitionedGraph, and allow an reference partition to be used

--- share/tractor/experiments/graph-decompose.R
+++ share/tractor/experiments/graph-decompose.R
@@ -1,4 +1,4 @@
-#@desc Decompose a graph into parts by factoring or partitioning it. Available algorithms for this include "principal networks" (Clayden et al., PLoS ONE, 2013) and modularity maximisation (Newman, PNAS, 2006). An edge weight threshold (ignoring sign) can be applied to the results if required. Eigenvalue and loading thresholds apply to the principal networks approach only.
+#@desc Decompose a graph into parts by factoring or partitioning it. Available algorithms for this include "principal networks" (Clayden et al., PLoS ONE, 2013) and modularity maximisation (Newman, PNAS, 2006). An edge weight threshold (ignoring sign) can be applied to the results if required. Eigenvalue and loading thresholds apply to the principal networks approach only. If a reference partition is specified, it will be applied to the graph, and the Method option will be ignored.
 #@args graph file
 
 library(tractor.graph)
@@ -8,6 +8,7 @@ runExperiment <- function ()
     requireArguments("graph file")
     
     method <- getConfigVariable("Method", "principal-networks", validValues=c("principal-networks","modularity"))
+    refPartitionFile <- getConfigVariable("ReferencePartition", NULL, "character")
     edgeWeightThreshold <- getConfigVariable("EdgeWeightThreshold", 0)
     eigenvalueThreshold <- getConfigVariable("EigenvalueThreshold", NULL, "numeric")
     loadingThreshold <- getConfigVariable("LoadingThreshold", 0.1)
@@ -15,23 +16,33 @@ runExperiment <- function ()
     
     graphName <- implode(Arguments, " ")
     graph <- readGraphFile(graphName)
-    outputFileName <- paste(ensureFileSuffix(basename(graphName),NULL,strip=c("Rdata","csv")), "decomposed", sep="_")
+    fileStem <- ensureFileSuffix(basename(graphName), NULL, strip=c("Rdata","csv"))
     
-    if (method == "principal-networks")
+    if (!is.null(refPartitionFile))
     {
-        partition <- principalNetworks(graph)
-        partition$graph$mask(vertices=fx(abs(x) >= loadingThreshold), communities=fx(x >= eigenvalueThreshold))
-        printLoadings(partition$graph$getVertexWeights())
-        componentGraphs <- lapply(seq_len(partition$graph$nCommunities()), fi(thresholdEdges(partition$graph[[i]], edgeWeightThreshold, ignoreSign=TRUE)))
-        serialiseReferenceObject(componentGraphs, outputFileName)
+        report(OL$Info, "Applying the reference partition")
+        refPartition <- deserialiseReferenceObject(refPartitionFile)
+        partition <- applyPartition(refPartition, graph)
+        subgraphs <- lapply(partition$getCommunities(), fx(inducedSubgraph(graph,x)))
+    }
+    else if (method == "principal-networks")
+    {
+        report(OL$Info, "Decomposing into principal networks")
+        partition <- principalNetworks(graph)$graph
+        partition$mask(vertices=fx(abs(x) >= loadingThreshold), communities=fx(x >= eigenvalueThreshold))
+        printLoadings(partition$getVertexWeights())
+        subgraphs <- lapply(seq_len(partition$nCommunities()), fi(thresholdEdges(partition[[i]], edgeWeightThreshold, ignoreSign=TRUE)))
     }
     else
     {
         if (graph$isWeighted())
             graph <- thresholdEdges(graph, edgeWeightThreshold, ignoreSign=TRUE)
         
+        report(OL$Info, "Partitioning based on modularity")
         partition <- partitionGraph(graph, method=method)
-        subgraphs <- lapply(partition$getCommunities(), function(x) inducedSubgraph(graph,x))
-        serialiseReferenceObject(subgraphs, outputFileName)
+        subgraphs <- lapply(partition$getCommunities(), fx(inducedSubgraph(graph,x)))
     }
+    
+    partition$serialise(paste(fileStem, "partitioned", sep="_"))
+    serialiseReferenceObject(subgraphs, paste(fileStem,"decomposed",sep="_"))
 }
