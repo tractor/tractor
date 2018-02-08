50120c9501ed3272049a3a8507887b867a656524
Jon Clayden <code@clayden.org>
2018-02-08 16:08:38 +0000

    Getting list to arrange scripts into categories

--- share/tractor/experiments/list.R
+++ share/tractor/experiments/list.R
@@ -1,19 +1,51 @@
-#@desc List all available TractoR experiment scripts.
+#@desc List all available TractoR experiment scripts, arranged into categories.
 #@nohistory TRUE
 
 library(tractor.base)
 
 runExperiment <- function ()
 {
-    pathDirs <- file.path(Sys.getenv("TRACTOR_HOME"), "share", "tractor", "experiments")
-    pathDirs <- c(pathDirs, unlist(strsplit(Sys.getenv("TRACTOR_PATH"), ":", fixed=TRUE)))
+    patterns <- c("DICOM handling"="(^dicom)|(^age$)",
+                  "Image processing"="^(binarise|components|morph|smooth|trim)$",
+                  "General analysis"="(^apply$)|(^image)|(^extract$)|(^values$)|(^mean$)|(^mkroi$)|(^reshape$)",
+                  "Visualisation"="^(slice|view)$",
+                  "Registration"="^reg-",
+                  "Working with sessions"="^(clone|import|status|transform)$",
+                  "Diffusion processing"="dpreproc|(^dir)|(^grad)|bedpost|tensor|track|plotcorrections",
+                  "Structural processing"="^(deface|parcellate|freesurf)$",
+                  "Heuristic neighbourhood tractography (deprecated)"="^hnt-",
+                  "Probabilistic neighbourhood tractography"="^pnt-",
+                  "Graph and network analysis"="^graph")
     
+    standardDir <- file.path(Sys.getenv("TRACTOR_HOME"), "share", "tractor", "experiments")
+    files <- sort(list.files(standardDir, "\\.R$"))
+    filesWithoutExtensions <- ensureFileSuffix(files, NULL, strip="R")
+    cat("\n")
+    for (category in names(patterns))
+    {
+        cat(es("#{category}:\n  "))
+        match <- filesWithoutExtensions %~% patterns[category]
+        cat(filesWithoutExtensions[match], sep=", ", fill=TRUE)
+        filesWithoutExtensions <- filesWithoutExtensions[!match]
+        cat("\n")
+    }
+    
+    if (length(filesWithoutExtensions) > 0)
+    {
+        cat(es("Other scripts:\n  "))
+        cat(filesWithoutExtensions, sep=", ", fill=TRUE)
+        cat("\n")
+    }
+    
+    pathDirs <- unlist(strsplit(Sys.getenv("TRACTOR_PATH"), ":", fixed=TRUE))
     for (pathDir in pathDirs)
     {
         files <- list.files(pathDir, "\\.R$")
         filesWithoutExtensions <- ensureFileSuffix(files, NULL, strip="R")
-        cat(paste("Experiment scripts found in ", pathDir, ":\n", sep=""))
-        print(noquote(filesWithoutExtensions))
+        cat(es("Additional scripts found in #{pathDir}:\n  "), fill=TRUE)
+        cat(filesWithoutExtensions, sep=", ", fill=TRUE)
         cat("\n")
     }
+    
+    cat("For information on a particular script, run \"tractor -o <script>\"\n\n", file=stderr())
 }
