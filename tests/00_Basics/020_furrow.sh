c7350997e6a5415d38691228d627f967f853f5c3
Jon Clayden <code@clayden.org>
2018-02-14 12:35:24 +0000

    Updating furrow test

--- tests/00_Basics/020_furrow.sh
+++ tests/00_Basics/020_furrow.sh
@@ -1,5 +1,5 @@
 #@desc Checking that furrow works
 
-# Note that furrow has no -w option, so it is run from the tests directory
+# This is run from the tests directory
 # This is OK because it does not create any files
-${FURROW} -s data/session basename @FA
+${FURROW} basename data/session@FA
