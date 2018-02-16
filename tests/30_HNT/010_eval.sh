eaa6fba2e0d1127f4c26ae90ce1db3ca8113b74c
Jon Clayden <code@clayden.org>
2018-02-16 15:50:50 +0000

    Removing HNT experiments, reference tracts, tests

--- tests/30_HNT/010_eval.sh
+++
@@ -1,4 +0,0 @@
-#@desc Checking that we can run HNT with a standard reference tract
-${TRACTOR} hnt-eval $TRACTOR_TEST_DATA/session TractName:genu Streamlines:50 ResultsName:hnt_results | grep -v unique
-${TRACTOR} peek hnt_results
-${TRACTOR} hnt-interpret ResultsName:hnt_results Mode:location
