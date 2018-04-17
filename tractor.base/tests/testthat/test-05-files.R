6341c15750a6bf92e2ce57d9259e1cfc093ab03c
Jon Clayden <code@clayden.org>
2018-04-17 09:49:02 +0100

    More tests

--- tractor.base/tests/testthat/test-05-files.R
+++ tractor.base/tests/testthat/test-05-files.R
@@ -83,3 +83,21 @@ test_that("we can read and write MGH files", {
     rereadImage <- readImageFile(writePath$fileStem)
     expect_equal(image$setSource(NULL)$serialise(), rereadImage$setSource(NULL)$serialise(), tol=1e-4)
 })
+
+test_that("we can sort and read DICOM files", {
+    path <- system.file("extdata", "dicom", package="tractor.base")
+    temp <- tempdir()
+    file.copy(path, temp, recursive=TRUE)
+    path <- file.path(temp, "dicom")
+    
+    sortDicomDirectories(path, deleteOriginals=TRUE)
+    expect_equal(length(list.files(path)), 2L)
+    expect_true(all(c("4630_DTIb3000s5","6157_fl3D_t1_sag") %in% list.files(path)))
+    
+    image <- readDicomDirectory(file.path(path,"6157_fl3D_t1_sag"))$image
+    expect_equal(image$getDimensions(), c(2,224,256))
+    expect_equal(image$getVoxelDimensions(), rep(1,3L))
+    expect_equal(round(image$getOrigin()), c(19,95,135))
+    
+    unlink(path, recursive=TRUE)
+})
