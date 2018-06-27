8469d138667df6fe8bcba0d575d4fc05c89415e0
Jon Clayden <code@clayden.org>
2018-06-27 13:15:07 +0100

    Adding support for exclusions in the extract script

--- share/tractor/experiments/extract.R
+++ share/tractor/experiments/extract.R
@@ -8,25 +8,40 @@ runExperiment <- function ()
     requireArguments("image file", "data value(s) to extract")
     values <- splitAndConvertString(Arguments[-1], ",", fixed=TRUE)
     
+    exclusions <- getConfigVariable("Exclude", NULL, "character")
     regionName <- getConfigVariable("RegionName", "region")
     
-    if (isValidAs(values, "numeric"))
-    {
-        image <- readImageFile(Arguments[1])
-        values <- as.numeric(values)
-    }
-    else
+    namedValues <- !isValidAs(values, "numeric")
+    namedExclusions <- !is.null(exclusions) && !isValidAs(exclusions,"numeric")
+    if (namedValues || namedExclusions)
     {
         imagePath <- identifyImageFileNames(Arguments[1])
         if (file.exists(ensureFileSuffix(imagePath$fileStem, "lut")))
         {
             parcellation <- tractor.reg::readParcellation(Arguments[1])
             image <- parcellation$image
-            values <- tractor.reg::matchRegions(values, parcellation)
         }
         else
             report(OL$Error, "Data values are not numeric and there is no image lookup table")
     }
+    else
+        image <- readImageFile(Arguments[1])
     
-    writeImageFile(image$map(function(x) ifelse(x %in% values, 1L, 0L)), regionName)
+    if (namedValues)
+        values <- tractor.reg::matchRegions(values, parcellation)
+    else
+        values <- as.numeric(values)
+    
+    if (namedExclusions)
+        values <- setdiff(values, tractor.reg::matchRegions(exclusions,parcellation))
+    else if (!is.null(exclusions))
+        values <- setdiff(values, as.numeric(exclusions))
+    
+    if (length(values) == 0)
+        report(OL$Error, "There are no remaining data values to extract")
+    else
+    {
+        report(OL$Info, "Retaining values #{implode(round(values,2),', ',' and ')}")
+        writeImageFile(image$map(function(x) ifelse(x %in% values, 1L, 0L)), regionName)
+    }
 }
