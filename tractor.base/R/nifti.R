7b522a71ab792aa6ec1a1c0f3b7f1d144d1a22aa
Jon Clayden <code@clayden.org>
2018-08-06 10:21:04 +0100

    Adding ANALYZE-specific tweaks

--- tractor.base/R/nifti.R
+++ tractor.base/R/nifti.R
@@ -30,6 +30,19 @@ readNifti <- function (fileNames, volumes = NULL)
     version <- niftiVersion(fileNames$headerFile)
     if (version < 0)
         report(OL$Error, "#{fileNames$headerFile} does not seem to be a valid NIfTI header file")
+    else if (version == 0)
+    {
+        analyze <- analyzeHeader(fileNames$headerFile)
+        
+        flag(OL$Warning, "Image orientation for ANALYZE format is inconsistently interpreted")
+        xform <- diag(c(-abs(analyze$pixdim[2]), analyze$pixdim[3:4], 1))
+        if (analyze$orient > 0 && analyze$orient < 6)
+            orientation(xform) <- switch(analyze$orient, "LSA", "ASL", "LPS", "LIA", "AIL")
+        xform[1:3,4] <- -(xform[1:3,1:3] %*% (analyze$origin[1:3]-1))
+        
+        result$image <- RNifti::readNifti(fileNames$headerFile, volumes=volumes)
+        qform(result$image) <- structure(xform, code=2L)
+    }
     else if (version == 2)
     {
         # The gzfile function can handle uncompressed files too
