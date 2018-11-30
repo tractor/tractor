8c09c158920ac12e93a862a53810bb33bb97d5c4
Jon Clayden <code@clayden.org>
2018-11-30 17:52:24 +0000

    Using workflows

--- tractor.session/R/freesurfer.R
+++ tractor.session/R/freesurfer.R
@@ -3,35 +3,8 @@ runFreesurferForSession <- function (session, options = NULL)
     if (!is(session, "MriSession"))
         report(OL$Error, "Specified session is not an MriSession object")
     
-    subjectsDirectory <- Sys.getenv("SUBJECTS_DIR")
-    if (subjectsDirectory == "")
-        report(OL$Error, "Freesurfer does not seem to be fully set up: can't identify subjects directory")
-    
-    nT1wImages <- getImageCountForSession(session, "t1", "structural")
-    if (nT1wImages == 0)
-        report(OL$Error, "No T1-weighted images are currently stored in the session directory")
-    
     session$unlinkDirectory("freesurfer")
-    freesurferDirectory <- session$getDirectory("freesurfer", createIfMissing=TRUE)
-    imagesDirectory <- file.path(freesurferDirectory, "mri", "orig")
-    dir.create(imagesDirectory, recursive=TRUE)
-    
-    report(OL$Info, "Reading ", nT1wImages, " T1-weighted image(s) and converting to MGH format")
-    for (i in 1:nT1wImages)
-    {
-        image <- session$getImageByType("t1", "structural", index=i)
-        targetFileName <- file.path(imagesDirectory, sprintf("%03d",i))
-        writeImageFile(image, targetFileName, "MGH_GZ")
-    }
-    
-    subjectName <- basename(session$getDirectory())
-    file.symlink(freesurferDirectory, file.path(subjectsDirectory,subjectName))
-    
-    report(OL$Info, "Running Freesurfer (recon-all)...")
-    reconAllArguments <- paste("-subjid", subjectName, options, sep=" ")
-    execute("recon-all", reconAllArguments, silent=TRUE)
-    
-    unlink(file.path(subjectsDirectory,subjectName))
+    runWorkflow("reconall", session, FreesurferOptions=implode(options," "))
 }
 
 showImagesInFreeview <- function (imageFileNames, wait = FALSE, lookupTable = NULL, opacity = NULL)
