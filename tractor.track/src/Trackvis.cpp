c8e9865138b18b7e7c511ac6ed3e3827ae9e89a7
Jon Clayden <code@clayden.org>
2022-05-10 21:49:06 +0100

    Updating core Trackvis read/write code; commenting out subclasses for now

--- tractor.track/src/Trackvis.cpp
+++ tractor.track/src/Trackvis.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-std::map<int,char> TrackvisDataSink::orientationCodeMap = TrackvisDataSink::createOrientationCodeMap();
-
 // Name.........................Data type........Bytes....Comment..........................................................
 // 
 // id_string[6]                 char             6        ID string for track file. The first 5 characters must be "TRACK".
@@ -37,7 +35,7 @@ std::map<int,char> TrackvisDataSink::orientationCodeMap = TrackvisDataSink::crea
 // Source: Trackvis documentation (http://www.trackvis.org/docs/?subsect=fileformat)
 
 template <typename ElementType>
-ElementType getNthElement (std::vector<ElementType> vec, size_t n)
+static ElementType getNthElement (std::vector<ElementType> vec, size_t n)
 {
     std::nth_element(vec.begin(), vec.begin()+n, vec.end());
     return *(vec.begin()+n);
@@ -53,32 +51,34 @@ void TrackvisDataSource::readStreamline (Streamline &data)
         for (int32_t i=0; i<nPoints; i++)
         {
             ImageSpace::Point point;
-            binaryStream.readVector<float>(point, 3);
+            binaryStream.readPoint<float>(point);
+            
             // TrackVis indexes from the left edge of each voxel
-            points.push_back(point / grid.spacings() - 0.5);
-        
+            for (int i=0; i<3; i++)
+                point[i] = point[i] / space->pixdim[i] - 0.5;
+            
             if (nScalars > 0)
                 fileStream.seekg(4 * nScalars, ios::cur);
         }
         
         if (seedProperty >= 0)
         {
-            fileStream.seekg(4 * seedProperty, ios::cur);
+            binaryStream->seekg(4 * seedProperty, ios::cur);
             seed = static_cast<int>(binaryStream.readValue<float>());
         }
         if (nProperties > 0)
-            fileStream.seekg(4 * (nProperties-seedProperty-1), ios::cur);
+            binaryStream->seekg(4 * (nProperties-seedProperty-1), ios::cur);
         
         data = Streamline(vector<ImageSpace::Point>(points.rend()-seed-1, points.rend()),
                           vector<ImageSpace::Point>(points.begin()+seed, points.end()),
-                          Streamline::VoxelPointType,
-                          grid.spacings(),
+                          ImageSpace::VoxelPointType,
+                          space->pixdim,
                           false);
     }
     else
     {
         if (nProperties > 0)
-            fileStream.seekg(4 * nProperties, ios::cur);
+            binaryStream->seekg(4 * nProperties, ios::cur);
     }
     
     currentStreamline++;
@@ -109,8 +109,8 @@ void TrackvisDataSource::setup ()
     if (binaryStream.readString(6).compare(0,5,"TRACK") != 0)
         throw runtime_error("Trackvis file does not seem to have a valid magic number");
     
-    binaryStream.readVector<int16_t>(grid.dimensions());
-    binaryStream.readVector<float>(grid.spacings());
+    binaryStream.readArray<int16_t>(space->dim);
+    binaryStream.readArray<float>(space->pixdim);
     
     fileStream.seekg(12, ios::cur);
     nScalars = binaryStream.readValue<int16_t>();
@@ -123,8 +123,12 @@ void TrackvisDataSource::setup ()
         if (propertyName.compare(0,4,"seed") == 0)
             seedProperty = i;
     }
+    
+    array<ImageSpace::Element,16> elements;
     fileStream.seekg(440, ios::beg);
-    binaryStream.readMatrix<float>(grid.transform());
+    binaryStream.readArray<float>(elements);
+    space->transform = ImageSpace::Transform(elements.data());
+    
     fileStream.seekg(988, ios::beg);
     totalStreamlines = binaryStream.readValue<int32_t>();
     
@@ -164,109 +168,84 @@ void TrackvisDataSource::seek (const int n)
 
 void TrackvisDataSink::writeStreamline (const Streamline &data)
 {
-    int nPoints = data.nPoints();
-    Eigen::ArrayX3f points;
+    const size_t nPoints = data.nPoints();
+    std::vector<ImageSpace::Point> points = data.getPoints();
     
-    data.concatenatePoints(points);
-    
-    binaryStream.writeValue<int32_t>(nPoints);
-    for (int i=0; i<nPoints; i++)
+    outputStream.writeValue<int32_t>(nPoints);
+    for (size_t i=0; i<nPoints; i++)
     {
         // TrackVis indexes from the left edge of each voxel
-        Eigen::Array3f row;
-        if (data.getPointType() == Streamline::VoxelPointType)
-            row = (points.row(i) + 0.5) * grid.spacings().transpose();
-        else
-            row = points.row(i) + (0.5 * grid.spacings().transpose());
-        binaryStream.writeVector<float>(row);
+        ImageSpace::Point trkPoint = points[i];
+        for (int j=0; j<3; j++)
+            trkPoint[j] = (trkPoint[j] + 0.5) * space->pixdim[j];
+        outputStream.writePoint<float>(trkPoint);
     }
     
     // In practice, we should be able to squeeze the seed index into a float, but check
     const size_t seedIndex = data.getSeedIndex();
     if (seedIndex > 16777216)
         Rf_warning("Seed index %lu is not representable exactly as a 32-bit floating point value\n", seedIndex);
-    binaryStream.writeValue<float>(seedIndex);
+    outputStream.writeValue<float>(seedIndex);
     
     // Store termination reasons
-    binaryStream.writeValue(static_cast<float>(data.getLeftTerminationReason()));
-    binaryStream.writeValue(static_cast<float>(data.getRightTerminationReason()));
+    outputStream.writeValue(static_cast<float>(data.getLeftTerminationReason()));
+    outputStream.writeValue(static_cast<float>(data.getRightTerminationReason()));
 }
 
 void TrackvisDataSink::attach (const std::string &fileStem)
 {
-    if (fileStream.is_open())
-        fileStream.close();
+    outputStream.attach(fileStem + ".trk");
     
     if (append)
     {
-        fileStream.open((fileStem + ".trk").c_str(), ios::in | ios::out | ios::binary);
-        fileStream.seekg(988, ios::beg);
-        int32_t existingStreamlines;
-        fileStream.read((char *) &existingStreamlines, sizeof(int32_t));
-        totalStreamlines = existingStreamlines;
-        fileStream.seekp(0, ios::end);
+        BinaryInputStream inputStream(fileStem + ".trk");
+        inputStream->seekg(988, ios::beg);
+        inputStream.readValue<int32_t>(totalStreamlines);
+        
+        outputStream->seekp(0, ios::end);
         return;
     }
     
-    fileStream.open((fileStem + ".trk").c_str(), ios::out | ios::binary | ios::trunc);
+    outputStream->seekp(0);
+    outputStream.writeString("TRACK");
     
-    char magicNumber[6] = { 'T','R','A','C','K','\0' };
-    fileStream.seekp(0);
-    fileStream.write(magicNumber, 6);
+    outputStream.writeArray<int16_t>(space->dim);
+    outputStream.writeArray<float>(space->pixdim);
+    outputStream.writeValues<float>(0.0, 3);
     
-    binaryStream.writeVector<int16_t>(grid.dimensions(), 3);
-    binaryStream.writeVector<float>(grid.spacings(), 3);
-    binaryStream.writeValues<float>(0.0, 3);
+    outputStream.writeValue<int16_t>(0);
+    outputStream.writeValues<char>(0, 200);
+    outputStream.writeValue<int16_t>(3);
+    outputStream.writeString("seed", false);
+    outputStream.writeValues<char>(0, 16);
+    outputStream.writeString("Ltermcode", false);
+    outputStream.writeValues<char>(0, 11);
+    outputStream.writeString("Rtermcode", false);
+    outputStream.writeValues<char>(0, 151);
     
-    binaryStream.writeValue<int16_t>(0);
-    binaryStream.writeValues<char>(0, 200);
-    binaryStream.writeValue<int16_t>(3);
-    fileStream.write("seed", 4);
-    binaryStream.writeValues<char>(0, 16);
-    fileStream.write("Ltermcode", 9);
-    binaryStream.writeValues<char>(0, 11);
-    fileStream.write("Rtermcode", 9);
-    binaryStream.writeValues<char>(0, 151);
+    outputStream.writeArray(space->transform.begin(), 16);
+    outputStream.writeValues<char>(0, 444);
     
-    const Eigen::Matrix4f xform = grid.transform();
-    binaryStream.writeMatrix<float>(xform);
-    binaryStream.writeValues<char>(0, 444);
+    RNifti::NiftiImage::Xform xform(space->transform);
+    outputStream.writeString(xform.orientation());
+    outputStream.writeValues<char>(0, 4);
     
-    ::mat44 xformStruct;
-    for (int i=0; i<4; i++)
-    {
-        for (int j=0; j<4; j++)
-            xformStruct.m[i][j] = xform(i, j);
-    }
+    RNifti::NiftiImage::Xform::Submatrix rotationMatrix = xform.rotation();
+    outputStream.writeValue<float>(-rotationMatrix(0,0));
+    outputStream.writeValue<float>(-rotationMatrix(0,1));
+    outputStream.writeValue<float>(rotationMatrix(0,2));
+    outputStream.writeValue<float>(-rotationMatrix(1,0));
+    outputStream.writeValue<float>(-rotationMatrix(1,1));
+    outputStream.writeValue<float>(rotationMatrix(1,2));
+    outputStream.writeValues<char>(0, 8);
     
-    int icode, jcode, kcode;
-    nifti_mat44_to_orientation(xformStruct, &icode, &jcode, &kcode);
-    char orientation[4];
-    orientation[0] = TrackvisDataSink::orientationCodeMap[icode];
-    orientation[1] = TrackvisDataSink::orientationCodeMap[jcode];
-    orientation[2] = TrackvisDataSink::orientationCodeMap[kcode];
-    orientation[3] = '\0';
-    fileStream.write(orientation, 4);
-    binaryStream.writeValues<char>(0, 4);
-    
-    float qb, qc, qd, qfac;
-    nifti_mat44_to_quatern(xformStruct, &qb, &qc, &qd, NULL, NULL, NULL, NULL, NULL, NULL, &qfac);
-    ::mat44 rotationMatrix = nifti_quatern_to_mat44(qb, qc, qd, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, qfac);
-    binaryStream.writeValue<float>(-rotationMatrix.m[0][0]);
-    binaryStream.writeValue<float>(-rotationMatrix.m[0][1]);
-    binaryStream.writeValue<float>(rotationMatrix.m[0][2]);
-    binaryStream.writeValue<float>(-rotationMatrix.m[1][0]);
-    binaryStream.writeValue<float>(-rotationMatrix.m[1][1]);
-    binaryStream.writeValue<float>(rotationMatrix.m[1][2]);
-    binaryStream.writeValues<char>(0, 8);
-    
-    binaryStream.writeValue<int32_t>(0);
-    binaryStream.writeValue<int32_t>(2);
-    binaryStream.writeValue<int32_t>(1000);
+    outputStream.writeValue<int32_t>(0);
+    outputStream.writeValue<int32_t>(2);
+    outputStream.writeValue<int32_t>(1000);
     
     totalStreamlines = 0;
 }
-
+/*
 void LabelledTrackvisDataSink::attach (const std::string &fileStem)
 {
     TrackvisDataSink::attach(fileStem);
@@ -300,7 +279,7 @@ void LabelledTrackvisDataSink::attach (const std::string &fileStem)
         auxBinaryStream.writeString(element.second);
         auxBinaryStream.writeValue<char>(0);
     }
-}
+}*/
 
 void TrackvisDataSink::setup (const size_type &count, const_iterator begin, const_iterator end)
 {
@@ -308,7 +287,7 @@ void TrackvisDataSink::setup (const size_type &count, const_iterator begin, cons
     if (totalStreamlines > std::numeric_limits<int32_t>::max())
         throw std::runtime_error("Total streamline count exceeds the storage capacity of the Trackvis format");
 }
-
+/*
 void MedianTrackvisDataSink::setup (const size_type &count, const_iterator begin, const_iterator end)
 {
     size_t i;
@@ -393,14 +372,14 @@ void LabelledTrackvisDataSink::put (const Streamline &data)
     auxBinaryStream.writeValue<int32_t>(labels.size());
     for (std::set<int>::const_iterator it=labels.begin(); it!=labels.end(); it++)
         auxBinaryStream.writeValue<int32_t>(*it);
-}
+}*/
 
 void TrackvisDataSink::done ()
 {
-    fileStream.seekp(988);
-    binaryStream.writeValue<int32_t>(totalStreamlines);
+    outputStream->seekp(988);
+    outputStream.writeValue<int32_t>(totalStreamlines);
 }
-
+/*
 void LabelledTrackvisDataSink::done ()
 {
     TrackvisDataSink::done();
@@ -416,4 +395,4 @@ void MedianTrackvisDataSink::done ()
     
     fileStream.seekp(1000);
     writeStreamline(median);
-}
+}*/
--- tractor.track/src/Trackvis.h
+++ tractor.track/src/Trackvis.h
@@ -3,7 +3,7 @@
 
 #include <Rcpp.h>
 
-#include "Grid.h"
+#include "Image.h"
 #include "Streamline.h"
 #include "DataSource.h"
 #include "BinaryStream.h"
@@ -12,143 +12,105 @@ class TrackvisDataSource : public StreamlineFileSource
 {
 protected:
     int nScalars, nProperties, seedProperty;
-    ImageSpace grid;
     
     void readStreamline (Streamline &data);
     
 public:
     using StreamlineFileSource::StreamlineFileSource;
-        
+    
     void setup ();
     void get (Streamline &data);
     void seek (const int n);
     bool seekable () { return true; }
-    
-    ImageSpace getGrid3D () const { return grid; }
 };
 
 class TrackvisDataSink : public ImageSpaceEmbedded, public DataSink<Streamline>
 {
 protected:
-    std::fstream fileStream;
-    BinaryOutputStream binaryStream;
+    BinaryOutputStream outputStream;
     size_t totalStreamlines;
-    ImageSpace grid;
-    bool append;
+    bool append = false;
     
-    TrackvisDataSink ()
-        : append(false)
-    {
-        binaryStream.attach(&fileStream);
-    }
-    
-    TrackvisDataSink (const std::string &fileStem, const bool append = false)
-        : append(append)
-    {
-        binaryStream.attach(&fileStream);
-        attach(fileStem);
-    }
-    
-    TrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const bool append = false)
-        : grid(grid), append(append)
-    {
-        binaryStream.attach(&fileStream);
-        attach(fileStem);
-    }
+    // TrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const bool append = false)
+    //     : grid(grid), append(append)
+    // {
+    //     binaryStream.attach(&fileStream);
+    //     attach(fileStem);
+    // }
     
     void writeStreamline (const Streamline &data);
     
 public:
-    static std::map<int,char> orientationCodeMap;
-    
-    static std::map<int,char> createOrientationCodeMap ()
+    TrackvisDataSink (const std::string &fileStem, const bool append = false)
+        : append(append)
     {
-        std::map<int,char> map;
-        map[NIFTI_L2R] = 'R';
-        map[NIFTI_R2L] = 'L';
-        map[NIFTI_P2A] = 'A';
-        map[NIFTI_A2P] = 'P';
-        map[NIFTI_I2S] = 'S';
-        map[NIFTI_S2I] = 'I';
-        return map;
-    }
-    
-    virtual ~TrackvisDataSink ()
-    {
-        binaryStream.detach();
-        if (fileStream.is_open())
-            fileStream.close();
+        attach(fileStem);
     }
     
     virtual void attach (const std::string &fileStem);
     void setup (const size_type &count, const_iterator begin, const_iterator end);
     void done ();
-    ImageSpace getGrid3D () const { return grid; }
 };
 
-class BasicTrackvisDataSink : public TrackvisDataSink
-{
-public:
-    BasicTrackvisDataSink (const std::string &fileStem, const bool append = false)
-        : TrackvisDataSink(fileStem,append) {}
-    
-    BasicTrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const bool append = false)
-        : TrackvisDataSink(fileStem,grid,append) {}
-    
-    void put (const Streamline &data) { writeStreamline(data); }
-    void append (const Streamline &data)
-    {
-        writeStreamline(data);
-        totalStreamlines++;
-    }
-};
-
-class LabelledTrackvisDataSink : public TrackvisDataSink
-{
-protected:
-    std::ofstream auxFileStream;
-    BinaryOutputStream auxBinaryStream;
-    std::map<int,std::string> labelDictionary;
-    
-public:
-    LabelledTrackvisDataSink ()
-    {
-        auxBinaryStream.attach(&auxFileStream);
-    }
-    
-    // Don't call base class constructor explicitly here
-    LabelledTrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const std::map<int,std::string> labelDictionary)
-        : labelDictionary(labelDictionary)
-    {
-        this->grid = grid;
-        auxBinaryStream.attach(&auxFileStream);
-        attach(fileStem);
-    }
-    
-    ~LabelledTrackvisDataSink ()
-    {
-        auxBinaryStream.detach();
-        if (auxFileStream.is_open())
-            auxFileStream.close();
-    }
-    
-    void attach (const std::string &fileStem);
-    void put (const Streamline &data);
-    void done ();
-};
-
-class MedianTrackvisDataSink : public TrackvisDataSink
-{
-protected:
-    double quantile;
-    Streamline median;
-    
-public:
-    MedianTrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const double quantile = 0.99)
-        : TrackvisDataSink(fileStem,grid), quantile(quantile) {}
-    
-    void setup (const size_type &count, const_iterator begin, const_iterator end);
-    void done ();
-};
+// class BasicTrackvisDataSink : public TrackvisDataSink
+// {
+// public:
+//     using TrackvisDataSink::TrackvisDataSink;
+//     
+//     void put (const Streamline &data) { writeStreamline(data); }
+//     void append (const Streamline &data)
+//     {
+//         writeStreamline(data);
+//         totalStreamlines++;
+//     }
+// };
+// 
+// class LabelledTrackvisDataSink : public TrackvisDataSink
+// {
+// protected:
+//     std::ofstream auxFileStream;
+//     BinaryOutputStream auxBinaryStream;
+//     std::map<int,std::string> labelDictionary;
+//     
+// public:
+//     LabelledTrackvisDataSink ()
+//     {
+//         auxBinaryStream.attach(&auxFileStream);
+//     }
+//     
+//     // Don't call base class constructor explicitly here
+//     LabelledTrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const std::map<int,std::string> labelDictionary)
+//         : labelDictionary(labelDictionary)
+//     {
+//         this->grid = grid;
+//         auxBinaryStream.attach(&auxFileStream);
+//         attach(fileStem);
+//     }
+//     
+//     ~LabelledTrackvisDataSink ()
+//     {
+//         auxBinaryStream.detach();
+//         if (auxFileStream.is_open())
+//             auxFileStream.close();
+//     }
+//     
+//     void attach (const std::string &fileStem);
+//     void put (const Streamline &data);
+//     void done ();
+// };
+// 
+// class MedianTrackvisDataSink : public TrackvisDataSink
+// {
+// protected:
+//     double quantile;
+//     Streamline median;
+//     
+// public:
+//     MedianTrackvisDataSink (const std::string &fileStem, const ImageSpace &grid, const double quantile = 0.99)
+//         : TrackvisDataSink(fileStem,grid), quantile(quantile) {}
+//     
+//     void setup (const size_type &count, const_iterator begin, const_iterator end);
+//     void done ();
+// };
 
 #endif
