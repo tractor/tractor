0e78b8ae77609c1aa3fc42ff9c560f714f3e5b68
Jon Clayden <code@clayden.org>
2022-07-01 09:37:06 +0100

    Moving ImageSpace::toVoxel() into source file

---
+++ tractor.track/src/Image.cpp
@@ -0,0 +1,56 @@
+#include <Rcpp.h>
+
+#include "Image.h"
+
+Point ImageSpace::toVoxel (const Point &point, const PointType type, const RoundingType round = ConventionalRounding) const
+{
+    Point result;
+    
+    switch (type)
+    {
+        case VoxelPointType:
+        result = point;
+        break;
+        
+        case ScaledPointType:
+        for (int i=0; i<3; i++)
+            result[i] = point[i] / fabs(pixdim[i]);
+        break;
+        
+        case WorldPointType:
+        RNifti::NiftiImage::Xform::Vector4 padded(1.0);
+        for (int i=0; i<3; i++)
+            padded[i] = point[i];
+        const RNifti::NiftiImage::Xform::Vector4 product = transform.multiply(padded);
+        for (int i=0; i<3; i++)
+            result[i] = product[i];
+        break;
+    }
+    
+    switch (round)
+    {
+        case NoRounding:
+        break;
+        
+        case ConventionalRounding:
+        for (int i=0; i<3; i++)
+            result[i] = std::round(result[i]);
+        break;
+        
+        case ProbabilisticRounding:
+        for (int i=0; i<3; i++)
+        {
+            const Element ceiling = std::ceil(result[i]);
+            const Element floor = std::floor(result[i]);
+            const Element distance = result[i] - floor;
+            
+            // Sample in proportion to proximity, unless we're off the end of the image
+            const Element uniformSample = static_cast<Element>(R::unif_rand());
+            const bool chooseFloor = (uniformSample > distance && floor >= 0.0) || ceiling >= static_cast<Element>(dim[i]);
+            result[i] = chooseFloor ? floor : ceiling;
+        }
+        break;
+    }
+    
+    return result;
+}
--- tractor.track/src/Image.h
+++ tractor.track/src/Image.h
@@ -87,60 +87,9 @@ public:
         transform = source.xform().matrix();
     }
     
-    std::string orientation () const    { return RNifti::NiftiImage::Xform(transform).orientation(); }
+    std::string orientation () const { return RNifti::NiftiImage::Xform(transform).orientation(); }
     
-    Point toVoxel (const Point &point, const PointType type, const RoundingType round = ConventionalRounding) const
-    {
-        Point result;
-        
-        switch (type)
-        {
-            case VoxelPointType:
-            result = point;
-            break;
-            
-            case ScaledPointType:
-            for (int i=0; i<3; i++)
-                result[i] = point[i] / fabs(pixdim[i]);
-            break;
-            
-            case WorldPointType:
-            RNifti::NiftiImage::Xform::Vector4 padded(1.0);
-            for (int i=0; i<3; i++)
-                padded[i] = point[i];
-            const RNifti::NiftiImage::Xform::Vector4 product = transform.multiply(padded);
-            for (int i=0; i<3; i++)
-                result[i] = product[i];
-            break;
-        }
-        
-        switch (round)
-        {
-            case NoRounding:
-            break;
-            
-            case ConventionalRounding:
-            for (int i=0; i<3; i++)
-                result[i] = std::round(result[i]);
-            break;
-            
-            case ProbabilisticRounding:
-            for (int i=0; i<3; i++)
-            {
-                const Element ceiling = std::ceil(result[i]);
-                const Element floor = std::floor(result[i]);
-                const Element distance = result[i] - floor;
-                
-                // Sample in proportion to proximity, unless we're off the end of the image
-                const Element uniformSample = static_cast<Element>(R::unif_rand());
-                const bool chooseFloor = (uniformSample > distance && floor >= 0.0) || ceiling >= static_cast<Element>(dim[i]);
-                result[i] = chooseFloor ? floor : ceiling;
-            }
-            break;
-        }
-        
-        return result;
-    }
+    Point toVoxel (const Point &point, const PointType type, const RoundingType round = ConventionalRounding) const;
 };
 
 class ImageSpaceEmbedded
