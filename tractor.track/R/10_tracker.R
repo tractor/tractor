5c286bb3e4d0e1445d4b1bce44c4084bc36867d1
Jon Clayden <code@clayden.org>
2022-11-15 16:53:03 +0000

    Updating track experiment; removing run() method from Tracker

--- tractor.track/R/10_tracker.R
+++ tractor.track/R/10_tracker.R
@@ -29,31 +29,6 @@ Tracker <- setRefClass("Tracker", fields=list(model="DiffusionModel",pointer="ex
         
         .Call("setTrackerTargets", pointer, list(image=image,indices=indices,labels=labels), terminate, PACKAGE="tractor.track")
         return (.self)
-    },
-    
-    run = function (seeds, count, basename = threadSafeTempFile(), profileFun = NULL, requireMap = TRUE, requireStreamlines = FALSE, requireMedian = FALSE, terminateAtTargets = FALSE, jitter = TRUE)
-    {
-        if (nilModel(model))
-            report(OL$Error, "No diffusion model has been specified")
-        if (length(maskPath) == 0)
-            report(OL$Error, "No tracking mask has been specfied")
-        
-        mapPath <- streamlinePath <- medianPath <- NULL
-        if (requireMap)
-            mapPath <- ensureFileSuffix(basename, tractor.base:::.FileTypes$headerSuffixes[tractor.base:::.FileTypes$typeNames == getOption("tractorFileType")])
-        if (requireStreamlines)
-            streamlinePath <- basename
-        if (requireMedian)
-            medianPath <- paste(basename, "median", sep="_")
-        
-        seeds <- promote(seeds, byrow=TRUE)
-        
-        nRetained <- .Call("track", model$getPointer(), seeds, as.integer(count), maskPath, .self$targetInfo, options$rightwardsVector, as.integer(options$maxSteps), as.double(options$stepLength), as.double(options$curvatureThreshold), isTRUE(options$useLoopcheck), isTRUE(options$oneWay), isTRUE(terminateAtTargets), as.integer(filters$minTargetHits), as.numeric(filters$minLength), as.numeric(filters$maxLength), isTRUE(jitter), mapPath, streamlinePath, medianPath, profileFun, 0L, PACKAGE="tractor.track")
-        
-        if (nRetained < nrow(seeds) * count)
-            report(OL$Info, "#{nRetained} streamlines (#{signif(nRetained/(nrow(seeds)*count)*100,3)}%) were retained after filtering")
-        
-        return (basename)
     }
 ))
 
