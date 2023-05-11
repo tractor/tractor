57492b29fc21acfb64ddf5c6fa57dbfb5ec02138
Jon Clayden <code@clayden.org>
2023-05-11 22:23:21 +0100

    Updating apply test

--- tests/10_ImageProcessing/025_apply.sh
+++ tests/10_ImageProcessing/025_apply.sh
@@ -1,3 +1,7 @@
 #@desc Checking that we can apply general functions to images
 ${TRACTOR} apply $TRACTOR_TEST_DATA/session/tractor/diffusion/dti_FA "sum(a>1,na.rm=TRUE)"
 ${TRACTOR} apply $TRACTOR_TEST_DATA/session/tractor/diffusion/dti_FA "range(a,na.rm=TRUE)" | awk '{ print substr($1,1,6) }'
+
+${TRACTOR} mkroi $TRACTOR_TEST_DATA/session/tractor/diffusion/dti_FA 50 59 33 Width:3 ROIName:region
+${TRACTOR} apply $TRACTOR_TEST_DATA/session/tractor/diffusion/dti_FA region Combine:prod ResultName:FA_ROI
+${TRACTOR} imagestats FA_ROI
