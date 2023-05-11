488af4fb273ad83181067f28919eda83ad2b5a6a
Jon Clayden <code@clayden.org>
2023-05-11 22:23:11 +0100

    Making apply script read one image at a time when combining, default to identity function

--- share/tractor/experiments/apply.R
+++ share/tractor/experiments/apply.R
@@ -1,5 +1,5 @@
 #@args image file(s), R expression
-#@desc Generate a new image by applying an R expression to one or more input images. (The expression should be quoted to prevent it from being interpreted by the shell.) The original images are represented in the expression using the symbols a, b, c, etc. The number of input images is usually guessed, but can be specified explicitly with the Inputs variable. If the Combine option is given then the function will be applied to each image in turn, and then the results combined. If the result is a single value or a simple vector then it will be printed, otherwise a new image is created and written to file. Metadata for the result image will be based on the first image specified.
+#@desc Generate a new image by applying an R expression to one or more input images. (The expression should be quoted to prevent it from being interpreted by the shell.) The original images are represented in the expression using the symbols a, b, c, etc. The number of input images is usually guessed, but can be specified explicitly with the Inputs variable. If the Combine option is given then the function (which must be of only one argument, a, with the identity function as a default) will be applied to each image in turn, and then the results combined. If the result is a single value or a simple vector then it will be printed, otherwise a new image is created and written to file. Metadata for the result image will be based on the first image specified.
 #@example # Create a new image from the log of the voxelwise sum of two others
 #@example tractor apply image1 image2 "log(a+b)"
 #@example # Threshold and binarise two images, then add them together
@@ -27,35 +27,42 @@ runExperiment <- function ()
         report(OL$Verbose, "Guessing that there are #{nInputs} input images")
     }
     
-    if (nInputs < 1)
-        report(OL$Error, "At least one input image must be specified")
-    
-    images <- lapply(Arguments[1:nInputs], readImageFile)
+    assert(nInputs > 0, "At least one input image must be specified")
+    assert(is.null(combine) || nInputs <= 26, "Images beyond the 26th cannot be referred to by the expression", level=OL$Warning)
     expression <- implode(Arguments[-(1:nInputs)], sep=" ")
-    funString <- paste("function(", implode(letters[1:min(26,nInputs)],sep=","), ") { ", expression, " }", sep="")
-    report(OL$Debug, "Function string is `", funString, "`")
-    fun <- eval(parse(text=funString))
     
     if (is.null(combine))
+    {
+        assert(expression != "", "Expression cannot be empty")
+        funString <- paste("function(", implode(letters[1:min(26,nInputs)],sep=","), ") { ", expression, " }", sep="")
+        report(OL$Debug, "Function string is `", funString, "`")
+        fun <- eval(parse(text=funString))
+        
+        images <- lapply(Arguments[1:nInputs], readImageFile)
         result <- do.call(fun, lapply(images, function(x) x$getData()))
+    }
     else
     {
-        combine <- match.fun(combine)
-        data <- array(NA_real_, c(dim(images[[1]]),nInputs))
-        args <- alist(i=,j=,k=,t=,u=,v=,w=)[1:images[[1]]$getDimensionality()]
-        for (i in seq_len(nInputs))
-        {
-            values <- do.call(fun, list(images[[i]]$getData()))
-            data <- do.call("[<-", c(list(data),args,list(i,values)))
-        }
-        result <- apply(data, 1:images[[1]]$getDimensionality(), combine, na.rm=TRUE)
+        if (expression == "")
+            expression <- "a"
+        funString <- paste("function(a) { ", expression, " }", sep="")
+        report(OL$Debug, "Function string is `", funString, "`")
+        fun <- eval(parse(text=funString))
+        
+        result <- fun(readImageFile(Arguments[1])$getData())
+        combineFun <- match.fun(switch(combine, mean="+", sum="+", prod="*", min=pmin, max=pmax))
+        for (i in seq_len(nInputs-1))
+            result <- combineFun(result, fun(readImageFile(Arguments[i+1])$getData()))
+        if (combine == "mean")
+            result <- result / nInputs
     }
     
     if (length(result) == 1 || (!is.array(result) && !(length(result) %in% cumprod(dim(images[[1]])))))
         cat(paste0(implode(result,sep="\n"), "\n"))
     else
     {
-        resultImage <- asMriImage(result, images[[1]])
+        metadata <- readImageFile(Arguments[1], metadataOnly=TRUE)
+        resultImage <- asMriImage(result, metadata)
         writeImageFile(resultImage, resultName)
     }
 }
