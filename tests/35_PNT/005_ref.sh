62f12828985a3cf9e633a2d892a3867a2273b60d
Jon Clayden <code@clayden.org>
2023-07-03 14:35:27 +0100

    Update test for new warning wording in R 4.3.0 or 4.3.1

--- tests/35_PNT/005_ref.sh
+++ tests/35_PNT/005_ref.sh
@@ -1,3 +1,3 @@
 #@desc Checking that we can create a PNT reference tract
-${TRACTOR} pnt-ref $TRACTOR_TEST_DATA/session 50 59 33 Streamlines:50 | grep -v misleading
+${TRACTOR} pnt-ref $TRACTOR_TEST_DATA/session 50 59 33 Streamlines:50 | grep -v rank-deficient
 ${TRACTOR} peek tract_ref | grep -v session | grep -v spacing | sed '/seed/s/\.[0-9]*//g'
