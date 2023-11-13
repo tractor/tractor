35759554b5123c49cc66008379434dac08fd6528
Jon Clayden <code@clayden.org>
2023-11-13 16:23:03 +0000

    Add maximum spherical harmonic order as a variable in MRtrix workflows; support for unnamed args

--- share/tractor/workflows/dwi2fod.sh
+++ share/tractor/workflows/dwi2fod.sh
@@ -2,20 +2,23 @@
 
 #@command dwi2fod
 #@prereq @mrtrix/wmresp.txt
+#@variables Order
+
+[ -n "$Order" ] && TRACTOR_COMMAND_ARGS="$TRACTOR_COMMAND_ARGS -lmax $Order"
 
 algorithm=csd
 mtargs=""
 
 if ${FURROW} -z test -f @mrtrix/gmresp.txt; then
     algorithm=msmt_csd
-    mtargs="$mtargs $(${FURROW} -z echo @mrtrix/gmresp.txt) $(${FURROW} -z echo @gmfod)"
+    mtargs="$mtargs @mrtrix/gmresp.txt @gmfod.mif.gz"
 fi
 
 if ${FURROW} -z test -f @mrtrix/csfresp.txt; then
     algorithm=msmt_csd
-    mtargs="$mtargs $(${FURROW} -z echo @mrtrix/csfresp.txt) $(${FURROW} -z echo @csffod)"
+    mtargs="$mtargs @mrtrix/csfresp.txt @csffod.mif.gz"
 fi
 
 set -x
 
-${FURROW} ${TRACTOR_COMMAND} $algorithm -grad @mrtrix/directions.txt -mask @mrtrix/mask @mrtrix/data @mrtrix/wmresp.txt @wmfod.mif.gz "$mtargs"
+${FURROW} ${TRACTOR_COMMAND} $algorithm -force -grad @mrtrix/directions.txt -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt @wmfod.mif.gz "$mtargs"
--- share/tractor/workflows/dwi2response.sh
+++ share/tractor/workflows/dwi2response.sh
@@ -1,16 +1,17 @@
 #! /bin/bash -e
 
 #@command dwi2response
-#@variables Algorithm
+#@variables Algorithm, Order
 
 [ -n "$Algorithm" ] || Algorithm=tournier
+[ -n "$Order" ] && TRACTOR_COMMAND_ARGS="$TRACTOR_COMMAND_ARGS -lmax $Order"
 
 source ${TRACTOR_HOME}/share/tractor/workflows/helpers/create-mrtrix-files.sh
 
 set -x
 
 case "$Algorithm" in
-    tournier | tax) ${FURROW} ${TRACTOR_COMMAND} $Algorithm -grad @mrtrix/directions.txt -mask @mrtrix/mask @mrtrix/data @mrtrix/wmresp.txt ;;
-    dhollander) ${FURROW} ${TRACTOR_COMMAND} dhollander -grad @mrtrix/directions.txt -mask @mrtrix/mask @mrtrix/data @mrtrix/wmresp.txt @mrtrix/gmresp.txt @mrtrix/csfresp.txt ;;
+    tournier | tax) ${FURROW} ${TRACTOR_COMMAND} $Algorithm -force -grad @mrtrix/directions.txt -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt ;;
+    dhollander) ${FURROW} ${TRACTOR_COMMAND} dhollander -force -grad @mrtrix/directions.txt -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt @mrtrix/gmresp.txt @mrtrix/csfresp.txt ;;
     *) echo "Error: The specified algorithm is not valid" 1>&2; exit 1 ;;
 esac
