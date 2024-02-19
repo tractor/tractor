fdf6eac22d703f56a3dffa7ee237a517d13cd110
Jon Clayden <code@clayden.org>
2024-02-19 17:49:10 +0000

    Concessions for the current igraph 2.0.x series: add system dependency for CI, skip self-connected graph test

--- tractor.graph/tests/testthat/test-45-diagonal.R
+++ tractor.graph/tests/testthat/test-45-diagonal.R
@@ -4,4 +4,6 @@ source("properties.R")
 
 graph <- randomGraph(10, M=30, selfConnections=TRUE)
 
-testMetricAgreement(graph)
+# igraph 2.0.x currently seems to drop self-connections from the adjacency matrix
+if (packageVersion("igraph") < "2.0")
+    testMetricAgreement(graph)
