b80c02c33840938c606da9fe0197ca1176620922
Jon Clayden <code@clayden.org>
2025-01-08 22:02:23 +0000

    Generalising gradread (WIP)

--- share/tractor/experiments/gradread.R
+++ share/tractor/experiments/gradread.R
@@ -1,26 +1,32 @@
-#@args session directory, text file, b-values
+#@args destination, directions file, b-values
 #@desc Read diffusion gradient directions from a text file, and update the specified session directory. The text file should contain the diffusion gradient vectors applied to the data set, given either one-per-column or one-per-row, normalised or unnormalised, and with or without zeroes for b=0 measurements. If exactly one b-value is given then zero is assumed to be used in addition; otherwise all b-values must be given. A file containing the b-values for each volume in order may also be given as the third argument.
 
-suppressPackageStartupMessages(require(tractor.session))
+library(tractor.session)
 
 runExperiment <- function ()
 {
-    requireArguments("session directory", "text file", "b-values")
-    session <- attachMriSession(Arguments[1])
-    fileName <- Arguments[2]
+    requireArguments("destination", "directions file", "b-values")
     
-    bValueFile <- FALSE
-    if (nArguments() == 3 && file.exists(Arguments[3]))
-    {
-        bValueFile <- TRUE
-        bvals <- as.vector(read.table(Arguments[3]))
-    }
+    destinationStem <- ensureFileSuffix(Arguments["destination"], NULL)
+    # True if destination exists and is a directory - assume this is a session
+    if (isTRUE(file.info(Arguments["destination"])$isdir))
+        destination <- attachMriSession(Arguments["destination"])
+    # If the target matches an image, we will adopt a standard suffix
+    else if (imageFileExists(destinationStem))
+        destination <- Arguments["destination"]
+    # Otherwise take the destination path literally, with whatever suffix it has
+    else
+        destination <- I(Arguments["destination"])
+    
+    sourceFile <- Arguments["directions file"]
+    
+    if (nArguments() == 3 && file.exists(Arguments["b-values"]))
+        bValues <- Arguments["b-values"]
     else
     {
         bValues <- sort(as.numeric(Arguments[-(1:2)]))
         if (length(bValues) == 1)
             bValues <- c(0, bValues)
-        nBValues <- length(unique(bValues))
     }
     
     bvecs <- as.matrix(read.table(fileName))
