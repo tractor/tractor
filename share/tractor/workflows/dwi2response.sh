4bc498a249add9961298a0df153b4e6a23660c89
Jon Clayden <code@clayden.org>
2025-01-14 16:07:14 +0000

    Use -fslgrad for MRtrix workflows, not -grad (since TractoR and MRtrix's format is not identical)

--- share/tractor/workflows/dwi2response.sh
+++ share/tractor/workflows/dwi2response.sh
@@ -11,7 +11,7 @@ source ${TRACTOR_HOME}/share/tractor/workflows/helpers/create-mrtrix-files.sh
 set -x
 
 case "$Algorithm" in
-    tournier | tax) ${FURROW} ${TRACTOR_COMMAND} $Algorithm -force -grad @mrtrix/directions.txt -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt ;;
-    dhollander) ${FURROW} ${TRACTOR_COMMAND} dhollander -force -grad @mrtrix/directions.txt -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt @mrtrix/gmresp.txt @mrtrix/csfresp.txt ;;
+    tournier | tax) ${FURROW} ${TRACTOR_COMMAND} $Algorithm -force -fslgrad @mrtrix/bvecs @mrtrix/bvals -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt ;;
+    dhollander) ${FURROW} ${TRACTOR_COMMAND} dhollander -force -fslgrad @mrtrix/bvecs @mrtrix/bvals -mask @mrtrix/mask ${TRACTOR_COMMAND_ARGS} @mrtrix/data @mrtrix/wmresp.txt @mrtrix/gmresp.txt @mrtrix/csfresp.txt ;;
     *) echo "Error: The specified algorithm is not valid" 1>&2; exit 1 ;;
 esac
