74e1d89bca9d4e900628f1a4b30a2205c74b325a
Jon Clayden <code@clayden.org>
2025-01-14 16:04:30 +0000

    Allow for a session source and/or no third argument; explicitly pass arguments through resolvePath()

--- share/tractor/experiments/gradread.R
+++ share/tractor/experiments/gradread.R
@@ -1,24 +1,30 @@
-#@args destination, directions file, b-values
+#@args destination, directions file, [b-values]
 #@desc Read diffusion gradient directions from a text file, and update the specified session directory. The text file should contain the diffusion gradient vectors applied to the data set, given either one-per-column or one-per-row, normalised or unnormalised, and with or without zeroes for b=0 measurements. If exactly one b-value is given then zero is assumed to be used in addition; otherwise all b-values must be given. A file containing the b-values for each volume in order may also be given as the third argument.
 
 library(tractor.session)
 
 runExperiment <- function ()
 {
-    requireArguments("destination", "directions file", "b-values")
+    requireArguments("destination", "directions file")
     
     # True if destination exists and is a directory - assume this is a session
     if (isTRUE(file.info(Arguments["destination"])$isdir))
         destination <- attachMriSession(Arguments["destination"])$getImageFileNameByType("rawdata", "diffusion")
-    # If the target matches an image, we will adopt a standard suffix
-    else if (imageFileExists(destinationStem))
-        destination <- Arguments["destination"]
-    # Otherwise take the destination path literally, with whatever suffix it has
     else
-        destination <- I(Arguments["destination"])
+    {
+        # If the target matches an image, we will adopt a standard suffix
+        # Otherwise take the destination path literally, with whatever suffix it has
+        destinationStem <- ensureFileSuffix(resolvePath(Arguments["destination"]), NULL)
+        if (imageFileExists(destinationStem))
+            destination <- destinationStem
+        else
+            destination <- I(resolvePath(Arguments["destination"]))
+    }
     
-    if (nArguments() == 3 && file.exists(Arguments["b-values"]))
-        bValues <- Arguments["b-values"]
+    if (nArguments() < 3)
+        bValues <- NULL
+    else if (nArguments() == 3 && file.exists(Arguments[3]))
+        bValues <- Arguments[3]
     else
     {
         bValues <- sort(as.numeric(Arguments[-(1:2)]))
@@ -26,7 +32,11 @@ runExperiment <- function ()
             bValues <- c(0, bValues)
     }
     
-    scheme <- readDiffusionScheme(Arguments["directions file"], bValues, ensureFileSuffix(destination,NULL))
+    # If the source is a directory assume it's a session
+    if (isTRUE(file.info(Arguments["directions file"])$isdir))
+        scheme <- attachMriSession(Arguments["directions file"])$getDiffusionScheme(unrotated=TRUE)
+    else
+        scheme <- readDiffusionScheme(Arguments["directions file"], bValues, ensureFileSuffix(destination,NULL))
     
     report(OL$Info, "Writing gradient direction files")
     scheme$writeToFile(destination)
