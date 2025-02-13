4c71070b9ac4fd19aac73b4dea2753dc7a7cf97a
Jon Clayden <code@clayden.org>
2025-02-13 11:12:55 +0000

    Document CSD script; add "auto" option for the sub-algorithm, which considers the number of shells

--- share/tractor/experiments/csd.R
+++ share/tractor/experiments/csd.R
@@ -1,3 +1,4 @@
+#@desc Run constrained spherical deconvolution (CSD) for the specified session using MRtrix, which must be installed and accessible for this to work. The default session is the current working directory. CSD works by assuming that the observed signal is a weighted combination of one or more copies of a fixed "response function" with different orientations; by estimating the response function(s) and then "deconvolving" the signal, the putative distribution of fibre directions can be recovered. These outputs are represented using the real spherical harmonic basis functions up to the specified maximum (even) order. There are several sub-algorithms available, which are detailed in the MRtrix documentation for the "dwi2response" binary. By default this script will select "dhollander" if the data has more than one nonzero b-value, producing response functions for multiple tissue types; otherwise it will choose "tournier".
 #@args [session directory]
 
 library(tractor.session)
@@ -6,8 +7,15 @@ runExperiment <- function ()
 {
 	session <- attachMriSession(ifelse(nArguments()==0, ".", Arguments[1]))
 	
-	algorithm <- getConfigVariable("Algorithm", "tournier", validValues=c("tournier", "tax", "dhollander"))
+	algorithm <- getConfigVariable("Algorithm", "auto", validValues=c("auto", "tournier", "tax", "dhollander"))
 	order <- getConfigVariable("Order", NULL, "integer")
+	
+	if (algorithm == "auto")
+	{
+		nShells <- session$getDiffusionScheme()$nShells()
+		algorithm <- ifelse(nShells > 1, "dhollander", "tournier")
+		report(OL$Info, "Choosing \"#{algorithm}\" CSD algorithm for #{nShells}-shell data")
+	}
 	
 	mrtrixDir <- session$getDirectory("mrtrix")
 	if (!file.exists(file.path(mrtrixDir, "wmresp.txt")))
