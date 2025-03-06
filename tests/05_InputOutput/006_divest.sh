046ecb67d35ee76752014b587a8a34240ed3ed35
Jon Clayden <code@clayden.org>
2025-03-06 15:53:12 +0000

    Update divest integration test

--- tests/05_InputOutput/006_divest.sh
+++ tests/05_InputOutput/006_divest.sh
@@ -1,4 +1,4 @@
 #@desc Checking the "divest" DICOM back-end
 ${TRACTOR} dicomsort $TRACTOR_TEST_DATA/images/dicom Method:divest Nested:false DeleteOriginals:false | grep -v dcm2niix
 ${TRACTOR} dicomread . Method:divest Interactive:false FileNames:metadata | grep -v dcm2niix
-${FURROW} cat *.tags
+cat *.tags | grep -v Version
