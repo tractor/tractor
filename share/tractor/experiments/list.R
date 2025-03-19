32395a1f8251570e1f0e7dca7f451af30450bbfc
Jon Clayden <code@clayden.org>
2025-03-19 17:34:46 +0000

    Update list script for the Descriptions:false case

--- share/tractor/experiments/list.R
+++ share/tractor/experiments/list.R
@@ -43,11 +43,35 @@ runExperiment <- function ()
     else
     {
         colWidths <- pmax(nameLengths + 1, contentLengths + 2)
-        width <- colWidths[1]
-        for (i in seq_along(groups)[-1])
+        width <- cols <- 0
+        lines <- character(0L)
+        for (i in seq_along(groups))
         {
+            if (width + colWidths[i] + 2 > screenWidth)
+            {
+                cat(paste(lines, collapse="\n"), "\n\n", sep="")
+                width <- cols <- 0
+                lines <- character(0L)
+            }
             
+            lineCountMismatch <- length(groups[[i]]) + 1 - length(lines)
+            if (lineCountMismatch > 0)
+                lines <- c(lines, rep(paste(rep(" ",width+2*cols),collapse=""), lineCountMismatch))
+            lines[1] <- paste0(lines[1], colour(groupNames[i],"cyan","bold"), ":  ", paste0(rep(" ", colWidths[i] - nameLengths[i]),collapse=""))
+            
+            padding <- colWidths[i] - nchar(groups[[i]]) + 2
+            for (j in seq_along(groups[[i]]))
+                lines[j+1] <- paste0(lines[j+1], "  ", groups[[i]][j], paste0(rep(" ", padding[j]), collapse=""))
+            
+            if (lineCountMismatch < 0)
+                lines[(length(lines)+lineCountMismatch):length(lines)] <- paste0(lines[(length(lines)+lineCountMismatch):length(lines)], paste0(rep(" ", colWidths[i]+2, collapse="")))
+            
+            width <- width + colWidths[i]
+            cols <- cols + 1
         }
+        
+        if (length(lines) > 0)
+            cat(paste(lines, collapse="\n"), "\n\n", sep="")
     }
     
     cat("For information on a particular script, run \"tractor -o <script>\"\n\n", file=stderr())
