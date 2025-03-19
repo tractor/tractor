156b20e25fa0453acb8275c696a76104087eeb91
Jon Clayden <code@clayden.org>
2025-03-19 18:48:48 +0000

    Comments and fixes

--- share/tractor/experiments/list.R
+++ share/tractor/experiments/list.R
@@ -42,37 +42,40 @@ runExperiment <- function ()
     }
     else
     {
+        # The width required is the greater of the group name plus a colon and the script names plus left-padding
         colWidths <- pmax(nameLengths + 1, contentLengths + 2)
-        width <- cols <- 0
+        width <- 0
         lines <- character(0L)
         for (i in seq_along(groups))
         {
+            # Check if there is space for the next column (plus padding)
+            # If not, write out and reset the buffer
             if (width + colWidths[i] + 2 > screenWidth)
             {
                 cat(paste(lines, collapse="\n"), "\n\n", sep="")
-                width <- cols <- 0
+                width <- 0
                 lines <- character(0L)
             }
             
+            # Check whether there are more or less lines than needed for this group and its header
             lineCountMismatch <- length(groups[[i]]) + 1 - length(lines)
             if (lineCountMismatch > 0)
-                lines <- c(lines, rep(paste(rep(" ",width+2*cols),collapse=""), lineCountMismatch))
-            lines[1] <- paste0(lines[1], colour(groupNames[i],"cyan","bold"), ":  ", paste0(rep(" ", colWidths[i] - nameLengths[i]),collapse=""))
+                lines <- c(lines, rep(paste(rep(" ",width),collapse=""), lineCountMismatch))
+            lines[1] <- paste0(lines[1], colour(groupNames[i],"cyan","bold"), ":  ", paste0(rep(" ", colWidths[i] - nameLengths[i] - 1),collapse=""))
             
-            padding <- colWidths[i] - nchar(groups[[i]]) + 2
+            padding <- colWidths[i] - nchar(groups[[i]])
             for (j in seq_along(groups[[i]]))
                 lines[j+1] <- paste0(lines[j+1], "  ", groups[[i]][j], paste0(rep(" ", padding[j]), collapse=""))
             
             if (lineCountMismatch < 0)
-                lines[(length(lines)+lineCountMismatch):length(lines)] <- paste0(lines[(length(lines)+lineCountMismatch):length(lines)], paste0(rep(" ", colWidths[i]+2, collapse="")))
+                lines[(length(lines)+lineCountMismatch+1):length(lines)] <- paste0(lines[(length(lines)+lineCountMismatch+1):length(lines)], paste0(rep(" ", colWidths[i]+2), collapse=""))
             
-            width <- width + colWidths[i]
-            cols <- cols + 1
+            width <- width + colWidths[i] + 2
         }
         
         if (length(lines) > 0)
             cat(paste(lines, collapse="\n"), "\n\n", sep="")
     }
     
-    cat("For information on a particular script, run \"tractor -o <script>\"\n\n", file=stderr())
+    cat(colour("For information on a particular script, run \"tractor -o <script>\"", "yellow"), "\n", sep="")
 }
