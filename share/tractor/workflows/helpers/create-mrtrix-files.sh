51c522d29e6c9454af8a63cb288c5d73eb14a5de
Jon Clayden <code@clayden.org>
2025-09-23 17:36:39 +0100

    Invert sense of MRtrix image mapping

--- share/tractor/workflows/helpers/create-mrtrix-files.sh
+++ share/tractor/workflows/helpers/create-mrtrix-files.sh
@@ -1,5 +1,5 @@
 set -x
 ${FURROW} mkdir -p @mrtrix/
-${FURROW} -z test -f @mrtrix/data || ${TRACTOR} image map @diffusion/data @mrtrix/data
-${FURROW} -z test -f @mrtrix/mask || ${TRACTOR} image map @diffusion/mask @mrtrix/mask
+${FURROW} -z test -f @mrtrix/data || ${TRACTOR} image map @mrtrix/data @diffusion/data
+${FURROW} -z test -f @mrtrix/mask || ${TRACTOR} image map @mrtrix/mask @diffusion/mask
 ${TRACTOR} gradread @mrtrix/bvecs .
