7a83b525d32b0cc4a9213fcca156b3ba50630ae3
Jon Clayden <code@clayden.org>
2025-10-10 22:25:57 +0100

    Update test

--- tests/35_PNT/022_viz.sh
+++ tests/35_PNT/022_viz.sh
@@ -3,4 +3,4 @@ ${TRACTOR} pnt-eval DatasetName:$TRACTOR_TEST_DATA/pnt/pnt_test_data ModelName:$
 ${TRACTOR} pnt-viz $TRACTOR_TEST_DATA/session TractName:genu DatasetName:$TRACTOR_TEST_DATA/pnt/pnt_test_data ResultsName:pnt_results Streamlines:50
 ${TRACTOR} transform $TRACTOR_TEST_DATA/session genu.1 SourceSpace:diffusion TargetSpace:MNI
 ${TRACTOR} apply genu.1_MNI 'ifelse(a>=0.01*max(a),1L,0L)' ResultName:genu.1_thresholded
-${TRACTOR} value genu.1_thresholded 100 168 83
+${TRACTOR} value genu.1_thresholded 100 163 83
