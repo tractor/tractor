6bf5cd1628c5f70f87d36bb3c7d48adc53053ff4
Jon Clayden <code@clayden.org>
2026-07-04 22:04:58 +0100

    Filter out unimportant messages

--- tests/15_Registration/012_bearnonlinear.sh
+++ tests/15_Registration/012_bearnonlinear.sh
@@ -1,3 +1,3 @@
 #@desc Checking uninitialised nonlinear registration
-${TRACTOR} reg-nonlinear $TRACTOR_TEST_DATA/transform.xfmb/source $TRACTOR_TEST_DATA/transform.xfmb/target TransformName:transform EstimateOnly:true
+${TRACTOR} reg-nonlinear $TRACTOR_TEST_DATA/transform.xfmb/source $TRACTOR_TEST_DATA/transform.xfmb/target TransformName:transform EstimateOnly:true | grep -v NiftyReg
 ${TRACTOR} reg-info transform | grep -v orient | grep -v path
